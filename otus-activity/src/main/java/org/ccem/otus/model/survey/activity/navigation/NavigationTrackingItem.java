4e6a38cb9f59ce0d552476a289f9be11cca7cece
Diego Santos <santos.diegoferreira@gmail.com>
2017-02-23 12:40:31 -0300

    OTUS-101: Adicionado mecanismo de tracking para elementos de survey.

--- otus-activity/src/main/java/org/ccem/otus/model/survey/activity/navigation/NavigationTrackingItem.java
+++ otus-activity/src/main/java/org/ccem/otus/model/survey/activity/navigation/NavigationTrackingItem.java
@@ -1,10 +1,14 @@
 package org.ccem.otus.model.survey.activity.navigation;
 
+import java.util.List;
+
 public class NavigationTrackingItem {
 	
 	public String objectType;
 	public String id;
 	public String state;
 	public String previous;
+	public List<String> inputs;
+	public List<String> outputs;
 
 }
