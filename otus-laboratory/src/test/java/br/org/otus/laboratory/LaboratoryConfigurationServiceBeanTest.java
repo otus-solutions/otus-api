e4b1f61f9cedd0ea57b0a0c21b01dfc389b5be9d
fdrtec <fdrtec@gmail.com>
2017-05-04 10:32:33 -0300

    OA-1  4 de 5 metodos em LaboratoryConfigurationServiceBean

--- otus-laboratory/src/test/java/br/org/otus/laboratory/LaboratoryConfigurationServiceBeanTest.java
+++ otus-laboratory/src/test/java/br/org/otus/laboratory/LaboratoryConfigurationServiceBeanTest.java
@@ -1,55 +1,150 @@
 package br.org.otus.laboratory;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import br.org.otus.laboratory.CodeConfiguration;
-import br.org.otus.laboratory.LaboratoryConfiguration;
-import br.org.otus.laboratory.LaboratoryConfigurationServiceBean;
 import br.org.otus.laboratory.collect.group.CollectGroupConfiguration;
+import br.org.otus.laboratory.collect.group.CollectGroupDescriptor;
+import br.org.otus.laboratory.collect.tube.Tube;
+import br.org.otus.laboratory.collect.tube.TubeDefinition;
+import br.org.otus.laboratory.collect.tube.generator.TubeSeed;
+import br.org.otus.laboratory.label.LabelPrintConfiguration;
+import br.org.otus.laboratory.label.LabelReference;
 
-@Ignore
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ LaboratoryConfigurationServiceBean.class })
-public class LaboratoryConfigurationServiceBeanTest {
+@PrepareForTest(LaboratoryConfigurationServiceBean.class)
 
+public class LaboratoryConfigurationServiceBeanTest {
+	
 	@InjectMocks
 	private LaboratoryConfigurationServiceBean laboratoryConfigurationServiceBean;
+	
+	@Mock
+	private LaboratoryConfigurationDao laboratorioConfigurationDao;
 
 	@Mock
-	private LaboratoryConfiguration tubeConfiguration;
-
+	private LaboratoryConfiguration laboratoryConfiguration;
+	
 	@Mock
-	private CollectGroupConfiguration groupConfiguration;
+	private TubeSeed seed;
 
-	@Mock
-	private CodeConfiguration codeDefinition;
+	@Before
+	public void setup() {
+		
+		PowerMockito.when(laboratorioConfigurationDao.find()).thenReturn(laboratoryConfiguration);	
+		
+	}
+	
+		
 
-	/*
-	 * @Before public void setup() { GroupDescriptor defaultGroup = new
-	 * GroupDescriptor("defaultGroup", new ArrayList<>(Arrays.asList("EDTA",
-	 * "GEL"))); Mockito.when(tubeConfiguration.getCodeDefinitions()).thenReturn(
-	 * codeDefinition);
-	 * Mockito.when(tubeConfiguration.getGroupDefinitions()).thenReturn(
-	 * groupDefinition);
-	 * Mockito.when(groupDefinition.getDefaultGroup()).thenReturn(new
-	 * ArrayList<GroupDescriptor>(Arrays.asList(defaultGroup))); }
-	 * 
-	 * @Test public void should_generate_a_tube_code() { String result =
-	 * laboratoryConfigurationServiceBean.getTubeCode(3); assertThat(result,
-	 * equalTo("331000000")); }
-	 * 
-	 * @Test public void should_generate_a_pack_of_tube_codes_of_given_size() {
-	 * List<String> result = laboratoryConfigurationServiceBean.getCodes(7, 3);
-	 * assertThat(result.size(), equalTo(7)); }
-	 * 
-	 * @Test public void should_generate_a_pack_of_tubes_of_given_size() {
-	 * List<String> result =
-	 * laboratoryConfigurationServiceBean.getDefaultTubeDefinitions();
-	 * assertThat(result.size(), equalTo(2)); }
-	 */
+	@Test
+	public void should_to_return_get_Default_TubeSet() {
+		Set<TubeDefinition> tubeSet = new HashSet<>();
+		tubeSet.add(new TubeDefinition(1, "FLOURIDE", "POST OVERLOAD"));
+
+		Set<CollectGroupDescriptor> collectGroupDescriptor = new HashSet<>();
+		collectGroupDescriptor.add(new CollectGroupDescriptor("DEFAULT", "DEFAULT", tubeSet));
+		CollectGroupConfiguration collectGroupConfiguration = new CollectGroupConfiguration(collectGroupDescriptor);
+		PowerMockito.when(laboratoryConfiguration.getCollectGroupConfiguration()).thenReturn(collectGroupConfiguration);
+		
+		TubeDefinition tube = new TubeDefinition(1, "FLOURIDE", "POST OVERLOAD");
+		assertEquals(tube.getCount(), laboratoryConfigurationServiceBean.getDefaultTubeSet().stream().findFirst().get().getCount());
+		assertEquals(tube.getType(), laboratoryConfigurationServiceBean.getDefaultTubeSet().stream().findFirst().get().getType());
+		assertEquals(tube.getMoment(), laboratoryConfigurationServiceBean.getDefaultTubeSet().stream().findFirst().get().getMoment());
+	}
+	
+	@Test
+	public void should_to_return_get_TubeSet_By_GroupName(){
+		Set<TubeDefinition> tubeSet = new HashSet<>();
+		tubeSet.add(new TubeDefinition(1, "CITRATE", "NONE"));
+		
+		Set<CollectGroupDescriptor> collectGroupDescriptor = new HashSet<>();
+		collectGroupDescriptor.add(new CollectGroupDescriptor("MG", "Center", tubeSet));
+		CollectGroupConfiguration collectGroupConfiguration = new CollectGroupConfiguration(collectGroupDescriptor);
+		PowerMockito.when(laboratoryConfiguration.getCollectGroupConfiguration()).thenReturn(collectGroupConfiguration);
+		
+		
+		TubeDefinition expectedTubeSet = new TubeDefinition(1, "CITRATE", "NONE");
+		assertEquals( expectedTubeSet.getCount(), laboratoryConfigurationServiceBean.getTubeSetByGroupName("MG").stream().findFirst().get().getCount());
+		assertEquals( expectedTubeSet.getType(), laboratoryConfigurationServiceBean.getTubeSetByGroupName("MG").stream().findFirst().get().getType());
+		assertEquals( expectedTubeSet.getMoment(), laboratoryConfigurationServiceBean.getTubeSetByGroupName("MG").stream().findFirst().get().getMoment());		
+		
+	}
+	
+	@Test
+	public void should_to_return_list_void_case_not_find_GroupName(){
+		Set<TubeDefinition> tubeSet = new HashSet<>();
+		tubeSet.add(new TubeDefinition(1, "CITRATE", "NONE"));		
+		
+		Set<CollectGroupDescriptor> collectGroupDescriptor = new HashSet<>();
+		collectGroupDescriptor.add(new CollectGroupDescriptor("MG", "Center", tubeSet));
+		CollectGroupConfiguration collectGroupConfiguration = new CollectGroupConfiguration(collectGroupDescriptor);
+		PowerMockito.when(laboratoryConfiguration.getCollectGroupConfiguration()).thenReturn(collectGroupConfiguration);		
+		
+		
+		Set<TubeDefinition> expectedTubeSet = new HashSet<>();		
+		assertEquals(expectedTubeSet , laboratoryConfigurationServiceBean.getTubeSetByGroupName("ES"));
+	
+	}
+	
+	@Test
+	public void should_get_label_order_by_Name(){
+		String reference = "QC_1";
+		List <LabelReference> labelReference = new ArrayList<LabelReference>();
+		labelReference.add(new LabelReference("DEFAULT", "GEL", "FASTING"));				
+		
+		HashMap<String, List<LabelReference>> orders = new HashMap<>();
+		orders.put(reference, labelReference);		
+		LabelPrintConfiguration labelPrintConfiguration = new LabelPrintConfiguration(orders);		
+		PowerMockito.when(laboratoryConfiguration.getLabelPrintConfiguration()).thenReturn(labelPrintConfiguration);
+		
+		List<LabelReference> expectedLabelReference = new ArrayList<LabelReference>();
+		expectedLabelReference.add(new LabelReference("DEFAULT", "GEL", "FASTING"));
+		
+		assertEquals(expectedLabelReference.stream().findFirst().get().getGroupName(),
+				laboratoryConfigurationServiceBean.getLabelOrderByName("QC_1").stream().findFirst().get().getGroupName());
+		assertEquals(expectedLabelReference.stream().findFirst().get().getType(),
+				laboratoryConfigurationServiceBean.getLabelOrderByName("QC_1").stream().findFirst().get().getType());
+		assertEquals(expectedLabelReference.stream().findFirst().get().getMoment(),
+		laboratoryConfigurationServiceBean.getLabelOrderByName("QC_1").stream().findFirst().get().getMoment());		
+						
+	}
+	
+	@Test
+	public void should_to_generate_tube_codes(){
+		
+		seed.setTubeCount(1);
+		Integer startingPoint;
+		
+		PowerMockito.when(laboratoryConfiguration.allocNextCodeList(seed)).thenReturn(startingPoint = 2);
+		
+		CodeConfiguration codeConfiguration = new CodeConfiguration();
+		PowerMockito.when(codeConfiguration.generateCodeList(seed, startingPoint));		
+		
+		
+		assertEquals(2, laboratoryConfigurationServiceBean.generateCodes(seed));
+		
+		
+		
+	}
+	
+	
+	
+	
+
 }
