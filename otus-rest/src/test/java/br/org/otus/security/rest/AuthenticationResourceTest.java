922d39dba2b3732b4e8812f47db5d463d172dbda
fdrtec <fdrtec@gmail.com>
2017-07-06 15:03:01 -0300

    +2 testes unitarios

--- otus-rest/src/test/java/br/org/otus/security/rest/AuthenticationResourceTest.java
+++ otus-rest/src/test/java/br/org/otus/security/rest/AuthenticationResourceTest.java
@@ -1,20 +1,16 @@
 package br.org.otus.security.rest;
 
-
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
-import static org.mockito.Mockito.verify;
-
-
-
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.ccem.otus.exceptions.webservice.security.AuthenticationException;
 import org.ccem.otus.exceptions.webservice.security.EncryptedException;
+import org.ccem.otus.exceptions.webservice.security.TokenException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,75 +19,81 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.nimbusds.jose.JOSEException;
+
 import br.org.otus.response.exception.HttpResponseException;
 import br.org.otus.rest.Response;
 import br.org.otus.security.EncryptorResources;
 import br.org.otus.security.api.SecurityFacade;
 import br.org.otus.security.dtos.AuthenticationDto;
+import br.org.otus.security.dtos.ProjectAuthenticationDto;
 import br.org.otus.security.dtos.UserSecurityAuthorizationDto;
-
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(EncryptorResources.class)
 public class AuthenticationResourceTest {
-	
+
+	private static final String JWT = "eyJhbGciOiJIUzI1NiJ9.eyJtb2RlIjoidXNlciIsImlzcyI6ImRpb2dvLnJvc2FzLmZ"
+			+ "lcnJlaXJhQGdtYWlsLmNvbSJ9.6lNpZm4IhjINsUsWi5paX15dD7gPqlvapilx5Tf90NE";
+
 	@InjectMocks
-	AuthenticationResource authenticationResource;		
-	
+	AuthenticationResource authenticationResource;
+
+	@Mock
+	SecurityFacade securityFacade;
+
 	@Mock
 	HttpServletRequest request;
-	
-	@Mock
-	SecurityFacade securityFacade;	
-	
+
 	@InjectMocks
 	UserSecurityAuthorizationDto userSecurityAuthorizationDto;
-	
-	
+
 	private AuthenticationDto authenticationDto;
-
 	private Response response;
-	
+	private ProjectAuthenticationDto projectAuthenticationDto;
 
-	@Before	
+	@Before
 	public void setUp() throws Exception {
 		mockStatic(EncryptorResources.class);
 		authenticationDto = new AuthenticationDto();
 		authenticationDto.setEmail("otus@otus.com");
-		response = new Response();		
-		
+		when(request.getRemoteAddr()).thenReturn("143.54.220.57");
+		response = new Response();
+		projectAuthenticationDto = new ProjectAuthenticationDto();
 	}
 
 	@Test
 	public void method_Authenticate_should_return_response_userSecurityAuthorizationDto() throws EncryptedException {
 		when(EncryptorResources.encryptIrreversible(anyString())).thenReturn("123");
-		when(request.getRemoteAddr()).thenReturn("143.54.220.57");
-		when(securityFacade.userAuthentication(authenticationDto, request.getRemoteAddr())).thenReturn(userSecurityAuthorizationDto);
-		
-		String autenticateResponseExpected = response.buildSuccess(securityFacade.userAuthentication(authenticationDto, request.getRemoteAddr())).toJson();		
-		System.out.println(autenticateResponseExpected);
-				
-		assertEquals(autenticateResponseExpected,authenticationResource.authenticate(authenticationDto, request));
-		
-		}
-	
-	@Test(expected = HttpResponseException.class)	
-	public void method_Authenticate_should_throw_EncryptedException() throws EncryptedException{	
+		when(securityFacade.userAuthentication(authenticationDto, request.getRemoteAddr()))
+				.thenReturn(userSecurityAuthorizationDto);
+		String autenticateResponseExpected = response
+				.buildSuccess(securityFacade.userAuthentication(authenticationDto, request.getRemoteAddr())).toJson();
+
+		assertEquals(autenticateResponseExpected, authenticationResource.authenticate(authenticationDto, request));
+	}
+
+	@Test(expected = HttpResponseException.class)
+	public void method_Authenticate_should_throw_EncryptedException() throws EncryptedException {
 		when(EncryptorResources.encryptIrreversible(anyString())).thenThrow(new EncryptedException());
 		authenticationResource.authenticate(authenticationDto, request);
-		
-		
-		
 	}
 
 	@Test
-	public void method_ProjectAuthenticate(){
-		
+	public void method_projectAuthenticate_should_return_response_JWT()
+			throws TokenException, AuthenticationException, JOSEException {
+		String responseJWTExpected = response.buildSuccess(JWT).toJson();
+		when(securityFacade.projectAuthentication(projectAuthenticationDto, request.getRemoteAddr().toString()))
+				.thenReturn(JWT);
+
+		assertEquals(responseJWTExpected, authenticationResource.projectAuthenticate(projectAuthenticationDto, request));
 	}
-		
+
 	@Test
-	public void method_Invalidate() {
-		
+	public void method_invalidate_should_call_securityFacade_invalidate_method() {
+		authenticationResource.invalidate(request);
+		verify(securityFacade).invalidate(anyString());
+
 	}
 
 }
