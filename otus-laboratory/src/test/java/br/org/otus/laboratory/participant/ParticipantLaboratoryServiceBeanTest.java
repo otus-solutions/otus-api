1810daafe3e887c081abe8267c2ff2536007eaa7
emanoelvianna <emanoel.vianna@outlook.com>
2017-07-13 17:02:38 -0300

    OA-8 - parcial, teste de unidade

--- otus-laboratory/src/test/java/br/org/otus/laboratory/participant/ParticipantLaboratoryServiceBeanTest.java
+++ otus-laboratory/src/test/java/br/org/otus/laboratory/participant/ParticipantLaboratoryServiceBeanTest.java
@@ -24,44 +24,39 @@ public class ParticipantLaboratoryServiceBeanTest {
 
 	@InjectMocks
 	private ParticipantLaboratoryServiceBean service;
+
 	@Mock
 	private UpdateAliquotsDTO updateAliquotsDTO;
 	@Mock
 	private ParticipantLaboratoryValidator aliquotUpdateValidator;
 	@Mock
 	private ParticipantLaboratoryDao participantLaboratoryDao;
-	@Mock
-	private ParticipantLaboratory mockParticipantLaboratory;
 
+	private ParticipantLaboratoryServiceBean laboratoryService;
 	private ParticipantLaboratory participantLaboratory;
 
 	@Before
 	public void setup() throws DataNotFoundException {
 		JsonObjectFactory jsonObjecParticipantLaboratory = new JsonObjecParticipantLaboratoryFactory();
-		
-		JsonObjectUpdateAliquotsDTOFactory aliquotsDTOFactory = new JsonObjectUpdateAliquotsDTOFactory();
-		updateAliquotsDTO = UpdateAliquotsDTO.deserialize(aliquotsDTOFactory.create().toString());
-
 		participantLaboratory = ParticipantLaboratory.deserialize(jsonObjecParticipantLaboratory.create().toString());
-		Mockito.when(participantLaboratoryDao.findByRecruitmentNumber(RECRUIMENT_NUMBER)).thenReturn(participantLaboratory);
+		
+		Mockito.when(service.getLaboratory(RECRUIMENT_NUMBER)).thenReturn(participantLaboratory);
 	}
 
 	@Ignore
 	@Test
 	public void updateAliquots_method_should_call_methed_validate() throws DataNotFoundException, ValidationException {
-
-		service.updateAliquots(updateAliquotsDTO);
+		
+		laboratoryService.updateAliquots(updateAliquotsDTO);
+		
 		Mockito.verify(aliquotUpdateValidator).validate();
-
+		
 	}
 
 	@Ignore
 	@Test
-	public void updateAliquots_method_should_call_methed_updateLaboratoryData() throws DataNotFoundException, ValidationException {
-
-		service.updateAliquots(updateAliquotsDTO);
-		Mockito.verify(participantLaboratoryDao).updateLaboratoryData(participantLaboratory);
-
+	public void UpdateAliquots_method_when_executed_with_success_should_call_method_update() throws DataNotFoundException, ValidationException {
+		
 	}
 
 	@Test(expected = Exception.class)
@@ -75,9 +70,12 @@ public class ParticipantLaboratoryServiceBeanTest {
 	@Ignore
 	@Test
 	public void when_the_method_is_executed_successfully_and_the_set_of_aliquots_are_valid_then_the_laboratory_must_be_updated() throws DataNotFoundException, ValidationException {
+		ParticipantLaboratoryServiceBean laboratoryService = new ParticipantLaboratoryServiceBean();
+		JsonObjectUpdateAliquotsDTOFactory dtoFactory = new JsonObjectUpdateAliquotsDTOFactory();
+		UpdateAliquotsDTO aliquotsDTO = UpdateAliquotsDTO.deserialize(dtoFactory.create().toString());
 		
-		service.updateAliquots(updateAliquotsDTO);
-		
+		laboratoryService.updateAliquots(aliquotsDTO);
+
 	}
 
 }
