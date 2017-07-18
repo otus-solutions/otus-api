c6ba61c75a9ec833586557ad95be475ee247951b
emanoelvianna <emanoel.vianna@outlook.com>
2017-07-18 13:03:13 -0300

    OA-8 - adicionado retorno de aliquotConfiguration junto com a configuração de laboratorio

--- otus-laboratory/src/main/java/br/org/otus/laboratory/LaboratoryConfigurationDTO.java
+++ otus-laboratory/src/main/java/br/org/otus/laboratory/LaboratoryConfigurationDTO.java
@@ -2,17 +2,22 @@ package br.org.otus.laboratory;
 
 import java.util.List;
 
+import br.org.otus.laboratory.collect.aliquot.AliquotConfiguration;
 import br.org.otus.laboratory.collect.moment.CollectMomentConfiguration;
 import br.org.otus.laboratory.collect.tube.TubeConfiguration;
 
 public class LaboratoryConfigurationDTO {
 	
+	public AliquotConfiguration aliquotConfiguration;
+	public List<MetadataConfiguration> metadataConfiguration;
+	public TubeConfiguration tubeConfiguration;
+	public CollectMomentConfiguration collectMomentConfiguration;
+	
 	public LaboratoryConfigurationDTO(LaboratoryConfiguration laboratoryConfiguration) {
+		this.aliquotConfiguration = laboratoryConfiguration.getAliquotConfiguration();
 		this.metadataConfiguration = laboratoryConfiguration.getMetadataConfiguration();
 		this.tubeConfiguration = laboratoryConfiguration.getTubeConfiguration();
 		this.collectMomentConfiguration = laboratoryConfiguration.getCollectMomentConfiguration();
 	}
-	public List<MetadataConfiguration> metadataConfiguration;
-	public TubeConfiguration tubeConfiguration;
-	public CollectMomentConfiguration collectMomentConfiguration;
+
 }
