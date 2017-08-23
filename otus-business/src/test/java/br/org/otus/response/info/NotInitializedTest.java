4e6c4248dcb94f5383540332b588dbd0a83f7dab
fdrtec <fdrtec@gmail.com>
2017-08-23 11:54:18 -0300

    OTUS-OA-INT110 ParticipantBuilderTest(1 unitTest)

--- otus-business/src/test/java/br/org/otus/response/info/NotInitializedTest.java
+++ otus-business/src/test/java/br/org/otus/response/info/NotInitializedTest.java
@@ -1,6 +1,6 @@
 package br.org.otus.response.info;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,7 +12,5 @@ public class NotInitializedTest {
 	@Test
 	public void method_build_should_return_instanceOf_NotInitialized() {
 		assertTrue(NotInitialized.build() instanceof NotInitialized);
-		
 	}
-
 }
---
+++ otus-participant/src/test/java/org/ccem/otus/participant/builder/ParticipantBuilderTest.java
@@ -0,0 +1,67 @@
+package org.ccem.otus.participant.builder;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import javax.validation.constraints.AssertTrue;
+
+import org.ccem.otus.model.FieldCenter;
+import org.ccem.otus.participant.importation.model.ParticipantImport;
+import org.ccem.otus.participant.model.Participant;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ParticipantBuilder.class)
+public class ParticipantBuilderTest {
+	private static final long RN = 1063154;
+	private static final String NAME = "Luis Alberto";
+	private static final String SEX = "M";
+	private static final String BIRTHDATE = "1954-09-20";
+	private static final String CENTER = "SP";
+
+	@InjectMocks
+	private ParticipantBuilder participantBuilder;
+	@Mock
+	private List<FieldCenter> availablefieldCenters;
+	@Mock
+	ParticipantImport participantImport;
+	@Mock
+	private FieldCenter fieldCenter;
+	Participant participant;
+
+	@Before
+	public void setUp() {
+
+	}
+
+	@Test
+	public void method_buildFromPartipantToImport_should_return_participant() throws Exception {
+
+		participantBuilder = PowerMockito.spy(new ParticipantBuilder(availablefieldCenters));
+
+		when(participantImport.getRn()).thenReturn(RN);
+		when(participantImport.getName()).thenReturn(NAME);
+		when(participantImport.getSex()).thenReturn(SEX);
+		when(participantImport.getBirthdate()).thenReturn(BIRTHDATE);
+		when(participantImport.getCenter()).thenReturn(CENTER);
+		PowerMockito.doReturn(fieldCenter).when(participantBuilder, "getFieldCenterByInitials", CENTER);
+
+		assertEquals(NAME, participantBuilder.buildFromPartipantToImport(participantImport).getName());
+		assertTrue(participantBuilder.buildFromPartipantToImport(participantImport) instanceof Participant);
+	
+		
+
+	}
+
+}
