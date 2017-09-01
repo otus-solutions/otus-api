a7859321efb905b7e1d0e5d6206fd712065c088a
brenoscheffer <brenoscheffer@gmail.com>
2017-09-01 16:52:30 -0300

    OA-17 aliquotCodeSize adicionado ao descritor de centro

--- otus-laboratory/src/main/java/br/org/otus/laboratory/configuration/collect/aliquot/AliquotCenterDescriptors.java
+++ otus-laboratory/src/main/java/br/org/otus/laboratory/configuration/collect/aliquot/AliquotCenterDescriptors.java
@@ -6,12 +6,14 @@ public class AliquotCenterDescriptors {
 
 	private String objectType;
 	private String name;
+	private Integer aliquotCodeSize;
 	private List<AliquotGroupDescriptors> aliquotGroupDescriptors;
 
-	public AliquotCenterDescriptors(String objectType, String name, List<AliquotGroupDescriptors> aliquotGroupDescriptors) {
+	public AliquotCenterDescriptors(String objectType, String name, List<AliquotGroupDescriptors> aliquotGroupDescriptors, Integer aliquotCodeSize) {
 		this.objectType = objectType;
 		this.name = name;
 		this.aliquotGroupDescriptors = aliquotGroupDescriptors;
+		this.aliquotCodeSize = aliquotCodeSize;
 	}
 
 	public String getObjectType() {
@@ -26,4 +28,7 @@ public class AliquotCenterDescriptors {
 		return aliquotGroupDescriptors;
 	}
 
+	public Integer getAliquotCodeSize() {
+		return aliquotCodeSize;
+	}
 }
