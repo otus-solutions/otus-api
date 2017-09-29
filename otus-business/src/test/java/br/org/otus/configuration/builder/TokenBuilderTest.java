1fc5dd1a11fdcc157bf5f3abec78f43009693def
fdrtec <fdrtec@gmail.com>
2017-09-29 11:33:49 -0300

    OA-16 UserFacadeTest (26 unitTests)

--- otus-business/src/test/java/br/org/otus/configuration/builder/TokenBuilderTest.java
+++ otus-business/src/test/java/br/org/otus/configuration/builder/TokenBuilderTest.java
@@ -7,22 +7,19 @@ import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(TokenBuilder.class)
 public class TokenBuilderTest {
-	private static String UUID_STRING = "45b07151-5824-4001-acb0-23981fad36aa";	
+	private static String UUID_STRING = "45b07151-5824-4001-acb0-23981fad36aa";
 
 	@Test
-	public void testBuild() {
+	public void method_build_should_return_token_generated() {
 		mockStatic(UUID.class);
-		Mockito.when(UUID.randomUUID().toString()).thenReturn(UUID_STRING);		
+		Mockito.when(UUID.randomUUID().toString()).thenReturn(UUID_STRING);
 		assertEquals(UUID_STRING, TokenBuilder.build());
 	}
 }
