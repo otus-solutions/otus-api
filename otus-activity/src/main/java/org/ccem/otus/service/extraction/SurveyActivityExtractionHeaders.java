ee84ef4172d97eb688712ade67c20f2ece99e00e
brenoscheffer <brenoscheffer@gmail.com>
2017-10-02 17:27:19 -0300

    OA-3 Mapeamento de cabeçalhos de extração de atividades - início

---
+++ otus-activity/src/main/java/org/ccem/otus/service/extraction/SurveyActivityExtractionHeaders.java
@@ -0,0 +1,20 @@
+package org.ccem.otus.service.extraction;
+
+public enum SurveyActivityExtractionHeaders {
+	recruitment_number,
+	acronym,
+	category,
+	type,
+	interviewer,
+	current_status,
+	current_status_date,
+	creation_date,
+	paper,
+	realization_date,
+	last_finalization_date;
+
+	public String getAll(){
+		return null;
+	}
+}
+
--- otus-activity/src/main/java/org/ccem/otus/service/extraction/SurveyActivityExtractor.java
+++ otus-activity/src/main/java/org/ccem/otus/service/extraction/SurveyActivityExtractor.java
@@ -1,8 +1,6 @@
 package org.ccem.otus.service.extraction;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import org.ccem.otus.model.survey.activity.SurveyActivity;
 
@@ -11,21 +9,31 @@ import br.org.otus.api.Extractable;
 public class SurveyActivityExtractor implements Extractable {
 
 	private List<SurveyActivity> surveyActivities;
+	private HashMap<String, Object> headers;
 	private List<List<Object>> values;
 
 	public SurveyActivityExtractor(List<SurveyActivity> surveyActivities) {
 		this.surveyActivities = surveyActivities;
+		setHeaders();
+	}
+
+	private void setHeaders() {
+		this.headers= new LinkedHashMap<>();
+
+		surveyActivities.get(0).getSurveyForm().getSurveyTemplate().itemContainer.forEach(surveyItem -> {
+			for (String header : surveyItem.getExtractionIDs()) headers.put(header, "");
+		});
 	}
 
 	@Override
 	public Set<String> getHeaders() {
-		return surveyActivities.get(0).getSurveyForm().getSurveyTemplate().getOrderedCustomIDs();
+		return headers.keySet();
 	}
 
 	@Override
 	public List<List<Object>> getValues() {
 		this.values = new ArrayList<List<Object>>();
-		for (SurveyActivity surveyActivity : surveyActivities) {
+		for (SurveyActivity surveyAcfativity : surveyActivities) {
 			// TODO: obter valores de repostas
 		}
 		return values;
