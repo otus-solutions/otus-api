681cfc1a0506dd5d25c75c8a98abd4640b7abbd6
Fabiano Ramires <fdrtec@gmail.com>
2018-01-19 13:04:06 -0200

    retirada do recordsFactory do construtor

--- otus-activity/src/main/java/org/ccem/otus/service/extraction/SurveyActivityExtraction.java
+++ otus-activity/src/main/java/org/ccem/otus/service/extraction/SurveyActivityExtraction.java
@@ -20,7 +20,7 @@ public class SurveyActivityExtraction implements Extractable {
 		this.surveyActivities = surveyActivities;
 		this.surveyForm = surveyForm;
 		this.headersFactory = new SurveyActivityExtractionHeadersFactory(this.surveyForm);
-		this.recordsFactory = new SurveyActivityExtractionRecordsFactory(this.surveyForm, this.headersFactory.getHeaders());
+		
 	}
 
 	@Override
@@ -33,13 +33,13 @@ public class SurveyActivityExtraction implements Extractable {
 		List<List<Object>> values = new ArrayList<>();
 
 		for (SurveyActivity surveyActivity : this.surveyActivities) {
-					
+			this.recordsFactory = new SurveyActivityExtractionRecordsFactory(this.surveyForm, this.headersFactory.getHeaders());			
 			List<Object> resultInformation = new ArrayList<>();
 			this.recordsFactory.getSurveyBasicInfo(surveyActivity);
 			this.recordsFactory.getSurveyQuestionInfo(surveyActivity);
 			resultInformation.addAll(new ArrayList<>(this.recordsFactory.getSurveyInformation().values()));
 			values.add(resultInformation);			
-			this.recordsFactory = new SurveyActivityExtractionRecordsFactory(this.surveyForm, this.headersFactory.getHeaders());
+			
 		}
 		return values;
 	}
