9e7555c8866442a30a13adfa7284b580b3c5547e
Fabiano Ramires <fdrtec@gmail.com>
2018-02-20 11:15:07 -0300

    OA-40 adição do processingDate ao model da aliquot

--- otus-laboratory/src/main/java/br/org/otus/laboratory/participant/aliquot/AliquotCollectionData.java
+++ otus-laboratory/src/main/java/br/org/otus/laboratory/participant/aliquot/AliquotCollectionData.java
@@ -8,11 +8,14 @@ public class AliquotCollectionData {
 	private String metadata;
 	private String operator;
 	private LocalDateTime time;
+	private LocalDateTime processingDate;
+	
 
-	public AliquotCollectionData(String metadata, String operator, LocalDateTime time) {
+	public AliquotCollectionData(String metadata, String operator, LocalDateTime time, LocalDateTime processingDate) {
 		this.metadata = metadata;
 		this.operator = operator;
 		this.time = time;
+		this.processingDate = processingDate;
 	}
 
 	public String getMetadata() {
@@ -29,6 +32,10 @@ public class AliquotCollectionData {
 
 	public String getObjectType() {
 		return objectType;
+	}
+	
+	public LocalDateTime getProcessingDate() {
+		return processingDate;
 	}
 
 }
