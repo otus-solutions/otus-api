f95ab1cd29105522bf7ec8196616377b3129d9f3
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2018-03-08 19:42:38 -0300

    OA-41 Correção mecanismo de extração de dados

---
+++ otus-activity/src/main/java/org/ccem/otus/service/extraction/factories/SurveyBasicInfoRecordsFactory.java
@@ -0,0 +1,102 @@
+package org.ccem.otus.service.extraction.factories;
+
+import org.ccem.otus.model.survey.activity.SurveyActivity;
+import org.ccem.otus.model.survey.activity.interview.Interview;
+import org.ccem.otus.model.survey.activity.status.ActivityStatus;
+import org.ccem.otus.model.survey.activity.status.ActivityStatusOptions;
+
+import java.time.LocalDateTime;
+
+public class SurveyBasicInfoRecordsFactory {
+
+    public static final String HEADER_MESSAGE = "[EXTRACTION]";
+    public static final String LAST_INTERVIEWER_NOT_FOUND = "Interviewer value error";
+    private static final String CURRENT_STATUS_NAME_NOT_FOUND = "Current status name value error";
+    private static final String CURRENT_STATUS_DATE_NOT_FOUND = "Current status date value error";
+    private static final String CREATION_DATE_NOT_FOUND = "Current creation date value error";
+    private static final String PAPPER_DATE_NOT_FOUND = "Paper date value error";
+    private static final String PAPPER_INTERVIEWER_NOT_FOUND = "Paper interviewer value error";
+    private static final String FINALIZED_STATUS_NOT_FOUND = "Finalized status value error";
+
+    public static String getLastInterviewer(SurveyActivity surveyActivity) {
+        try {
+            final Interview lastInterview = surveyActivity.getLastInterview().orElse(null);
+            final String interviewerEmail = lastInterview != null ? lastInterview.getInterviewer().getEmail() : null;
+            return interviewerEmail;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(LAST_INTERVIEWER_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static String getCurrentStatusName(ActivityStatus currentActivityStatus) {
+        try {
+            return currentActivityStatus != null ? currentActivityStatus.getName() : null;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(CURRENT_STATUS_NAME_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static LocalDateTime getCurrentStatusDate(ActivityStatus currentActivityStatus) {
+        try {
+            return (currentActivityStatus != null) ? currentActivityStatus.getDate() : null;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(CURRENT_STATUS_DATE_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static LocalDateTime getCreationDate(SurveyActivity surveyActivity) {
+        try {
+            final ActivityStatus creationStatus = surveyActivity.getLastStatusByName(ActivityStatusOptions.CREATED.getName()).orElse(null);
+            final LocalDateTime creationTime = (creationStatus != null) ? creationStatus.getDate() : null;
+            return creationTime;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(CREATION_DATE_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static LocalDateTime getPaperRealizationDate(ActivityStatus paperStatus) {
+        try {
+            return (paperStatus != null) ? paperStatus.getDate() : null;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(PAPPER_DATE_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static String getPaperInterviewer(ActivityStatus paperStatus) {
+        try {
+            return (paperStatus != null) ? paperStatus.getUser().getEmail() : null;
+
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(PAPPER_INTERVIEWER_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static LocalDateTime getLasFinalizationDate(SurveyActivity surveyActivity) {
+        try {
+            final ActivityStatus finalizedStatus = surveyActivity.getLastStatusByName(ActivityStatusOptions.FINALIZED.getName()).orElse(null);
+            return (finalizedStatus != null) ? finalizedStatus.getDate() : null;
+        } catch (Exception e) {
+            SurveyBasicInfoRecordsFactory.printMessageError(FINALIZED_STATUS_NOT_FOUND);
+            return null;
+        }
+    }
+
+    public static void printMessageError(String details) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(HEADER_MESSAGE);
+        stringBuilder.append(": ");
+        stringBuilder.append(details);
+        System.err.println(stringBuilder.toString());
+    }
+}
---
+++ otus-activity/src/test/java/org/ccem/otus/service/extraction/factories/SurveyBasicInfoRecordsFactoryTest.java
@@ -0,0 +1,59 @@
+package org.ccem.otus.service.extraction.factories;
+
+import org.ccem.otus.model.survey.activity.SurveyActivity;
+import org.ccem.otus.model.survey.activity.interview.Interview;
+import org.ccem.otus.model.survey.activity.status.ActivityStatus;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@PrepareForTest({SurveyBasicInfoRecordsFactory.class})
+public class SurveyBasicInfoRecordsFactoryTest {
+    @Mock
+    private SurveyActivity surveyActivity;
+
+    @Test
+    public void method_getLastInterview_should_return_null_when_interview_dont_have_user(){
+        PowerMockito.when(surveyActivity.getLastInterview()).thenReturn(null);
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getLastInterviewer(surveyActivity));
+    }
+
+    @Test
+    public void method_getCurrentStatusName_should_return_null_when_dont_exist_status(){
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getCurrentStatusName(null));
+    }
+
+    @Test
+    public void method_getCurrentStatusDate_should_return_null_when_don_exist_current_status(){
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getCurrentStatusDate(null));
+    }
+
+    @Test
+    public void method_getCreationDate_should_return_null_when_dont_exist_creation_status(){
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getCreationDate(null));
+    }
+
+    @Test
+    public void method_getPaperRealizationDate_should_return_null_when_dont_exist_paper_status(){
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getPaperRealizationDate(null));
+    }
+
+    @Test
+    public void method_getPaperInterviewer_should_return_null_when_dont_exist_user_in_status(){
+        ActivityStatus activityStatus = PowerMockito.mock(ActivityStatus.class);
+        Mockito.when(activityStatus.getUser()).thenReturn(null);
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getPaperInterviewer(activityStatus));
+    }
+
+    @Test
+    public void method_getLastFinalizedDate_should_return_null_when_finalized_status_dont_exist(){
+        Assert.assertNull(SurveyBasicInfoRecordsFactory.getLasFinalizationDate(null));
+
+    }
+}
