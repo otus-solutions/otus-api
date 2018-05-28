25a0104402fcc6a88bc0838a62650de664c53983
emanoelvianna <emanoel.vianna@outlook.com>
2018-05-28 13:52:04 -0300

    OA-68 - modificações na query de busca do statusHistory

--- otus-report/src/main/java/org/ccem/otus/model/dataSources/activity/ActivityDataSource.java
+++ otus-report/src/main/java/org/ccem/otus/model/dataSources/activity/ActivityDataSource.java
@@ -1,6 +1,7 @@
 package org.ccem.otus.model.dataSources.activity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.bson.Document;
 import org.ccem.otus.model.dataSources.ReportDataSource;
@@ -39,8 +40,11 @@ public class ActivityDataSource extends ReportDataSource<ActivityDataSourceResul
 
   private void buildProjectionStage(ArrayList<Document> query) {
     Document projectionFields = new Document("_id", -1);
+    projectionFields.append("statusHistory", 1);
     if (this.filters.getStatusHistory() != null) {
-      projectionFields.append("statusHistory", 1);
+      if (this.filters.getStatusHistory().getPosition() != null) {
+        projectionFields.append("statusHistoryPosition", new Document("$slice", Arrays.asList("$statusHistory", this.filters.getStatusHistory().getPosition(), 1)));
+      }
     }
     projectionFields.append("mode", 1);
     Document projectStage = new Document("$project", projectionFields);
@@ -49,7 +53,12 @@ public class ActivityDataSource extends ReportDataSource<ActivityDataSourceResul
 
   private void appendStatusHistoryFilter(ArrayList<Document> query) {
     if (this.filters.getStatusHistory() != null) {
-      Document statusHistoryMatchStage = new Document("$match", new Document("statusHistory.name", this.filters.getStatusHistory().getName()));
+      Document statusHistoryMatchStage;
+      if (this.filters.getStatusHistory().getPosition() == null) {
+        statusHistoryMatchStage = new Document("$match", new Document("statusHistory.name", this.filters.getStatusHistory().getName()));
+      } else {
+        statusHistoryMatchStage = new Document("$match", new Document("statusHistoryPosition.name", this.filters.getStatusHistory().getName()));
+      }
       query.add(statusHistoryMatchStage);
     }
   }
--- otus-report/src/test/java/org/ccem/otus/model/dataSources/activity/ActivityDataSourceTest.java
+++ otus-report/src/test/java/org/ccem/otus/model/dataSources/activity/ActivityDataSourceTest.java
@@ -16,56 +16,56 @@ import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
 public class ActivityDataSourceTest {
-	private static final Long RECRUITMENT_NUMBER = 343545345L;
-	private static final String EXPECTED_RESULT = "{ \"$match\" : { \"participantData.recruitmentNumber\" : { \"$numberLong\" : \"343545345\" }, \"surveyForm.surveyTemplate.identity.acronym\" : \"TF\", \"category.name\" : \"C0\", \"isDiscarded\" : false } }";
-	private static final String EXPECTED_RESULT_PROJECTION = "{ \"$project\" : { \"_id\" : -1, \"mode\" : 1 } }";
-	private static final String EXPECTED_RESULT_PROJECTION_WHIT_STATUS_HISTORY = "{ \"$project\" : { \"_id\" : -1, \"statusHistory\" : 1, \"mode\" : 1 } }";
-	private static final String EXPECTED_RESULT_MATCH_WHIT_STATUS_HISTORY = "{ \"$match\" : { \"statusHistory.name\" : \"FINALIZED\" } }";
+  private static final Long RECRUITMENT_NUMBER = 343545345L;
+  private static final String EXPECTED_RESULT = "{ \"$match\" : { \"participantData.recruitmentNumber\" : { \"$numberLong\" : \"343545345\" }, \"surveyForm.surveyTemplate.identity.acronym\" : \"TF\", \"category.name\" : \"C0\", \"isDiscarded\" : false } }";
+  private static final String EXPECTED_RESULT_PROJECTION = "{ \"$project\" : { \"_id\" : -1, \"statusHistory\" : 1, \"mode\" : 1 } }";
+  private static final String EXPECTED_RESULT_PROJECTION_WHIT_STATUS_HISTORY = "{ \"$project\" : { \"_id\" : -1, \"statusHistory\" : 1, \"statusHistoryPosition\" : { \"$slice\" : [\"$statusHistory\", -1, 1] }, \"mode\" : 1 } }";
+  private static final String EXPECTED_RESULT_MATCH_WHIT_STATUS_HISTORY = "{ \"$match\" : { \"statusHistoryPosition.name\" : \"FINALIZED\" } }";
 
-	private ActivityDataSource activityDataSource;
-	private ActivityDataSourceStatusHistoryFilter activityDataSourceStatusHistoryFilter;
-	private ActivityDataSourceFilters activityDataSourceFilters;
-	private ActivityDataSource activityDataSourceAddResult = spy(new ActivityDataSource());
+  private ActivityDataSource activityDataSource;
+  private ActivityDataSourceStatusHistoryFilter activityDataSourceStatusHistoryFilter;
+  private ActivityDataSourceFilters activityDataSourceFilters;
+  private ActivityDataSource activityDataSourceAddResult = spy(new ActivityDataSource());
 
-	@Before
-	public void setUp() {
-		activityDataSourceFilters = new ActivityDataSourceFilters();
-		activityDataSource = new ActivityDataSource();
+  @Before
+  public void setUp() {
+    activityDataSourceFilters = new ActivityDataSourceFilters();
+    activityDataSource = new ActivityDataSource();
 
-		Whitebox.setInternalState(activityDataSourceFilters, "acronym", "TF");
-		Whitebox.setInternalState(activityDataSourceFilters, "category", "C0");
-	}
+    Whitebox.setInternalState(activityDataSourceFilters, "acronym", "TF");
+    Whitebox.setInternalState(activityDataSourceFilters, "category", "C0");
+  }
 
-	@Test
-	public void method_builtQuery() {
-		Whitebox.setInternalState(activityDataSource, "filters", activityDataSourceFilters);
-		ArrayList<Document> query = activityDataSource.buildQuery((long) 343545345);
+  @Test
+  public void method_builtQuery() {
+    Whitebox.setInternalState(activityDataSource, "filters", activityDataSourceFilters);
+    ArrayList<Document> query = activityDataSource.buildQuery((long) 343545345);
 
-		assertEquals(EXPECTED_RESULT, query.get(0).toJson());
-		assertEquals(EXPECTED_RESULT_PROJECTION, query.get(1).toJson());
-	}
+    assertEquals(EXPECTED_RESULT, query.get(0).toJson());
+    assertEquals(EXPECTED_RESULT_PROJECTION, query.get(1).toJson());
+  }
 
-	@Test
-	public void method_builtQuery_with_StatusHistoryFilter() {
-		activityDataSourceStatusHistoryFilter = new ActivityDataSourceStatusHistoryFilter();
+  @Test
+  public void method_builtQuery_with_StatusHistoryFilter() {
+    activityDataSourceStatusHistoryFilter = new ActivityDataSourceStatusHistoryFilter();
 
-		Whitebox.setInternalState(activityDataSourceStatusHistoryFilter, "name", "FINALIZED");
-		Whitebox.setInternalState(activityDataSourceStatusHistoryFilter, "position", -1);
-		Whitebox.setInternalState(activityDataSourceFilters, "statusHistory", activityDataSourceStatusHistoryFilter);
-		Whitebox.setInternalState(activityDataSource, "filters", activityDataSourceFilters);
+    Whitebox.setInternalState(activityDataSourceStatusHistoryFilter, "name", "FINALIZED");
+    Whitebox.setInternalState(activityDataSourceStatusHistoryFilter, "position", -1);
+    Whitebox.setInternalState(activityDataSourceFilters, "statusHistory", activityDataSourceStatusHistoryFilter);
+    Whitebox.setInternalState(activityDataSource, "filters", activityDataSourceFilters);
 
-		ArrayList<Document> query = activityDataSource.buildQuery(RECRUITMENT_NUMBER);
+    ArrayList<Document> query = activityDataSource.buildQuery(RECRUITMENT_NUMBER);
 
-		assertEquals(EXPECTED_RESULT, query.get(0).toJson());
-		assertEquals(EXPECTED_RESULT_PROJECTION_WHIT_STATUS_HISTORY, query.get(1).toJson());
-		assertEquals(EXPECTED_RESULT_MATCH_WHIT_STATUS_HISTORY, query.get(2).toJson());
-	}
+    assertEquals(EXPECTED_RESULT, query.get(0).toJson());
+    assertEquals(EXPECTED_RESULT_PROJECTION_WHIT_STATUS_HISTORY, query.get(1).toJson());
+    assertEquals(EXPECTED_RESULT_MATCH_WHIT_STATUS_HISTORY, query.get(2).toJson());
+  }
 
-	@Test
-	public void method_add_result() throws Exception {
-		ActivityDataSourceResult activityDataSourceResult = new ActivityDataSourceResult();
-		activityDataSourceAddResult.addResult(activityDataSourceResult);
+  @Test
+  public void method_add_result() throws Exception {
+    ActivityDataSourceResult activityDataSourceResult = new ActivityDataSourceResult();
+    activityDataSourceAddResult.addResult(activityDataSourceResult);
 
-		verifyPrivate(activityDataSourceAddResult, times(1)).invoke("addResult", activityDataSourceResult);
-	}
+    verifyPrivate(activityDataSourceAddResult, times(1)).invoke("addResult", activityDataSourceResult);
+  }
 }
