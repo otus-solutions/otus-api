483ac6f0b1bf0e64507985d83ce15692fd99bbcf
emanoelvianna <emanoel.vianna@outlook.com>
2018-05-28 13:04:02 -0300

    OA-68 - indentação do código modificado

--- otus-report/src/main/java/org/ccem/otus/model/dataSources/activity/ActivityDataSourceResult.java
+++ otus-report/src/main/java/org/ccem/otus/model/dataSources/activity/ActivityDataSourceResult.java
@@ -10,29 +10,29 @@ import org.ccem.otus.survey.template.utils.adapters.LocalDateTimeAdapter;
 import com.google.gson.GsonBuilder;
 
 public class ActivityDataSourceResult {
-	private List<ActivityStatus> statusHistory;
-	private ActivityMode mode;
+  private List<ActivityStatus> statusHistory;
+  private ActivityMode mode;
 
-	public static String serialize(ActivityDataSourceResult activityDataSourceResult) {
-		return getGsonBuilder().create().toJson(activityDataSourceResult);
-	}
+  public static String serialize(ActivityDataSourceResult activityDataSourceResult) {
+    return getGsonBuilder().create().toJson(activityDataSourceResult);
+  }
 
-	public static ActivityDataSourceResult deserialize(String DataSource) {
-		GsonBuilder builder = ActivityDataSourceResult.getGsonBuilder();
-		return builder.create().fromJson(DataSource, ActivityDataSourceResult.class);
-	}
+  public static ActivityDataSourceResult deserialize(String DataSource) {
+    GsonBuilder builder = ActivityDataSourceResult.getGsonBuilder();
+    return builder.create().fromJson(DataSource, ActivityDataSourceResult.class);
+  }
 
-	private static GsonBuilder getGsonBuilder() {
-		GsonBuilder builder = new GsonBuilder();
-		builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
-		return builder;
-	}
+  private static GsonBuilder getGsonBuilder() {
+    GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+    return builder;
+  }
 
-	public List<ActivityStatus> getStatusHistory() {
-		return statusHistory;
-	}
+  public List<ActivityStatus> getStatusHistory() {
+    return statusHistory;
+  }
 
-	public ActivityMode getMode() {
-		return mode;
-	}
+  public ActivityMode getMode() {
+    return mode;
+  }
 }
