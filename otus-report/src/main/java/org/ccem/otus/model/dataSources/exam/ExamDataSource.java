bde634f33d8d70d592cd2ef5e8aadfa95befc394
Allister Terhorst Ramos <allistertr@hotmail.com>
2018-05-30 14:53:55 -0300

    OA-69 - Incluindo Constantes e correção dos teste

--- otus-report/src/main/java/org/ccem/otus/model/dataSources/exam/ExamDataSource.java
+++ otus-report/src/main/java/org/ccem/otus/model/dataSources/exam/ExamDataSource.java
@@ -9,63 +9,85 @@ import org.ccem.otus.model.dataSources.ReportDataSource;
 
 public class ExamDataSource extends ReportDataSource<ExamDataSourceResult> {
 
-	private ExamDataSourceFilters filters;
+    public static String SIGN_ID = "$_id";
+    public static String SIGN_EQ = "$eq";
+    public static String SIGN_EXPR = "$expr";
+    public static String SIGN_MATCH = "$match";
+    public static String SIGN_EXISTS = "$exists";
+    public static String SIGN_LOOKUP = "$lookup";
+    public static String SIGN_EXAM_ID = "$examId";
+    public static String SIGN_EXAM_OID = "$$exam_oid";
 
-	@Override
-	public void addResult(ExamDataSourceResult result) {
-		super.getResult().add(result);
-	}
+    public static String AS = "as";
+    public static String LET = "let";
+    public static String FROM = "from";
+    public static String NAME = "name";
+    public static String EXAM = "Exam";
+    public static String PIPELINE = "pipeline";
+    public static String EXAM_OID = "exam_oid";
+    public static String OBJECT_TYPE = "objectType";
+    public static String EXAM_RESULTS = "examResults";
+    public static String ALIQUOT_VALID = "aliquotValid";
+    public static String COLLECTION_EXAM_RESULT = "exam_result";
+    public static String EXAM_SENDING_LOT_ID = "examSendingLotId";
+    public static String RECRUITMENT_NUMBER = "recruitmentNumber";
+    public static String EXAM_RESULTS_RECRUITMENT_NUMBER = "examResults.recruitmentNumber";
 
-	@Override
-	public ArrayList<Document> buildQuery(Long recruitmentNumber) {
-		return this.buildQueryToExamSendingLot(recruitmentNumber);
-	}
+    private ExamDataSourceFilters filters;
 
-	public ArrayList<Document> buildQueryToExamResults(ObjectId objectId, Long recruitmentNumber) {
-		ArrayList<Document> query = new ArrayList<>();
+    @Override
+    public void addResult(ExamDataSourceResult result) {
+        super.getResult().add(result);
+    }
 
-		Document match = new Document("$match", new Document("examSendingLotId", objectId)
-			.append("objectType", "Exam").append("name", this.filters.getExamName())
-		);
-		query.add(match);
+    @Override
+    public ArrayList<Document> buildQuery(Long recruitmentNumber) {
+        return this.buildQueryToExamSendingLot(recruitmentNumber);
+    }
 
-		Document lookup = new Document("$lookup", new Document("from", "exam_result")
-			.append("let", new Document("exam_oid", "$_id"))
-				.append("pipeline", Arrays.asList(
-						new Document("$match", new Document("recruitmentNumber", recruitmentNumber)
-								.append("aliquotValid", Boolean.TRUE)
-								.append("$expr", new Document("$and", Arrays.asList(
-										new Document("$eq", Arrays.asList("$examId", "$$exam_oid"))
-								)))
-						)
-				))
-				.append("as", "examResults")
-		);
-		query.add(lookup);
+    public ArrayList<Document> buildQueryToExamResults(ObjectId objectId, Long recruitmentNumber) {
+        ArrayList<Document> query = new ArrayList<>();
 
-		Document matchHasExamResult = new Document("$match", new Document("examResults.recruitmentNumber", new Document("$exists", 1)));
-		query.add(matchHasExamResult);
+        Document match = new Document(SIGN_MATCH, new Document(EXAM_SENDING_LOT_ID, objectId)
+                .append(OBJECT_TYPE, EXAM).append(NAME, this.filters.getExamName())
+        );
+        query.add(match);
 
-		return query;
-	}
+        Document lookup = new Document(SIGN_LOOKUP, new Document(FROM, COLLECTION_EXAM_RESULT)
+                .append(LET, new Document(EXAM_OID, SIGN_ID))
+                .append(PIPELINE, Arrays.asList(
+                        new Document(SIGN_MATCH, new Document(RECRUITMENT_NUMBER, recruitmentNumber)
+                                .append(ALIQUOT_VALID, Boolean.TRUE)
+                                .append(SIGN_EXPR, new Document(SIGN_EQ, Arrays.asList(SIGN_EXAM_ID, SIGN_EXAM_OID)))
+                        )
+                ))
+                .append(AS, EXAM_RESULTS)
+        );
+        query.add(lookup);
 
-	private ArrayList<Document> buildQueryToExamSendingLot(Long recruitmentNumber) {
-		ArrayList<Document> query = new ArrayList<>();
+        Document matchHasExamResult = new Document(SIGN_MATCH, new Document(EXAM_RESULTS_RECRUITMENT_NUMBER, new Document(SIGN_EXISTS, 1)));
+        query.add(matchHasExamResult);
+
+        return query;
+    }
+
+    private ArrayList<Document> buildQueryToExamSendingLot(Long recruitmentNumber) {
+        ArrayList<Document> query = new ArrayList<>();
 
         Document match = new Document(
-            "$match",
-            new Document("objectType", "ExamResults")
-                .append("examName", this.filters.getExamName())
-                .append("recruitmentNumber", recruitmentNumber)
+                "$match",
+                new Document("objectType", "ExamResults")
+                        .append("examName", this.filters.getExamName())
+                        .append("recruitmentNumber", recruitmentNumber)
         );
         query.add(match);
 
         Document lookup = new Document(
-            "$lookup",
-            new Document("from", "exam_sending_lot")
-                .append("localField", "examSendingLotId")
-                .append("foreignField", "_id")
-                .append("as", "sendingLot")
+                "$lookup",
+                new Document("from", "exam_sending_lot")
+                        .append("localField", "examSendingLotId")
+                        .append("foreignField", "_id")
+                        .append("as", "sendingLot")
         );
         query.add(lookup);
 
@@ -76,7 +98,7 @@ public class ExamDataSource extends ReportDataSource<ExamDataSourceResult> {
         query.add(sort);
 
         Document limitToResults = new Document("$limit", 1);
-		query.add(limitToResults);
+        query.add(limitToResults);
 
         Document unwind = new Document("$unwind", new Document("path", "$sendingLot"));
         query.add(unwind);
@@ -84,7 +106,7 @@ public class ExamDataSource extends ReportDataSource<ExamDataSourceResult> {
         Document replaceRoot = new Document("$replaceRoot", new Document("newRoot", "$sendingLot"));
         query.add(replaceRoot);
 
-		return query;
-	}
+        return query;
+    }
 
 }
