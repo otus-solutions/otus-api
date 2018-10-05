90a79b2995830ba042254fb9a6068ade8e4cb209
brenoscheffer <brenoscheffer@gmail.com>
2018-10-05 12:42:25 -0300

    OA-47 Evolução dos testes unitários

--- otus-persistence/src/test/java/br/org/otus/monitoring/builder/ActivityStatusQueryBuilderTest.java
+++ otus-persistence/src/test/java/br/org/otus/monitoring/builder/ActivityStatusQueryBuilderTest.java
@@ -1,47 +1,69 @@
 package br.org.otus.monitoring.builder;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ActivityStatusQueryBuilderTest {
-  private static final ActivityStatusQueryBuilder BUILDER = new ActivityStatusQueryBuilder();
+  private static ActivityStatusQueryBuilder builder = null;
 
+  @Before
+  public void setUp() {
+    builder = new ActivityStatusQueryBuilder();
+  }
 
   @Test
   public void build() {
+    builder.matchFieldCenter("RS");
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void matchFieldCenter() {
-    BUILDER.matchFieldCenter("RS");
+    builder.matchFieldCenter("RS");
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void limit() {
+    builder.limit(10);
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void projectLastStatus() {
+    builder.projectLastStatus();
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void getStatusValue() {
+    builder.getStatusValue();
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void projectId() {
+    builder.projectId();
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void sortByDate() {
+    builder.sortByDate();
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void removeStatusDate() {
+    builder.removeStatusDate();
+    assertEquals(1, builder.build().size());
   }
 
   @Test
   public void groupByParticipant() {
+    builder.groupByParticipant();
+    assertEquals(1, builder.build().size());
   }
 }
