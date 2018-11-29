ffe66a0c91b09a1af83102a411559366c22c2644
emanoelvianna <emanoel.vianna@outlook.com>
2018-11-29 10:38:19 -0200

    OA-101 - modificações realizadas para funcionamento da importação do participante

--- otus-participant/src/main/java/org/ccem/otus/participant/builder/ParticipantBuilder.java
+++ otus-participant/src/main/java/org/ccem/otus/participant/builder/ParticipantBuilder.java
@@ -1,43 +1,45 @@
 package org.ccem.otus.participant.builder;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ccem.otus.model.FieldCenter;
 import org.ccem.otus.participant.importation.model.ParticipantImport;
 import org.ccem.otus.participant.model.Participant;
 import org.ccem.otus.participant.model.Sex;
 import org.ccem.otus.survey.template.utils.date.ImmutableDate;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 public class ParticipantBuilder {
 
-	private Participant participant;
-	private List<FieldCenter> fieldCenters;
+  private Participant participant;
+  private List<FieldCenter> fieldCenters;
 
-	public ParticipantBuilder(List<FieldCenter> availableFieldCenters) {
-		fieldCenters = new ArrayList<FieldCenter>();
-		fieldCenters = availableFieldCenters;
-	}
+  public ParticipantBuilder(List<FieldCenter> availableFieldCenters) {
+    fieldCenters = new ArrayList<FieldCenter>();
+    fieldCenters = availableFieldCenters;
+  }
 
-	public Participant buildFromPartipantToImport(ParticipantImport participantToImport) {
-		participant = new Participant(participantToImport.getRn());
-		participant.setName(participantToImport.getName());
-		participant.setSex(Sex.valueOf(participantToImport.getSex()));
-		participant.setBirthdate(new ImmutableDate(LocalDate.parse(participantToImport.getBirthdate())));
+  public Participant buildFromPartipantToImport(ParticipantImport participantToImport) {
+    // TODO: Quando alguns dos dados aqui presentes estar null, deve ser lançado uma execção!  
+    
+    participant = new Participant(participantToImport.getRn());
+    participant.setName(participantToImport.getName());
+    participant.setSex(Sex.valueOf(participantToImport.getSex()));
+    participant.setBirthdate(new ImmutableDate(LocalDate.parse(participantToImport.getBirthdate())));
+    participant.setLate(participantToImport.getLate());
+    participant.setFieldCenter(getFieldCenterByInitials(participantToImport.getCenter()));
 
-		participant.setFieldCenter(getFieldCenterByInitials(participantToImport.getCenter()));
+    return participant;
+  }
 
-		return participant;
-	}
-
-	private FieldCenter getFieldCenterByInitials(String fieldCenterInitials) {
-		for (FieldCenter fieldCenter : fieldCenters) {
-			if (fieldCenter.getAcronym().equals(fieldCenterInitials)) {
-				return fieldCenter;
-			}
-		}
-		return null;
-	}
+  private FieldCenter getFieldCenterByInitials(String fieldCenterInitials) {
+    for (FieldCenter fieldCenter : fieldCenters) {
+      if (fieldCenter.getAcronym().equals(fieldCenterInitials)) {
+        return fieldCenter;
+      }
+    }
+    return null;
+  }
 
 }
