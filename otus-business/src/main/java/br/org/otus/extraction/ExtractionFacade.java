df2ae4291225fd1331a763942e20623ca6aa3519
emanoelvianna <emanoel.vianna@outlook.com>
2019-02-04 13:21:36 -0200

    OA-112 - descrição do rest para extração

--- otus-business/src/main/java/br/org/otus/extraction/ExtractionFacade.java
+++ otus-business/src/main/java/br/org/otus/extraction/ExtractionFacade.java
@@ -11,34 +11,40 @@ import org.ccem.otus.service.extraction.SurveyActivityExtraction;
 import org.ccem.otus.survey.form.SurveyForm;
 
 import br.org.otus.api.ExtractionService;
+import br.org.otus.examUploader.api.ExamUploadFacade;
 import br.org.otus.survey.activity.api.ActivityFacade;
 import br.org.otus.survey.api.SurveyFacade;
 
 public class ExtractionFacade {
 
-	@Inject
-	private ActivityFacade activityFacade;
+  @Inject
+  private ActivityFacade activityFacade;
+  @Inject
+  private ExamUploadFacade examUploadFacade;
+  @Inject
+  private SurveyFacade surveyFacade;
 
-	@Inject
-	private SurveyFacade surveyFacade;
+  @Inject
+  private ExtractionService extractionService;
 
-	@Inject
-	private ExtractionService extractionService;
+  public byte[] createActivityExtraction(String acronym, Integer version) throws DataNotFoundException {
+    List<SurveyActivity> activities = new ArrayList<>();
 
-	public byte[] createActivityExtraction(String acronym, Integer version) throws DataNotFoundException {
-		List<SurveyActivity> activities = new ArrayList<>();
-		
-		activities = activityFacade.get(acronym, version);
-		SurveyForm surveyForm = surveyFacade.get(acronym, version);
-		SurveyActivityExtraction extractor = new SurveyActivityExtraction(surveyForm, activities);
-		try {
-			return extractionService.createExtraction(extractor);
-		} catch (DataNotFoundException e) {
-			throw new DataNotFoundException(new Throwable("RESULTS TO EXTRACTION {" + acronym + "} not found."));
-		}
-	}
-
-	public List<Integer> listSurveyVersions(String acronym){
-        return surveyFacade.listVersions(acronym);
+    activities = activityFacade.get(acronym, version);
+    SurveyForm surveyForm = surveyFacade.get(acronym, version);
+    SurveyActivityExtraction extractor = new SurveyActivityExtraction(surveyForm, activities);
+    try {
+      return extractionService.createExtraction(extractor);
+    } catch (DataNotFoundException e) {
+      throw new DataNotFoundException(new Throwable("RESULTS TO EXTRACTION {" + acronym + "} not found."));
     }
+  }
+
+  public List<Integer> listSurveyVersions(String acronym) {
+    return surveyFacade.listVersions(acronym);
+  }
+
+  public byte[] extractionOfCorrelationBetweenExamAliquotAndTube() {
+    return null;
+  }
 }
