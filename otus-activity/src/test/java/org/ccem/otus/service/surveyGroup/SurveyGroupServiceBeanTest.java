5cfa90fb43deb72ce39e8b5cf404802d935046fd
Fabiano Ramires <fdrtec@gmail.com>
2019-03-04 23:35:28 -0300

    OA-59 3 unit tests que lancam exceções de dentro de metodos privados

--- otus-activity/src/test/java/org/ccem/otus/service/surveyGroup/SurveyGroupServiceBeanTest.java
+++ otus-activity/src/test/java/org/ccem/otus/service/surveyGroup/SurveyGroupServiceBeanTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,6 +31,9 @@ public class SurveyGroupServiceBeanTest {
     private static final String USER_EMAIL = "otus@otus.com";
     private static final String SURVEY_GROUP_ID = "5c7400d2d767afded0d84dcf";
     private static final ObjectId EXPECTED_ID = new ObjectId(SURVEY_GROUP_ID);
+    private static final String EMPTY_JSON = "";
+    private static final String SURVEY_GROUP_NAME = "CI";
+
     @InjectMocks
     private SurveyGroupServiceBean surveyGroupServiceBean = PowerMockito.spy(new SurveyGroupServiceBean());
     @Mock
@@ -37,7 +41,6 @@ public class SurveyGroupServiceBeanTest {
     private SurveyGroup surveyGroup;
     private List<SurveyGroup> surveyGroups;
     private String surveyGroupJson;
-
 
     @Before
     public void setUp() throws Exception {
@@ -48,19 +51,39 @@ public class SurveyGroupServiceBeanTest {
     }
 
     @Test
-    public void getListOfSurveyGroups() {
+    public void getListOfSurveyGroupsMethod_should_return_list_SurveyGroups() {
         when(surveyGroupDao.getListOfSurveyGroups()).thenReturn(surveyGroups);
         assertEquals(EXPECTED_SIZE, surveyGroupServiceBean.getListOfSurveyGroups().size());
     }
 
     @Test
-    public void addNewGroup() throws Exception {
+    public void addNewGroup_should_return_id_in_case_sucess_persist() throws Exception {
         mockStatic(SurveyGroup.class);
         when(SurveyGroup.deserialize(surveyGroupJson)).thenReturn(surveyGroup);
         when(surveyGroupDao.persist(surveyGroup)).thenReturn(new ObjectId(SURVEY_GROUP_ID));
         assertEquals(EXPECTED_ID, surveyGroupServiceBean.addNewGroup(surveyGroupJson));
-        verifyPrivate(surveyGroupServiceBean, times(1)).invoke("verifySurveyGroupNameValid",surveyGroup);
+        verifyPrivate(surveyGroupServiceBean, times(1)).invoke("verifySurveyGroupNameValid", surveyGroup);
         verifyPrivate(surveyGroupServiceBean, times(1)).invoke("verifySurveyGroupNameConflits", surveyGroup.getName());
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addNewGroupMethod_with_paramenter_empty_should_throws_ValidationException() throws Exception {
+        surveyGroupServiceBean.addNewGroup(EMPTY_JSON);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addNewGroupMethod_with_surveyGroupName_invalid_should_throws_ValidationException() throws Exception {
+        mockStatic(SurveyGroup.class);
+        when(SurveyGroup.deserialize(surveyGroupJson)).thenReturn(surveyGroup);
+        surveyGroup.setName("ci");
+        surveyGroupServiceBean.addNewGroup(surveyGroupJson);
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addNewGroupMethod_with_surveyGroupName_existence_should_throws_ValidationException() throws Exception {
+        Whitebox.invokeMethod(surveyGroupServiceBean, "verifySurveyGroupNameConflits", SURVEY_GROUP_NAME);
+        PowerMockito.doThrow(new ValidationException()).when(surveyGroupDao, "findSurveyGroupNameConflits", SURVEY_GROUP_NAME);
+        surveyGroupServiceBean.addNewGroup(surveyGroupJson);
     }
 
     @Test
