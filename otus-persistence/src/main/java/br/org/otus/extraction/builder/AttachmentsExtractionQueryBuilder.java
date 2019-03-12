28e523411477a1ce0fd99aa1f720ba5e519211d5
Adriano <desenvacc@gmail.com>
2019-03-12 19:32:23 -0300

    OA-121 query alterada para retornar custom ID

--- otus-persistence/src/main/java/br/org/otus/extraction/builder/AttachmentsExtractionQueryBuilder.java
+++ otus-persistence/src/main/java/br/org/otus/extraction/builder/AttachmentsExtractionQueryBuilder.java
@@ -25,12 +25,14 @@ public class AttachmentsExtractionQueryBuilder {
 
     private ArrayList<Bson> getBsons(String acronym, Integer version, ArrayList<ObjectId> objectIds, String state, String statueSubtitle) {
         pipeline.add(parseQuery("{$match:{\"surveyForm.surveyTemplate.identity.acronym\":"+acronym+",\"surveyForm.version\":"+version+",\"isDiscarded\":false}}"));
-        pipeline.add(parseQuery("{$project:{\"participantData.recruitmentNumber\":1,\"fillContainer\":1,\"surveyForm.surveyTemplate.identity.acronym\":1}}"));
+        pipeline.add(parseQuery("{\"$addFields\":{\"itemContainer\":\"$surveyForm.surveyTemplate.itemContainer\"}}"));
+        pipeline.add(parseQuery("{\"$project\":{\"itemContainer\":1,\"participantData.recruitmentNumber\":1.0,\"fillContainer\":1.0}}"));
         pipeline.add(parseQuery("{$unwind:\"$fillContainer.fillingList\"}"));
         pipeline.add(parseQuery("{$match:{\"fillContainer.fillingList.answer.type\":\"FileUploadQuestion\"}}"));
         pipeline.add(parseQuery("{$unwind:\"$fillContainer.fillingList.answer.value\"}"));
         pipeline.add(parseQuery("{$match:{\"fillContainer.fillingList.answer.value.oid\":{"+state+objectIds+"}}}"));
-        pipeline.add(parseQuery("{$group:{_id:{recruitmentNumber:\"$participantData.recruitmentNumber\",questionId:\"$fillContainer.fillingList.questionID\", archiveId:\"$fillContainer.fillingList.answer.value.oid\",archiveName:\"$fillContainer.fillingList.answer.value.name\",status:\"$status\",uploadDate:\"$fillContainer.fillingList.answer.value.sentDate\"}}}"));
+        pipeline.add(parseQuery("{\"$addFields\":{\"questionFound\":{\"$filter\":{\"input\":\"$itemContainer\",\"as\":\"item\",\"cond\":{\"$eq\":[\"$$item.templateID\",\"$fillContainer.fillingList.questionID\"]}}}}}"));
+        pipeline.add(parseQuery("{\"$group\":{\"_id\":{\"recruitmentNumber\":\"$participantData.recruitmentNumber\",\"questionId\":{\"$arrayElemAt\": [ {\"$slice\": [ \"$questionFound.customID\",-1.0] }, 0.0]},\"archiveId\":\"$fillContainer.fillingList.answer.value.oid\",\"archiveName\":\"$fillContainer.fillingList.answer.value.name\",\"status\":\"$status\",\"uploadDate\":\"$fillContainer.fillingList.answer.value.sentDate\"}}}"));
         pipeline.add(parseQuery("{\"$group\":{\"_id\":{},\"attachmentsList\":{\"$push\":{\"recruitmentNumber\":\"$_id.recruitmentNumber\",\"questionId\":\"$_id.questionId\",\"archiveId\":\"$_id.archiveId\",\"status\":"+statueSubtitle+",\"archiveName\":\"$_id.archiveName\",\"uploadDate\":\"$_id.uploadDate\"}}}}"));
         return pipeline;
     }
