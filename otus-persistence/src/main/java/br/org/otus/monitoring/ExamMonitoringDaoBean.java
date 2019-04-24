382790d43ce9401fc1f287e8c44e89cd1ea64d2a
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-04-24 13:22:06 -0300

    OA-128 refatoração da query resultado de exames e teste de unidade

--- otus-persistence/src/main/java/br/org/otus/monitoring/ExamMonitoringDaoBean.java
+++ otus-persistence/src/main/java/br/org/otus/monitoring/ExamMonitoringDaoBean.java
@@ -2,16 +2,16 @@ package br.org.otus.monitoring;
 
 import br.org.mongodb.MongoGenericDao;
 import br.org.otus.laboratory.configuration.LaboratoryConfigurationDao;
-import com.google.gson.GsonBuilder;
+import br.org.otus.monitoring.builder.ExamStatusQueryBuilder;
 import com.mongodb.client.MongoCursor;
 import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.ccem.otus.model.monitoring.ParticipantExamReportDto;
 import org.ccem.otus.persistence.ExamMonitoringDao;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.List;
 
 @Stateless
 public class ExamMonitoringDaoBean extends MongoGenericDao<Document> implements ExamMonitoringDao {
@@ -26,88 +26,9 @@ public class ExamMonitoringDaoBean extends MongoGenericDao<Document> implements 
 
     @Override
     public ArrayList<ParticipantExamReportDto> getParticipantExams(Long rn) {
+        List<String> examName = laboratoryConfigurationDao.getExamName();
 
-        ArrayList<Bson> pipeline = new ArrayList<Bson>();
-        pipeline.add(parseQuery("{$match:{\"recruitmentNumber\":"+rn+"}}"));
-        pipeline.add(parseQuery("{\n" +
-                "      $group:{\n" +
-                "          _id:{examName:\"$examName\",examId:\"$examId\"}\n" +
-                "      }  \n" +
-                "    }"));
-        pipeline.add(parseQuery(" {\n" +
-                "        $group:{\n" +
-                "            _id:{examName:\"$_id.examName\"},\n" +
-                "            quantity:{$sum:1}\n" +
-                "        }\n" +
-                "    }"));
-        pipeline.add(parseQuery("  {\n" +
-                "        $group:{\n" +
-                "            _id:{},\n" +
-                "            exams:{$push:{name:\"$_id.examName\",quantity:\"$quantity\"}}\n" +
-                "        }\n" +
-                "    }"));
-        pipeline.add(new Document("$addFields", new Document("allExams", laboratoryConfigurationDao.getExamName())));
-        pipeline.add(parseQuery("{$unwind:\"$allExams\"}"));
-        pipeline.add(parseQuery("{\n" +
-                "        $addFields:{\n" +
-                "            \n" +
-                "            examFound:{\n" +
-                "                $arrayElemAt:[\n" +
-                "                    {$filter:{\n" +
-                "                        input:\"$exams\",\n" +
-                "                        as:\"exam\",\n" +
-                "                        cond:{\n" +
-                "                            $eq: [\n" +
-                "                                    \"$$exam.name\",\n" +
-                "                                    \"$allExams\"\n" +
-                "                                ]\n" +
-                "                        }\n" +
-                "                    }},0\n" +
-                "                ]\n" +
-                "            }\n" +
-                "        }\n" +
-                "    }"));
-        pipeline.add(parseQuery("{\n" +
-                "        $project:{\n" +
-                "            name: \"$allExams\",\n" +
-                "            quantity:{$cond:[{$ifNull:[\"$examFound\",false]},\"$examFound.quantity\",0]}\n" +
-                "        }\n" +
-                "    }"));
-        pipeline.add(parseQuery("{\n" +
-                "        $lookup:{\n" +
-                "            from:\"exam_inapplicability\",\n" +
-                "            let:{name:\"$name\",rn:\"$recruitmentNumber\"},\n" +
-                "            pipeline:[\n" +
-                "                {\n" +
-                "                    $match:{\n" +
-                "                        $expr:{\n" +
-                "                            $and:[\n" +
-                "                                {$eq:[\"$name\",\"$$name\"]},\n" +
-                "                                {$eq:[\"$recruitmentNumber\","+rn+"]}\n" +
-                "                            ]\n" +
-                "                        }\n" +
-                "                    }\n" +
-                "                },\n" +
-                "                {\n" +
-                "                    $project:{\n" +
-                "                            \"_id\":0,\n" +
-                "                            \"name\":0,\n"+
-                "                            \"recruitmentNumber\":0\n"+
-                "                        }  \n" +
-                "                }\n" +
-                "                ],\n" +
-                "            as:\"examInapplicability\"\n" +
-                "        }\n" +
-                "    }"));
-        pipeline.add(parseQuery("{\n" +
-                "        $project:{\n" +
-                "            \"name\":1,\n" +
-                "            \"quantity\":1,\n" +
-                "            \"doesNotApply\":{$arrayElemAt: [\"$examInapplicability\",0]}\n" +
-                "        }\n" +
-                "    }"));
-
-        MongoCursor<Document> resultsDocument = super.aggregate(pipeline).iterator();
+        MongoCursor<Document> resultsDocument = super.aggregate(new ExamStatusQueryBuilder().getExamQuery(rn, examName)).iterator();
 
         ArrayList<ParticipantExamReportDto> dtos = new ArrayList<>();
 
@@ -120,10 +41,6 @@ public class ExamMonitoringDaoBean extends MongoGenericDao<Document> implements 
         }
 
         return dtos;
-    }
-
-    private Bson parseQuery(String stage) {
-        return new GsonBuilder().create().fromJson(stage, Document.class);
     }
 
 }
---
+++ otus-persistence/src/main/java/br/org/otus/monitoring/builder/ExamStatusQueryBuilder.java
@@ -0,0 +1,104 @@
+package br.org.otus.monitoring.builder;
+
+import com.google.gson.GsonBuilder;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExamStatusQueryBuilder {
+
+    private ArrayList<Bson> pipeline;
+
+    public ExamStatusQueryBuilder() {
+        this.pipeline = new ArrayList<>();
+    }
+
+    public List<Bson> getExamQuery(Long rn, List<String> examName) {
+        pipeline.add(parseQuery("{$match:{\"recruitmentNumber\":"+rn+"}}"));
+        pipeline.add(parseQuery("{\n" +
+                "      $group:{\n" +
+                "          _id:{examName:\"$examName\",examId:\"$examId\"}\n" +
+                "      }  \n" +
+                "    }"));
+        pipeline.add(parseQuery(" {\n" +
+                "        $group:{\n" +
+                "            _id:{examName:\"$_id.examName\"},\n" +
+                "            quantity:{$sum:1}\n" +
+                "        }\n" +
+                "    }"));
+        pipeline.add(parseQuery("  {\n" +
+                "        $group:{\n" +
+                "            _id:{},\n" +
+                "            exams:{$push:{name:\"$_id.examName\",quantity:\"$quantity\"}}\n" +
+                "        }\n" +
+                "    }"));
+        pipeline.add(new Document("$addFields", new Document("allExams", examName)));
+        pipeline.add(parseQuery("{$unwind:\"$allExams\"}"));
+        pipeline.add(parseQuery("{\n" +
+                "        $addFields:{\n" +
+                "            \n" +
+                "            examFound:{\n" +
+                "                $arrayElemAt:[\n" +
+                "                    {$filter:{\n" +
+                "                        input:\"$exams\",\n" +
+                "                        as:\"exam\",\n" +
+                "                        cond:{\n" +
+                "                            $eq: [\n" +
+                "                                    \"$$exam.name\",\n" +
+                "                                    \"$allExams\"\n" +
+                "                                ]\n" +
+                "                        }\n" +
+                "                    }},0\n" +
+                "                ]\n" +
+                "            }\n" +
+                "        }\n" +
+                "    }"));
+        pipeline.add(parseQuery("{\n" +
+                "        $project:{\n" +
+                "            name: \"$allExams\",\n" +
+                "            quantity:{$cond:[{$ifNull:[\"$examFound\",false]},\"$examFound.quantity\",0]}\n" +
+                "        }\n" +
+                "    }"));
+        pipeline.add(parseQuery("{\n" +
+                "        $lookup:{\n" +
+                "            from:\"exam_inapplicability\",\n" +
+                "            let:{name:\"$name\",rn:\"$recruitmentNumber\"},\n" +
+                "            pipeline:[\n" +
+                "                {\n" +
+                "                    $match:{\n" +
+                "                        $expr:{\n" +
+                "                            $and:[\n" +
+                "                                {$eq:[\"$name\",\"$$name\"]},\n" +
+                "                                {$eq:[\"$recruitmentNumber\","+rn+"]}\n" +
+                "                            ]\n" +
+                "                        }\n" +
+                "                    }\n" +
+                "                },\n" +
+                "                {\n" +
+                "                    $project:{\n" +
+                "                            \"_id\":0,\n" +
+                "                            \"name\":0,\n"+
+                "                            \"recruitmentNumber\":0\n"+
+                "                        }  \n" +
+                "                }\n" +
+                "                ],\n" +
+                "            as:\"examInapplicability\"\n" +
+                "        }\n" +
+                "    }"));
+        pipeline.add(parseQuery("{\n" +
+                "        $project:{\n" +
+                "            \"name\":1,\n" +
+                "            \"quantity\":1,\n" +
+                "            \"doesNotApply\":{$arrayElemAt: [\"$examInapplicability\",0]}\n" +
+                "        }\n" +
+                "    }"));
+        return this.pipeline;
+    }
+
+    private Document parseQuery(String query) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        return gsonBuilder.create().fromJson(query, Document.class);
+    }
+}
