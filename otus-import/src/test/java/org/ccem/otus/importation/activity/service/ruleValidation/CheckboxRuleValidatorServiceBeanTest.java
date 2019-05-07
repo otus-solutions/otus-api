58fb8a8bdfd4e6dd98b7b116570af1e600c37c41
Fabiano Ramires <fdrtec@gmail.com>
2019-05-07 16:21:48 -0300

    OA-127 - 10 unitTests para serviceBean de checkboxRuleValidator

---
+++ otus-import/src/test/java/org/ccem/otus/importation/activity/service/ruleValidation/CheckboxRuleValidatorServiceBeanTest.java
@@ -0,0 +1,123 @@
+package org.ccem.otus.importation.activity.service.ruleValidation;
+
+import org.ccem.otus.model.survey.activity.SurveyActivity;
+import org.ccem.otus.model.survey.activity.filling.AnswerFill;
+import org.ccem.otus.model.survey.activity.filling.answer.CheckboxAnswer;
+import org.ccem.otus.model.survey.activity.filling.answer.CheckboxAnswerOption;
+import org.ccem.otus.survey.template.navigation.route.Rule;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.omg.CORBA.PolicyError;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.spy;
+
+@RunWith(PowerMockRunner.class)
+public class CheckboxRuleValidatorServiceBeanTest {
+
+    private CheckboxRuleValidatorServiceBean checkboxRuleValidatorServiceBean = new CheckboxRuleValidatorServiceBean();
+    @Spy
+    CheckboxAnswer answer = new CheckboxAnswer();
+    @Spy
+    CheckboxAnswerOption checkboxAnswerOption = new CheckboxAnswerOption();
+    List<CheckboxAnswerOption> checkboxAnswerOptions = new ArrayList();
+    Rule rule;
+
+    @Before
+    public void setUp() throws Exception {
+        rule = new Rule();
+        rule.extents = "SurveyTemplateObject";
+        rule.objectType = "Rule";
+        rule.when = "TST1";
+        rule.isMetadata = false;
+        Whitebox.setInternalState(checkboxAnswerOption, "option", "B");
+        Whitebox.setInternalState(checkboxAnswerOption, "state", true);
+        checkboxAnswerOptions.add(checkboxAnswerOption);
+        Whitebox.setInternalState(answer, "objectType", "AnswerFill");
+        Whitebox.setInternalState(answer, "type", "CheckboxQuestion");
+        Whitebox.setInternalState(answer, "value", checkboxAnswerOptions);
+    }
+
+    @Test
+    public void run_method_should_deliver_positive_results_in_the_event_that_isEqual_has_any_items_on_the_list() {
+        rule.operator = "equal";
+        rule.answer = "B";
+        Assert.assertTrue(checkboxRuleValidatorServiceBean.run(rule, answer));
+        Mockito.verify(checkboxAnswerOption, Mockito.times(1)).getOption();
+        Mockito.verify(checkboxAnswerOption, Mockito.times(1)).getState();
+    }
+
+    @Test
+    public void run_method_should_deliver_negative_results_in_the_event_that_isEqual_not_has_any_items_on_the_list() {
+        rule.operator = "equal";
+        rule.answer = "A";
+        Assert.assertFalse(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_positive_results_in_the_event_that_notEqual_has_any_items_on_the_list() {
+        rule.operator = "notEqual";
+        rule.answer = "A";
+        Assert.assertTrue(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_negative_results_in_the_event_that_notEqual_not_has_any_items_on_the_list() {
+        rule.operator = "notEqual";
+        rule.answer = "B";
+        Assert.assertFalse(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_positive_results_in_the_event_that_quantity_has_any_items_on_the_list() {
+        rule.operator = "quantity";
+        rule.answer = String.valueOf(1);
+        Assert.assertTrue(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_negative_results_in_the_event_that_quantity__not_has_any_items_on_the_list() {
+        rule.operator = "quantity";
+        rule.answer = String.valueOf(0);
+        Assert.assertFalse(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+        @Test
+    public void run_method_should_deliver_positive_results_in_the_event_that_minSelected_has_any_items_on_the_list() {
+        rule.operator = "minSelected";
+        rule.answer = "1";
+        Assert.assertTrue(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_negative_results_in_the_event_that_minSelected_not_has_any_items_on_the_list() {
+        rule.operator = "minSelected";
+        rule.answer = "2";
+        Assert.assertFalse(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_positive_results_in_the_event_that_maxSelected_has_any_items_on_the_list() {
+        rule.operator = "maxSelected";
+        rule.answer = "1";
+        Assert.assertTrue(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test
+    public void run_method_should_deliver_negative_results_in_the_event_that_maxSelected_not_has_any_items_on_the_list() {
+        rule.operator = "maxSelected";
+        rule.answer = String.valueOf(0);
+        Assert.assertFalse(checkboxRuleValidatorServiceBean.run(rule, answer));
+    }
+}
