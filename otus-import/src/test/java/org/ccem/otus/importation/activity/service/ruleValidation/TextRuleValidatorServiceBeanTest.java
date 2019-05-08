90b52b3f2f77716f22c237c923d250d34ed00121
Fabiano Ramires <fdrtec@gmail.com>
2019-05-08 13:24:36 -0300

    OA-127 sincronização e refatorção com nova inserção de exception

--- otus-import/src/test/java/org/ccem/otus/importation/activity/service/ruleValidation/TextRuleValidatorServiceBeanTest.java
+++ otus-import/src/test/java/org/ccem/otus/importation/activity/service/ruleValidation/TextRuleValidatorServiceBeanTest.java
@@ -1,5 +1,6 @@
 package org.ccem.otus.importation.activity.service.ruleValidation;
 
+import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
 import org.ccem.otus.model.survey.activity.filling.answer.TextAnswer;
 import org.ccem.otus.survey.template.navigation.route.Rule;
 import org.junit.Before;
@@ -32,44 +33,50 @@ public class TextRuleValidatorServiceBeanTest {
     }
 
     @Test
-    public void run_method_with_equalCase_should_validate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_equalCase_should_validate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "equal";
         rule.answer = "Otus";
         assertTrue(textRuleValidatorServiceBean.run(rule, answer));
     }
 
     @Test
-    public void run_method_with_equalCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_equalCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "equal";
         rule.answer = "otus";
         assertFalse(textRuleValidatorServiceBean.run(rule, answer));
     }
 
     @Test
-    public void run_method_with_notEqualCase_should_validate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_notEqualCase_should_validate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "notEqual";
         rule.answer = "otus";
         assertTrue(textRuleValidatorServiceBean.run(rule, answer));
     }
 
     @Test
-    public void run_method_with_notEqualCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_notEqualCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "notEqual";
         rule.answer = "Otus";
         assertFalse(textRuleValidatorServiceBean.run(rule, answer));
     }
 
     @Test
-    public void run_method_with_greaterCase_should_validate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_greaterCase_should_validate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "greater";
         rule.answer = "Otu";
         assertTrue(textRuleValidatorServiceBean.run(rule, answer));
     }
 
     @Test
-    public void run_method_with_greaterCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() {
+    public void run_method_with_greaterCase_should_invalidate_the_comparison_of_values_between_answer_and_rule() throws DataNotFoundException {
         rule.operator = "greater";
         rule.answer = "otu";
         assertFalse(textRuleValidatorServiceBean.run(rule, answer));
+    }
+
+    @Test(expected = DataNotFoundException.class)
+    public void run_method_with_invalidCase_should_throws_DataNotFoundException() throws DataNotFoundException {
+        rule.operator = "anything";
+        textRuleValidatorServiceBean.run(rule, answer);
     }
 }
