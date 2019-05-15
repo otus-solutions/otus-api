55e059ef059f987b72360c6171b3732f82db29bb
Fabiano Ramires <fdrtec@gmail.com>
2019-05-15 12:28:48 -0300

    OA-132 Implementação da query de update da aliquota

--- otus-persistence/src/main/java/br/org/otus/laboratory/participant/AliquotDaoBean.java
+++ otus-persistence/src/main/java/br/org/otus/laboratory/participant/AliquotDaoBean.java
@@ -5,9 +5,12 @@ import br.org.otus.laboratory.participant.aliquot.Aliquot;
 import br.org.otus.laboratory.participant.aliquot.persistence.AliquotDao;
 import br.org.otus.laboratory.participant.dto.ConvertAliquotRoleDTO;
 import br.org.otus.laboratory.project.transportation.persistence.TransportationAliquotFiltersDTO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mongodb.Block;
 import com.mongodb.client.ClientSession;
 import com.mongodb.client.FindIterable;
+import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.result.UpdateResult;
@@ -15,6 +18,8 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,163 +28,162 @@ import java.util.List;
 import static com.mongodb.client.model.Filters.*;
 
 public class AliquotDaoBean extends MongoGenericDao<Document> implements AliquotDao {
-  private static final String COLLECTION_NAME = "aliquot";
+    private static final String COLLECTION_NAME = "aliquot";
 
-  private static final String EXAM = "EXAM";
-  private static final String STORAGE = "STORAGE";
+    private static final String EXAM = "EXAM";
+    private static final String STORAGE = "STORAGE";
 
-  public AliquotDaoBean() {
-    super(COLLECTION_NAME, Document.class);
-  }
-
-  @Override
-  public List<Aliquot> getAliquots() {
-    List<Aliquot> aliquots = new ArrayList<>();
-
-    FindIterable<Document> result = collection.find();
-    result.forEach((Block<Document>) document -> aliquots.add(Aliquot.deserialize(document.toJson())));
-
-    return aliquots;
-  }
-
-  @Override
-  public void persist(Aliquot aliquot) {
-    super.persist(Aliquot.serialize(aliquot));
-  }
-
-  @Override
-  public List<Aliquot> list(Long recruitmentNumber) {
-    MongoCursor<Document> cursor = collection.find(eq("recruitmentNumber", recruitmentNumber)).iterator();
-    ArrayList<Aliquot> aliquots = new ArrayList<>();
-
-    try {
-      while (cursor.hasNext()) {
-        aliquots.add(Aliquot.deserialize(cursor.next().toJson()));
-      }
-    } finally {
-      cursor.close();
+    public AliquotDaoBean() {
+        super(COLLECTION_NAME, Document.class);
     }
 
-    return aliquots;
-  }
+    @Override
+    public List<Aliquot> getAliquots() {
+        List<Aliquot> aliquots = new ArrayList<>();
 
-  @Override
-  public boolean exists(String code) {
-    Document first = collection.find(eq("code", code)).first();
-    if (first != null) {
-      return true;
-    }
-    return false;
+        FindIterable<Document> result = collection.find();
+        result.forEach((Block<Document>) document -> aliquots.add(Aliquot.deserialize(document.toJson())));
 
-  }
-
-  @Override
-  public void delete(String code) throws DataNotFoundException {
-    DeleteResult deleteResult = collection.deleteOne(new Document("code", code));
-
-    if(deleteResult.getDeletedCount() == 0){
-      throw new DataNotFoundException(new Throwable("Aliquot code not found."), code);
-    }
-  }
-
-  @Override
-  public Aliquot find(String code) throws DataNotFoundException {
-    Document result = collection.find(eq("code", code)).first();
-    if(result == null ){
-      throw new DataNotFoundException(new Throwable("Aliquot not found"));
-    }
-    return Aliquot.deserialize(result.toJson());
-  }
-
-  @Override
-  public List<Aliquot> getExamLotAliquots(ObjectId lotOId) {
-    FindIterable<Document> documents = collection.find(new Document("$or", Arrays.asList(new Document("examLotData.id",lotOId),new Document("examLotId",lotOId)))).sort(new Document("examLotData.position",1));
-
-    ArrayList<Aliquot> aliquotList = new ArrayList<>();
-    for (Object oneDocument : documents) {
-      Document next = (Document) oneDocument;
-      aliquotList.add(Aliquot.deserialize(next.toJson()));
+        return aliquots;
     }
 
-    return aliquotList;
-  }
-
-  @Override
-  public void updateExamLotId(ArrayList<String> codeList, ObjectId lotId) throws DataNotFoundException {
-    collection.updateMany(new Document("$or", Arrays.asList(new Document("examLotData.id",lotId),new Document("examLotId",lotId))), new Document("$set", new Document("examLotId", null).append("examLotData",null)));
-    if (!codeList.isEmpty()) {
-      codeList.forEach(aliquotCode -> collection.updateOne(new Document("code",aliquotCode), new Document("$set", new Document("examLotData", new Document("id", lotId).append("position",codeList.indexOf(aliquotCode))))));
-    }
-  }
-
-  @Override
-  public Aliquot getAliquot(TransportationAliquotFiltersDTO transportationAliquotFiltersDTO) throws DataNotFoundException {
-    Document document = collection.find(eq("code", transportationAliquotFiltersDTO.getCode())).first();
-    if (document == null) {
-      throw new DataNotFoundException(
-        new Throwable("Aliquot not found."));
-    }
-    Aliquot aliquot = Aliquot.deserialize(document.toJson());
-    return aliquot;
-  }
-
-  @Override
-  public List<Aliquot> getAliquotsByPeriod(TransportationAliquotFiltersDTO transportationAliquotFiltersDTO) {
-    List<Aliquot> aliquots = new ArrayList<>();
-
-    ArrayList<String> roles = new ArrayList<>();
-    roles.add(EXAM);
-
-    if (transportationAliquotFiltersDTO.getRole().equals(STORAGE)){
-      roles.add(transportationAliquotFiltersDTO.getRole());
+    @Override
+    public void persist(Aliquot aliquot) {
+        super.persist(Aliquot.serialize(aliquot));
     }
 
-    FindIterable<Document> result = collection.find(
-      and (
-        gte("aliquotCollectionData.processing", transportationAliquotFiltersDTO.getInitialDate()),
-        lte("aliquotCollectionData.processing", transportationAliquotFiltersDTO.getFinalDate()),
-        eq("fieldCenter.acronym", transportationAliquotFiltersDTO.getFieldCenter()),
-        eq("transportationLotId", null),
-        in("role", roles)
-        )
-    );
+    @Override
+    public List<Aliquot> list(Long recruitmentNumber) {
+        MongoCursor<Document> cursor = collection.find(eq("recruitmentNumber", recruitmentNumber)).iterator();
+        ArrayList<Aliquot> aliquots = new ArrayList<>();
 
-    result.forEach((Block<Document>) document -> aliquots.add(Aliquot.deserialize(document.toJson())));
-    return aliquots;
-  }
+        try {
+            while (cursor.hasNext()) {
+                aliquots.add(Aliquot.deserialize(cursor.next().toJson()));
+            }
+        } finally {
+            cursor.close();
+        }
 
-  @Override
-  public void addToTransportationLot(ArrayList<String> codeList, ObjectId transportationLotId) throws DataNotFoundException {
-    Document query = new Document("code",new Document("$in", codeList));
-    UpdateResult updateManyResult = collection.updateMany(query,new Document("$set", new Document("transportationLotId", transportationLotId)));
-
-    if (updateManyResult.getMatchedCount() == 0) {
-      throw new DataNotFoundException(new Throwable("Aliquot not found"));
+        return aliquots;
     }
-  }
 
-  @Override
-  public void updateTransportationLotId(ArrayList<String> codeList, ObjectId loId) throws DataNotFoundException {
-    Document query = new Document("code", new Document("$in", codeList));
+    @Override
+    public boolean exists(String code) {
+        Document first = collection.find(eq("code", code)).first();
+        if (first != null) {
+            return true;
+        }
+        return false;
 
-    collection.updateMany(new Document("transportationLotId", loId), new Document("$set", new Document("transportationLotId", null)));
-    if (!codeList.isEmpty()) {
-      UpdateResult updateManyResult = collection.updateMany(query, new Document("$set", new Document("transportationLotId", loId)));
-      if (updateManyResult.getMatchedCount() != codeList.size()) {
-        throw new DataNotFoundException(new Throwable("aliquots not found"));
-      }
     }
-  }
 
-  @Override
-  public String convertAliquotRole(ConvertAliquotRoleDTO convertAliquotRoleDTO) {
-    Bson filter = new Document("code", convertAliquotRoleDTO.getAliquotCode());
-    Bson convert = new Document("role", convertAliquotRoleDTO.getRole());
-    Bson aliquotHistory = new Document("aliquotHistory", convertAliquotRoleDTO.getAliquotHistories());
-    //Bson updateOperation= new Document("$set", convert).append("$set", aliquotHistory);
-    Bson updateOperation= new Document("$set", convert);
+    @Override
+    public void delete(String code) throws DataNotFoundException {
+        DeleteResult deleteResult = collection.deleteOne(new Document("code", code));
 
-    UpdateResult result = collection.updateOne(filter, updateOperation);
-    return String.valueOf(result.getModifiedCount());
-  }
+        if (deleteResult.getDeletedCount() == 0) {
+            throw new DataNotFoundException(new Throwable("Aliquot code not found."), code);
+        }
+    }
+
+    @Override
+    public Aliquot find(String code) throws DataNotFoundException {
+        Document result = collection.find(eq("code", code)).first();
+        if (result == null) {
+            throw new DataNotFoundException(new Throwable("Aliquot not found"));
+        }
+        return Aliquot.deserialize(result.toJson());
+    }
+
+    @Override
+    public List<Aliquot> getExamLotAliquots(ObjectId lotOId) {
+        FindIterable<Document> documents = collection.find(new Document("$or", Arrays.asList(new Document("examLotData.id", lotOId), new Document("examLotId", lotOId)))).sort(new Document("examLotData.position", 1));
+
+        ArrayList<Aliquot> aliquotList = new ArrayList<>();
+        for (Object oneDocument : documents) {
+            Document next = (Document) oneDocument;
+            aliquotList.add(Aliquot.deserialize(next.toJson()));
+        }
+
+        return aliquotList;
+    }
+
+    @Override
+    public void updateExamLotId(ArrayList<String> codeList, ObjectId lotId) throws DataNotFoundException {
+        collection.updateMany(new Document("$or", Arrays.asList(new Document("examLotData.id", lotId), new Document("examLotId", lotId))), new Document("$set", new Document("examLotId", null).append("examLotData", null)));
+        if (!codeList.isEmpty()) {
+            codeList.forEach(aliquotCode -> collection.updateOne(new Document("code", aliquotCode), new Document("$set", new Document("examLotData", new Document("id", lotId).append("position", codeList.indexOf(aliquotCode))))));
+        }
+    }
+
+    @Override
+    public Aliquot getAliquot(TransportationAliquotFiltersDTO transportationAliquotFiltersDTO) throws DataNotFoundException {
+        Document document = collection.find(eq("code", transportationAliquotFiltersDTO.getCode())).first();
+        if (document == null) {
+            throw new DataNotFoundException(
+                    new Throwable("Aliquot not found."));
+        }
+        Aliquot aliquot = Aliquot.deserialize(document.toJson());
+        return aliquot;
+    }
+
+    @Override
+    public List<Aliquot> getAliquotsByPeriod(TransportationAliquotFiltersDTO transportationAliquotFiltersDTO) {
+        List<Aliquot> aliquots = new ArrayList<>();
+
+        ArrayList<String> roles = new ArrayList<>();
+        roles.add(EXAM);
+
+        if (transportationAliquotFiltersDTO.getRole().equals(STORAGE)) {
+            roles.add(transportationAliquotFiltersDTO.getRole());
+        }
+
+        FindIterable<Document> result = collection.find(
+                and(
+                        gte("aliquotCollectionData.processing", transportationAliquotFiltersDTO.getInitialDate()),
+                        lte("aliquotCollectionData.processing", transportationAliquotFiltersDTO.getFinalDate()),
+                        eq("fieldCenter.acronym", transportationAliquotFiltersDTO.getFieldCenter()),
+                        eq("transportationLotId", null),
+                        in("role", roles)
+                )
+        );
+
+        result.forEach((Block<Document>) document -> aliquots.add(Aliquot.deserialize(document.toJson())));
+        return aliquots;
+    }
+
+    @Override
+    public void addToTransportationLot(ArrayList<String> codeList, ObjectId transportationLotId) throws DataNotFoundException {
+        Document query = new Document("code", new Document("$in", codeList));
+        UpdateResult updateManyResult = collection.updateMany(query, new Document("$set", new Document("transportationLotId", transportationLotId)));
+
+        if (updateManyResult.getMatchedCount() == 0) {
+            throw new DataNotFoundException(new Throwable("Aliquot not found"));
+        }
+    }
+
+    @Override
+    public void updateTransportationLotId(ArrayList<String> codeList, ObjectId loId) throws DataNotFoundException {
+        Document query = new Document("code", new Document("$in", codeList));
+
+        collection.updateMany(new Document("transportationLotId", loId), new Document("$set", new Document("transportationLotId", null)));
+        if (!codeList.isEmpty()) {
+            UpdateResult updateManyResult = collection.updateMany(query, new Document("$set", new Document("transportationLotId", loId)));
+            if (updateManyResult.getMatchedCount() != codeList.size()) {
+                throw new DataNotFoundException(new Throwable("aliquots not found"));
+            }
+        }
+    }
+
+    @Override
+    public String convertAliquotRole(ConvertAliquotRoleDTO convertAliquotRoleDTO) {
+        Bson filter = new Document("code", convertAliquotRoleDTO.getAliquotCode());
+        Bson convertHistory = Document.parse(new GsonBuilder().create()
+                .toJson(new Document("aliquotHistory", convertAliquotRoleDTO.getAliquotHistories()).append("role", convertAliquotRoleDTO.getRole())));
+        Bson updateOperation= new Document("$set", convertHistory);
+
+        UpdateResult result = collection.updateOne(filter, updateOperation);
+        return String.valueOf(result.getModifiedCount());
+    }
 }
