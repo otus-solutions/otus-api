75c58c5914c55f75385c0d8e0e3d132f6154ff6d
emanoelvianna <emanoel.vianna@outlook.com>
2019-05-17 17:26:55 -0300

    OA-133 - correção sobre problems retornados do code review

--- otus-rest/src/main/java/br/org/otus/laboratory/configuration/LaboratoryConfigurationResource.java
+++ otus-rest/src/main/java/br/org/otus/laboratory/configuration/LaboratoryConfigurationResource.java
@@ -1,55 +1,56 @@
 package br.org.otus.laboratory.configuration;
 
-import br.org.otus.laboratory.configuration.collect.aliquot.AliquotConfiguration;
-import br.org.otus.laboratory.configuration.collect.aliquot.AliquoteDescriptor;
-import br.org.otus.rest.Response;
-import br.org.otus.security.Secured;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
-import java.util.List;
+
+import br.org.otus.laboratory.configuration.collect.aliquot.AliquotConfiguration;
+import br.org.otus.laboratory.configuration.collect.aliquot.AliquoteDescriptor;
+import br.org.otus.rest.Response;
+import br.org.otus.security.Secured;
 
 @Path("/laboratory-configuration")
 public class LaboratoryConfigurationResource {
 
-	@Inject
-	private LaboratoryConfigurationService laboratoryConfigurationService;
+  @Inject
+  private LaboratoryConfigurationService laboratoryConfigurationService;
 
-	@GET
-	@Secured
-    @Path("/exists")
-	public boolean existsLaboratoryConfiguration() {
-	  return laboratoryConfigurationService.existsLaboratoryConfiguration();
-	}
-	
-	@GET
-	@Secured
-	@Path("/descriptor")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String getDescriptor() {
-		LaboratoryConfiguration laboratoryConfiguration = laboratoryConfigurationService.getLaboratoryConfiguration();
-		LaboratoryConfigurationDTO laboratoryConfigurationDTO = new LaboratoryConfigurationDTO(laboratoryConfiguration);
-		return new Response().buildSuccess(laboratoryConfigurationDTO).toJson();
-	}
+  @GET
+  @Secured
+  @Path("/exists")
+  public boolean existsLaboratoryConfiguration() {
+    return laboratoryConfigurationService.existsLaboratoryConfiguration();
+  }
 
-	@GET
-	@Secured
-	@Path("/aliquot-configuration")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String getAliquotConfiguration() {
-		AliquotConfiguration aliquotConfiguration = laboratoryConfigurationService.getAliquotConfiguration();
-		return new Response().buildSuccess(aliquotConfiguration).toJson();
-	}
+  @GET
+  @Secured
+  @Path("/descriptor")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public String getDescriptor() {
+    LaboratoryConfiguration laboratoryConfiguration = laboratoryConfigurationService.getLaboratoryConfiguration();
+    LaboratoryConfigurationDTO laboratoryConfigurationDTO = new LaboratoryConfigurationDTO(laboratoryConfiguration);
+    return new Response().buildSuccess(laboratoryConfigurationDTO).toJson();
+  }
 
-	@GET
-	@Secured
-	@Path("/aliquot-descriptors")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String getAliquotDescriptors() {
-		List<AliquoteDescriptor> aliquoteDescriptors = laboratoryConfigurationService.getAliquotDescriptors();
-		return new Response().buildSuccess(aliquoteDescriptors).toJson();
-	}
+  @GET
+  @Secured
+  @Path("/aliquot-configuration")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public String getAliquotConfiguration() {
+    AliquotConfiguration aliquotConfiguration = laboratoryConfigurationService.getAliquotConfiguration();
+    return new Response().buildSuccess(aliquotConfiguration).toJson();
+  }
+
+  @GET
+  @Secured
+  @Path("/aliquot-descriptors")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public String getAliquotDescriptors() {
+    List<AliquoteDescriptor> aliquoteDescriptors = laboratoryConfigurationService.getAliquotDescriptors();
+    return new Response().buildSuccess(aliquoteDescriptors).toJson();
+  }
 }
