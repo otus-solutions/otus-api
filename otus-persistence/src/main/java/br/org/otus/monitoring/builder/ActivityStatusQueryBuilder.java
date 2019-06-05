f83530fcdc234b58078833b5c8d212617ba807c3
Fabiano Ramires <fdrtec@gmail.com>
2019-06-05 13:22:14 -0300

    OA-135 Marca para comparação de  em pipelines com array

--- otus-persistence/src/main/java/br/org/otus/monitoring/builder/ActivityStatusQueryBuilder.java
+++ otus-persistence/src/main/java/br/org/otus/monitoring/builder/ActivityStatusQueryBuilder.java
@@ -10,6 +10,7 @@ import org.bson.conversions.Bson;
 import org.json.JSONArray;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -97,16 +98,26 @@ public class ActivityStatusQueryBuilder {
                 "    }\n" +
                 "  }"));
 
-        pipeline.add(parseQuery("{" +
-                "    $addFields:{" +
-                "        activityInapplicabilities:{" +
-                "            $filter: {" +
-                "                input:"+ AIS.get("AI") +", as: \"activityInapplicalibity\"," +
-                "                cond: {" +
-                "                    $and:[" +
-                "                        {$eq:[\"$$activityInapplicalibity.recruitmentNumber\",\"$_id\"]}," +
-                "                        {$eq:[\"$$activityInapplicalibity.acronym\",\"$headers\"]}" +
-                "    ]}}}}}"));
+        pipeline.add(new Document("$addFields",
+                new Document("activityInapplicabilities",
+                        new Document("$filter",
+                                new Document("input",AIS.get("AI")).append("as","activityInapplicalibity").append("cond",
+                                        new Document("$and", Arrays.asList(
+                                                new Document("$eq",Arrays.asList("$$activityInapplicalibity.recruitmentNumber","$_id")),
+                                                new Document("$eq",Arrays.asList("$$activityInapplicalibity.acronym","$headers"))))
+                                )))));
+
+
+//        pipeline.add(parseQuery("{" +
+//                "    $addFields:{" +
+//                "        activityInapplicabilities:{" +
+//                "            $filter: {" +
+//                "                input:"+ AIS.get("AI")+", as: \"activityInapplicalibity\"," +
+//                "                cond: {" +
+//                "                    $and:[" +
+//                "                        {$eq:[\"$$activityInapplicalibity.recruitmentNumber\",\"$_id\"]}," +
+//                "                        {$eq:[\"$$activityInapplicalibity.acronym\",\"$headers\"]}" +
+//                "    ]}}}}}"));
 
     pipeline.add(parseQuery("{\n" +
             "    $group: {\n" +
@@ -179,79 +190,107 @@ public class ActivityStatusQueryBuilder {
             "  }"));
 
 
-    pipeline.add(parseQuery("{\n" +
-            "        $group:{\n" +
-            "            _id:\"$_id\",\n" +
-            "            filteredActivities:{\n" +
-            "                $push:{\n" +
-            "                    $cond:[\n" +
-            "                        {$gt:[{$size:\"$activityInapplicabilities\"},0]},\n" +
-            "                        {\n" +
-            "                            status: 0,\n" +
-            "                            rn:\"$_id\",\n" +
-            "                            acronym:\"$headers\"\n" +
-            "                        },\n" +
-            "                        { \n" +
-            "                            $cond:[\n" +
-            "                                {$gt:[{$size:\"$activityFound\"},0]},\n" +
-            "                                {$arrayElemAt:[\"$activityFound\",-1]},\n" +
-            "                                { \n" +
-            "                                    status: null,\n" +
-            "                                    rn:'$_id',\n" +
-            "                                    acronym:'$headers'\n" +
-            "                                }\n" +
-            "                            ]\n" +
-            "                        }\n" +
-            "                    ]\n" +
-            "                    \n" +
-            "                }\n" +
-            "            }\n" +
-            "        }\n" +
-            "    }"));
+//============================== teste activityInapplicabilities sem $activityFound
+//        pipeline.add(parseQuery("{\n" +
+//                "        $group:{\n" +
+//                "            _id:\"$_id\",\n" +
+//                "            filteredActivities:{\n" +
+//                "                $push:{\n" +
+//                "                    $cond:[\n" +
+//                "                        {$gt:[{$size:\"$activityInapplicabilities\"},0]},\n" +
+//                "                        {\n" +
+//                "                            status: 0,\n" +
+//                "                            rn:\"$_id\",\n" +
+//                "                            acronym:\"$headers\"\n" +
+//                "                        },\n" +
+//                "                    ]\n" +
+//                "                    \n" +
+//                "                }\n" +
+//                "            }\n" +
+//                "        }\n" +
+//                "    }"));
 
+
+
+
+//============================================== Copia da nova query com activityInapplicabilities
 //    pipeline.add(parseQuery("{\n" +
-//            "    $group: {\n" +
-//            "      _id: \"$_id\",\n" +
-//            "      filteredActivities: {\n" +
-//            "        $push: {\n" +
-//            "          $cond: [\n" +
-//            "            {\n" +
-//            "              $gt: [\n" +
-//            "                {\n" +
-//            "                  $size: \"$activityFound\"\n" +
-//            "                },\n" +
-//            "                0\n" +
-//            "              ]\n" +
-//            "            },\n" +
-//            "            {\n" +
-//            "              $arrayElemAt: [\n" +
-//            "                \"$activityFound\",\n" +
-//            "                -1\n" +
-//            "              ]\n" +
-//            "            },\n" +
-//            "            {\n" +
-//            "              status: null,\n" +
-//            "              rn: \"$_id\",\n" +
-//            "              acronym: \"$headers\"\n" +
+//            "        $group:{\n" +
+//            "            _id:\"$_id\",\n" +
+//            "            filteredActivities:{\n" +
+//            "                $push:{\n" +
+//            "                    $cond:[\n" +
+//            "                        {$gt:[{$size:\"$activityInapplicabilities\"},0]},\n" +
+//            "                        {\n" +
+//            "                            status: 0,\n" +
+//            "                            rn:\"$_id\",\n" +
+//            "                            acronym:\"$headers\"\n" +
+//            "                        },\n" +
+//            "                        { \n" +
+//            "                            $cond:[\n" +
+//            "                                {$gt:[{$size:\"$activityFound\"},0]},\n" +
+//            "                                {$arrayElemAt:[\"$activityFound\",-1]},\n" +
+//            "                                { \n" +
+//            "                                    status: null,\n" +
+//            "                                    rn:'$_id',\n" +
+//            "                                    acronym:'$headers'\n" +
+//            "                                }\n" +
+//            "                            ]\n" +
+//            "                        }\n" +
+//            "                    ]\n" +
+//            "                    \n" +
+//            "                }\n" +
 //            "            }\n" +
-//            "          ]\n" +
 //            "        }\n" +
-//            "      }\n" +
-//            "    }\n" +
-//            "  }"));
+//            "    }"));
 
-//    pipeline.add(parseQuery("{\n" +
-//            "    $group: {\n" +
-//            "      _id: {\n" +
-//            "      },\n" +
-//            "      index: {\n" +
-//            "        $push: \"$_id\"\n" +
-//            "      },\n" +
-//            "      data: {\n" +
-//            "        $push: \"$filteredActivities.status\"\n" +
-//            "      }\n" +
-//            "    }\n" +
-//            "  }"));
+
+
+//==================================== copia do pipeline original funcional sem activityInapplicabilities
+    pipeline.add(parseQuery("{\n" +
+            "    $group: {\n" +
+            "      _id: \"$_id\",\n" +
+            "      filteredActivities: {\n" +
+            "        $push: {\n" +
+            "          $cond: [\n" +
+            "            {\n" +
+            "              $gt: [\n" +
+            "                {\n" +
+            "                  $size: \"$activityFound\"\n" +
+            "                },\n" +
+            "                0\n" +
+            "              ]\n" +
+            "            },\n" +
+            "            {\n" +
+            "              $arrayElemAt: [\n" +
+            "                \"$activityFound\",\n" +
+            "                -1\n" +
+            "              ]\n" +
+            "            },\n" +
+            "            {\n" +
+            "              status: null,\n" +
+            "              rn: \"$_id\",\n" +
+            "              acronym: \"$headers\"\n" +
+            "            }\n" +
+            "          ]\n" +
+            "        }\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }"));
+//===========================================
+
+    pipeline.add(parseQuery("{\n" +
+            "    $group: {\n" +
+            "      _id: {\n" +
+            "      },\n" +
+            "      index: {\n" +
+            "        $push: \"$_id\"\n" +
+            "      },\n" +
+            "      data: {\n" +
+            "        $push: \"$filteredActivities.status\"\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }"));
 
 
     }
