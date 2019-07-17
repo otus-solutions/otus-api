d7a5b789316c826e2861c745f320f83a8bbe119e
Fabiano Ramires <fdrtec@gmail.com>
2019-07-17 17:10:49 -0300

    OA-144 requisiçao post enviando string de variaveis pelo body

--- otus-business/src/main/java/br/org/otus/gateway/GatewayFacade.java
+++ otus-business/src/main/java/br/org/otus/gateway/GatewayFacade.java
@@ -1,6 +1,9 @@
 package br.org.otus.gateway;
 
 
+import com.google.gson.JsonObject;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,21 +14,35 @@ public class GatewayFacade {
 
     private static final String HOST = "http://localhost:";
     private static final String PORT = "8083";
-    private static final String LIST_VARIABLES_RESOURCE = "/api/findAllCurrentVariables";
+    private static final String GET_VARIABLES_RESOURCE = "/api/findAllCurrentVariables";
+    private static final String POST_VARIABLES_RESOURCE = "/api/findCurrentVariables";
+    private static final String  TYPE_REQ_GET = "GET";
+    private static final String  TYPE_REQ_POST = "POST";
 
 
-    public String getCurrentVariables() throws IOException {
-        HttpURLConnection microserviceConnection = connectorPerTypeOfRequest(HOST, PORT, LIST_VARIABLES_RESOURCE, "GET");
+
+//    public String getCurrentVariables() throws IOException {
+//        HttpURLConnection microserviceConnection = connectorPerTypeOfRequest(HOST, PORT, GET_VARIABLES_RESOURCE, TYPE_REQ);
+//        requisitionValidator(microserviceConnection);
+//        String currentVariables = extractionCurrentVariables(microserviceConnection);
+//        microserviceConnection.disconnect();
+//        return currentVariables;
+//    }
+
+    public String findCurrentVariables(String infoVariables) throws IOException {
+        HttpURLConnection microserviceConnection = connectorPerTypeOfRequest(HOST, PORT, POST_VARIABLES_RESOURCE, TYPE_REQ_POST, infoVariables);
         requisitionValidator(microserviceConnection);
         String currentVariables = extractionCurrentVariables(microserviceConnection);
         microserviceConnection.disconnect();
         return currentVariables;
     }
 
-    private HttpURLConnection connectorPerTypeOfRequest(String host, String port, String resource, String typeReq) throws IOException {
+    private HttpURLConnection connectorPerTypeOfRequest(String host, String port, String resource, String typeReq, String infoVariables) throws IOException {
         URL url = new URL(host + port + resource);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod(typeReq);
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(infoVariables.getBytes("UTF-8"));
         return conn;
     }
 
--- otus-business/src/test/java/br/org/otus/gateway/GatewayFacadeTest.java
+++ otus-business/src/test/java/br/org/otus/gateway/GatewayFacadeTest.java
@@ -1,5 +1,8 @@
 package br.org.otus.gateway;
 
+import com.google.gson.JsonParser;
+import com.mongodb.util.JSON;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,11 +23,27 @@ public class GatewayFacadeTest {
     public void setUp() throws Exception {
     }
 
-
-
     @Test
     public void getCurrentFacadeMethod_should() throws IOException, ClassNotFoundException {
+        String InfoVariables = "{\n" +
+                "    \"recruitmentNumber\": 2047555,\n" +
+                "    \"variables\": [\n" +
+                "        {\n" +
+                "            \"identification\": 123456,\n" +
+                "            \"name\": \"tst1\",\n" +
+                "            \"value\": \"Text\",\n" +
+                "            \"sending\": 1\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"identification\": null,\n" +
+                "            \"name\": \"tst1\",\n" +
+                "            \"value\": \"Text\",\n" +
+                "            \"sending\": 9\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
         //assertEquals(gatewayFacade.getCurrentVariables(), "[{\"identification\":123456,\"name\":\"tst1\",\"value\":\"Text\"}]");
+        //assertEquals(gatewayFacade.findCurrentVariables(InfoVariables), "[{\"identification\":123456,\"name\":\"tst1\",\"value\":\"Text\"}]");
         assertEquals("variable", "variable");
     }
 }
