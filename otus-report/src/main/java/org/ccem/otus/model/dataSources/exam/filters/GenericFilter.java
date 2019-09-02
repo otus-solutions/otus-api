f529eb1196d9e9cb3ae95221a12f51a11b96da2b
emanoelvianna <emanoel.vianna@outlook.com>
2019-09-02 15:21:40 -0300

    OA-147 - inicio dos filtros específicos

---
+++ otus-report/src/main/java/org/ccem/otus/model/dataSources/exam/filters/GenericFilter.java
@@ -0,0 +1,11 @@
+package org.ccem.otus.model.dataSources.exam.filters;
+
+public class GenericFilter {
+
+  private String examName;
+
+  public String getExamName() {
+    return examName;
+  }
+
+}
---
+++ otus-report/src/main/java/org/ccem/otus/model/dataSources/exam/filters/RetinographyFilter.java
@@ -0,0 +1,7 @@
+package org.ccem.otus.model.dataSources.exam.filters;
+
+public class RetinographyFilter extends GenericFilter {
+
+  // TODO:
+
+}
