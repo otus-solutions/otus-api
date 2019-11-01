194436ad6822f834b4481bc45742bdcaa787cf7f
Fabiano Ramires <fdrtec@gmail.com>
2019-11-01 13:51:57 -0300

    OA-154 revisão do código

--- source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/SurveyActivity.java
+++ source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/SurveyActivity.java
@@ -1,8 +1,7 @@
 package org.ccem.otus.model.survey.activity;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.google.gson.GsonBuilder;
+import com.google.gson.annotations.SerializedName;
 import org.bson.types.ObjectId;
 import org.ccem.otus.model.survey.activity.configuration.ActivityCategory;
 import org.ccem.otus.model.survey.activity.filling.AnswerFill;
@@ -17,129 +16,124 @@ import org.ccem.otus.utils.AnswerAdapter;
 import org.ccem.otus.utils.LongAdapter;
 import org.ccem.otus.utils.ObjectIdToStringAdapter;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.annotations.SerializedName;
+import java.util.List;
+import java.util.Optional;
 
 public class SurveyActivity {
 
-	private String objectType;
-	@SerializedName("_id")
-	private ObjectId activityID;
-	private SurveyForm surveyForm;
-	private ActivityMode mode;
-	private ActivityCategory category;
-	private Participant participantData;
-	private List<Interview> interviews;
-	private FillContainer fillContainer;
-	private List<ActivityStatus> statusHistory;
-	private Boolean isDiscarded;
-	private NavigationTracker navigationTracker;
-	private String externalID;
+  private String objectType;
+  @SerializedName("_id")
+  private ObjectId activityID;
+  private SurveyForm surveyForm;
+  private ActivityMode mode;
+  private ActivityCategory category;
+  private Participant participantData;
+  private List<Interview> interviews;
+  private FillContainer fillContainer;
+  private List<ActivityStatus> statusHistory;
+  private Boolean isDiscarded;
+  private NavigationTracker navigationTracker;
+  private String externalID;
 
-	public SurveyActivity() {	this.isDiscarded = Boolean.FALSE;	}
+  public SurveyActivity() { this.isDiscarded = Boolean.FALSE; }
 
-	public void setIsDiscarded(Boolean isDiscarded) {
-		this.isDiscarded = isDiscarded;
-	}
-
-	public Boolean isDiscarded() {
-		return isDiscarded;
-	}
-
-	public ObjectId getActivityID() {
-		return activityID;
-	}
-
-	public String getObjectType() {
-		return objectType;
-	}
-
-	public SurveyForm getSurveyForm() {
-		return surveyForm;
-	}
-
-	public ActivityMode getMode() {
-		return mode;
-	}
-
-	public ActivityCategory getCategory() {
-		return category;
-	}
-
-	public List<Interview> getInterviews() {
-		return interviews;
-	}
-
-	public FillContainer getFillContainer() {
-		return fillContainer;
-	}
-
-	public List<ActivityStatus> getStatusHistory() {
-		return statusHistory;
-	}
-
-	public Participant getParticipantData() {
-		return participantData;
-	}
-
-	public NavigationTracker getNavigationTracker() {
-		return navigationTracker;
-	}
-
-  public String getExternalID() { return externalID; }
-
-  public void setExternalID(String externalID) { this.externalID = externalID; }
-
-  public Boolean hasRequiredExternalID(){
-	  return this.getSurveyForm().getRequiredExternalID();
+  public void setIsDiscarded(Boolean isDiscarded) {
+    this.isDiscarded = isDiscarded;
   }
 
+  public Boolean isDiscarded() {
+    return isDiscarded;
+  }
+
+  public ObjectId getActivityID() {
+    return activityID;
+  }
+
+  public String getObjectType() {
+    return objectType;
+  }
+
+  public SurveyForm getSurveyForm() {
+    return surveyForm;
+  }
+
+  public ActivityMode getMode() {
+    return mode;
+  }
+
+  public ActivityCategory getCategory() {
+    return category;
+  }
+
+  public List<Interview> getInterviews() {
+    return interviews;
+  }
+
+  public FillContainer getFillContainer() {
+    return fillContainer;
+  }
+
+  public List<ActivityStatus> getStatusHistory() {
+    return statusHistory;
+  }
+
+  public Participant getParticipantData() {
+    return participantData;
+  }
+
+  public NavigationTracker getNavigationTracker() {
+    return navigationTracker;
+  }
+
+  public String getExternalID() { return externalID;  }
+
+  public Boolean hasRequiredExternalID() { return this.getSurveyForm().getRequiredExternalID();  }
+
   public Optional<ActivityStatus> getCurrentStatus() {
-		return this.statusHistory.stream().reduce((activityStatus, activityStatus2) -> activityStatus2);
-	}
+    return this.statusHistory.stream().reduce((activityStatus, activityStatus2) -> activityStatus2);
+  }
 
-	public Optional<ActivityStatus> getLastStatusByName(String name) {
-		return statusHistory.stream()
-				.filter(status -> status.getName().equals(name))
-				.reduce((activityStatus, activityStatus2) -> activityStatus2);
-	}
+  public Optional<ActivityStatus> getLastStatusByName(String name) {
+    return statusHistory.stream()
+      .filter(status -> status.getName().equals(name))
+      .reduce((activityStatus, activityStatus2) -> activityStatus2);
+  }
 
-	public Optional<Interview> getLastInterview(){
-		return this.interviews.stream().reduce((interview, interview2) -> interview2);
+  public Optional<Interview> getLastInterview() {
+    return this.interviews.stream().reduce((interview, interview2) -> interview2);
 
-	}
+  }
 
-	public Boolean isFinalized() {
-			ActivityStatus activityStatus = getCurrentStatus().get();
-			return activityStatus.getName().equals("FINALIZED");
-    }
+  public Boolean isFinalized() {
+    ActivityStatus activityStatus = getCurrentStatus().get();
+    return activityStatus.getName().equals("FINALIZED");
+  }
 
-	public static String serialize(SurveyActivity surveyActivity) {
-		return getGsonBuilder().create().toJson(surveyActivity);
-	}
+  public static String serialize(SurveyActivity surveyActivity) {
+    return getGsonBuilder().create().toJson(surveyActivity);
+  }
 
-	public static SurveyActivity deserialize(String surveyActivity) {
-		GsonBuilder builder = getGsonBuilder();
-		builder.registerTypeAdapter(Long.class, new LongAdapter());
-		return builder.create().fromJson(surveyActivity, SurveyActivity.class);
-	}
+  public static SurveyActivity deserialize(String surveyActivity) {
+    GsonBuilder builder = getGsonBuilder();
+    builder.registerTypeAdapter(Long.class, new LongAdapter());
+    return builder.create().fromJson(surveyActivity, SurveyActivity.class);
+  }
 
-	/**
-	 * @return a GsonBuilder instance with AnswerAdapter, ObjectIdToStringAdapter
-	 *         registered and also all registered adapters of SurveyForm.
-	 *         {@link SurveyForm#getGsonBuilder}
-	 *
-	 */
-	public static GsonBuilder getGsonBuilder() {
-		//antes de editar este método, levar em conta os usuários de Response.toSurveyJson()
-		//geralmente usam esse método por causa do adapter de Oid
+  /**
+   * @return a GsonBuilder instance with AnswerAdapter, ObjectIdToStringAdapter
+   * registered and also all registered adapters of SurveyForm.
+   * {@link SurveyForm#getGsonBuilder}
+   */
+  public static GsonBuilder getGsonBuilder() {
+    //antes de editar este método, levar em conta os usuários de Response.toSurveyJson()
+    //geralmente usam esse método por causa do adapter de Oid
 
-		GsonBuilder builder = SurveyForm.getGsonBuilder();
+    GsonBuilder builder = SurveyForm.getGsonBuilder();
 
-		builder.registerTypeAdapter(AnswerFill.class, new AnswerAdapter());
-		builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
-		builder.serializeNulls();
+    builder.registerTypeAdapter(AnswerFill.class, new AnswerAdapter());
+    builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
+    builder.serializeNulls();
 
-		return builder;
-	}
+    return builder;
+  }
 }
