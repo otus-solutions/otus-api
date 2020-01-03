8ed65c096857e8c9520478598099b992fc0f7a0a
FlaviaAP <flavia.avila@ufrgs.br>
2020-01-03 10:12:59 -0300

    OA-157 retirado dir persistence do arquivo gitignore

---
+++ source/otus-user/src/main/java/br/org/otus/persistence/pendency/UserActivityPendencyDao.java
@@ -0,0 +1,28 @@
+package br.org.otus.persistence.pendency;
+
+import br.org.otus.model.pendency.UserActivityPendency;
+import org.bson.types.ObjectId;
+import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
+import org.ccem.otus.exceptions.webservice.common.MemoryExcededException;
+import org.ccem.otus.exceptions.webservice.validation.ValidationException;
+
+import java.util.ArrayList;
+
+public interface UserActivityPendencyDao {
+  //here
+
+  ObjectId create(UserActivityPendency userActivityPendency);
+
+  void update(ObjectId userActivityPendencyOID, UserActivityPendency userActivityPendency) throws ValidationException, DataNotFoundException;
+
+  void delete(ObjectId oid) throws DataNotFoundException;
+
+  UserActivityPendency findByActivityInfo(String activityId) throws DataNotFoundException;
+
+  ArrayList<UserActivityPendency> findAllPendencies() throws DataNotFoundException, MemoryExcededException;
+
+  ArrayList<UserActivityPendency> findOpenedPendencies() throws DataNotFoundException, MemoryExcededException;
+
+  ArrayList<UserActivityPendency> findDonePendencies() throws DataNotFoundException, MemoryExcededException;
+
+}
