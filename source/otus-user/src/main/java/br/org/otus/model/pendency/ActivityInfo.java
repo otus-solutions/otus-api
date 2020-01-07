c57253a899005c4d6d784002f7ffd37a6ad957a4
FlaviaAP <flavia.avila@ufrgs.br>
2020-01-07 13:24:20 -0300

    OA-157 add método equals na classe ActivityInfo

--- source/otus-user/src/main/java/br/org/otus/model/pendency/ActivityInfo.java
+++ source/otus-user/src/main/java/br/org/otus/model/pendency/ActivityInfo.java
@@ -4,6 +4,8 @@ import br.org.otus.utils.ObjectIdAdapter;
 import com.google.gson.GsonBuilder;
 import org.bson.types.ObjectId;
 
+import java.util.Objects;
+
 public class ActivityInfo {
 
   private ObjectId id;
@@ -20,6 +22,13 @@ public class ActivityInfo {
 
   public int getRecruitmentNumber() {
     return recruitmentNumber;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    return Objects.equals(id, ((ActivityInfo) o).getId());
   }
 
   public static String serialize(ActivityInfo activityInfo) {
