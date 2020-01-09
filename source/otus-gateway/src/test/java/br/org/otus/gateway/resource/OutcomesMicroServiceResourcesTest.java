43b9e992111ad45c950b374452ffc7774a8876c7
Adriano <adriano@otus-solutions.com.br>
2020-01-09 20:06:15 -0300

    OAP-1 OutcomesMicroServiceResourcesTest

---
+++ source/otus-gateway/src/test/java/br/org/otus/gateway/resource/OutcomesMicroServiceResourcesTest.java
@@ -0,0 +1,55 @@
+package br.org.otus.gateway.resource;
+
+import br.org.otus.gateway.MicroservicesEnvironments;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static junit.framework.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ OutcomesMicroServiceResources.class })
+public class OutcomesMicroServiceResourcesTest {
+
+  private static final String HOST = "localhost";
+  private static final String PORT = "8081";
+  private OutcomesMicroServiceResources resources;
+  private MicroservicesEnvironments microservicesEnvironments;
+  private URL url;
+
+  @Before
+  public void setUp() throws Exception {
+    microservicesEnvironments = MicroservicesEnvironments.OUTCOMES;
+    mockStatic(System.class);
+    when(System.getenv(microservicesEnvironments.getHost())).thenReturn(HOST);
+    when(System.getenv(microservicesEnvironments.getPort())).thenReturn(PORT);
+
+    resources = new OutcomesMicroServiceResources();
+  }
+
+  @Test
+  public void getCreateOutcomeAddress_method_should_return_expected_url() throws MalformedURLException {
+    url = new URL("http://localhost:8081/create");
+    Assert.assertEquals(resources.getCreateOutcomeAddress(), url);
+  }
+
+  @Test
+  public void getUpdateOutcomeAddress_method_should_return_expected_url() throws MalformedURLException {
+    url = new URL("http://localhost:8081/update");
+    Assert.assertEquals(resources.getUpdateOutcomeAddress(), url);
+  }
+
+  @Test
+  public void getListOutcomesAddress_method_should_return_expected_url() throws MalformedURLException {
+    url = new URL("http://localhost:8081/list");
+    Assert.assertEquals(resources.getListOutcomesAddress(), url);
+  }
+}
