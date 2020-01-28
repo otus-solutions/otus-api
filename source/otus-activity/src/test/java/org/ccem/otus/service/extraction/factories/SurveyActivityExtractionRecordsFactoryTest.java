4f274b7e1f106d78655527d7396572973dde3e54
Adonis <adonis.garcia.adg@gmail.com>
2020-01-28 13:00:32 -0300

    OA-153 teste de unidade parcial SurveyActivityExtractionRecordsFactoryTest

--- source/otus-activity/src/test/java/org/ccem/otus/service/extraction/factories/SurveyActivityExtractionRecordsFactoryTest.java
+++ source/otus-activity/src/test/java/org/ccem/otus/service/extraction/factories/SurveyActivityExtractionRecordsFactoryTest.java
@@ -13,6 +13,7 @@ import org.ccem.otus.model.survey.activity.status.ActivityStatusOptions;
 import org.ccem.otus.participant.model.Participant;
 import org.ccem.otus.survey.form.SurveyForm;
 import org.ccem.otus.survey.template.SurveyTemplate;
+import org.ccem.otus.survey.template.identity.Identity;
 import org.ccem.otus.survey.template.item.SurveyItem;
 import org.ccem.otus.survey.template.item.questions.numeric.NumericQuestion;
 import org.junit.Assert;
@@ -22,10 +23,12 @@ import org.powermock.reflect.Whitebox;
 
 public class SurveyActivityExtractionRecordsFactoryTest {
   private Long RECRUITMENT_NUMBER = (Long) 322148795L;
+  private static final String ACRONYM = "ATCA";
   private static final String FIELD_CENTER = "RS";
 
   private SurveyActivityExtractionRecordsFactory records;
   private NumericQuestion numericQuestion;
+  private Identity identity;
   private List<SurveyItem> itemContainer;
   private SurveyTemplate surveyTemplate;
   private SurveyForm surveyForm;
@@ -37,10 +40,13 @@ public class SurveyActivityExtractionRecordsFactoryTest {
     this.numericQuestion = new NumericQuestion();
     Whitebox.setInternalState(this.numericQuestion, "customID", "FORM1");
     Whitebox.setInternalState(this.numericQuestion, "templateID", "FORM1");
+    this.identity = new Identity();
+    Whitebox.setInternalState(this.identity, "acronym",ACRONYM);
     this.itemContainer = new ArrayList<>();
     this.itemContainer.add(this.numericQuestion);
     this.surveyTemplate = new SurveyTemplate();
     Whitebox.setInternalState(this.surveyTemplate, "itemContainer", this.itemContainer);
+    Whitebox.setInternalState(this.surveyTemplate, "identity", identity);
     this.surveyForm = new SurveyForm(this.surveyTemplate, "test@test.com");
     this.headers = new SurveyActivityExtractionHeadersFactory(this.surveyForm);
 
@@ -54,38 +60,38 @@ public class SurveyActivityExtractionRecordsFactoryTest {
     Assert.assertTrue(values instanceof LinkedHashMap);
   }
 
-//  @Test
-//  public void getSurveyInformation_method_should_return_values_expected() {
-//    Participant participant = new Participant(RECRUITMENT_NUMBER);
-//
-//    ActivityStatusOptions activityStatusOptions = ActivityStatusOptions.FINALIZED;
-//
-//    ActivityStatus activityStatus = new ActivityStatus();
-//    Whitebox.setInternalState(activityStatus, "objectType", "ActivityStatus");
-//    Whitebox.setInternalState(activityStatus, "name", activityStatusOptions);
-//    Whitebox.setInternalState(activityStatus, "date", LocalDateTime.now());
-//
-//    ArrayList<ActivityStatus> statusHistory = new ArrayList<>();
-//    statusHistory.add(activityStatus);
-//
-//    FillContainer fillContainer = new FillContainer();
-//    List<QuestionFill> fillingList = new ArrayList<>();
-//    QuestionFill questionFill = new QuestionFill();
-//    Whitebox.setInternalState(questionFill, "questionID", "ATCA4");
-//
-//    fillingList.add(questionFill);
-//
-//    Whitebox.setInternalState(fillContainer, "fillingList", fillingList);
-//
-//    this.surveyActivity = new SurveyActivity();
-//    Whitebox.setInternalState(this.surveyActivity, "surveyForm", surveyForm);
-//    Whitebox.setInternalState(this.surveyActivity, "participantData", participant);
-//    Whitebox.setInternalState(this.surveyActivity, "statusHistory", statusHistory);
-//    Whitebox.setInternalState(this.surveyActivity, "fillContainer", fillContainer);
-//
-//    this.records.buildSurveyBasicInfo(surveyActivity, FIELD_CENTER);
-//    LinkedHashMap<String, Object> values = this.records.getSurveyInformation();
-//
-//  }
+  @Test
+  public void getSurveyInformation_method_should_return_values_expected() {
+    Participant participant = new Participant(RECRUITMENT_NUMBER);
+
+    ActivityStatusOptions activityStatusOptions = ActivityStatusOptions.FINALIZED;
+
+    ActivityStatus activityStatus = new ActivityStatus();
+    Whitebox.setInternalState(activityStatus, "objectType", "ActivityStatus");
+    Whitebox.setInternalState(activityStatus, "name", activityStatusOptions);
+    Whitebox.setInternalState(activityStatus, "date", LocalDateTime.now());
+
+    ArrayList<ActivityStatus> statusHistory = new ArrayList<>();
+    statusHistory.add(activityStatus);
+
+    FillContainer fillContainer = new FillContainer();
+    List<QuestionFill> fillingList = new ArrayList<>();
+    QuestionFill questionFill = new QuestionFill();
+    Whitebox.setInternalState(questionFill, "questionID", "ATCA4");
+
+    fillingList.add(questionFill);
+
+    Whitebox.setInternalState(fillContainer, "fillingList", fillingList);
+
+    this.surveyActivity = new SurveyActivity();
+    Whitebox.setInternalState(this.surveyActivity, "surveyForm", surveyForm);
+    Whitebox.setInternalState(this.surveyActivity, "participantData", participant);
+    Whitebox.setInternalState(this.surveyActivity, "statusHistory", statusHistory);
+    Whitebox.setInternalState(this.surveyActivity, "fillContainer", fillContainer);
+
+    this.records.buildSurveyBasicInfo(surveyActivity, FIELD_CENTER);
+    LinkedHashMap<String, Object> values = this.records.getSurveyInformation();
+
+  }
 
 }
