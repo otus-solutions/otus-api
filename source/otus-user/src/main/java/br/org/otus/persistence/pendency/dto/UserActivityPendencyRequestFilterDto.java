3d6c36cd510cf18ebf65bb35aff7f75193be0827
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-17 17:25:54 -0300

    OA-166 refatoração da classe UserActivityPendencyRequestFilterDto

--- source/otus-user/src/main/java/br/org/otus/persistence/pendency/dto/UserActivityPendencyRequestFilterDto.java
+++ source/otus-user/src/main/java/br/org/otus/persistence/pendency/dto/UserActivityPendencyRequestFilterDto.java
@@ -5,57 +5,39 @@ import com.google.gson.annotations.SerializedName;
 
 public class UserActivityPendencyRequestFilterDto {
 
-  @SerializedName(value = "finalized")
-  private Boolean finalized;
+  public static final String FINALIZED_STATUS = "FINALIZED";
+  public static final String NOT_FINALIZED_STATUS = "NOT_FINALIZED";
+
+  @SerializedName(value = "status")
+  private String status;
+
+  @SerializedName(value = "acronym")
+  private String acronym;
+
+  @SerializedName(value = "rn")
+  private Long rn;
 
   @SerializedName(value = "requester")
-  private String requester;
-  //  private List<String> requester;
+  private String[] requester;
 
   @SerializedName(value = "receiver")
-  private String receiver;
-  //  private List<String> receiver;
-
-  public Boolean getFinalized() {
-    return finalized;
-  }
-
-  public void setFinalized(Boolean finalized) {
-    this.finalized = finalized;
-  }
-
-//  public List<String> getRequester() {
-//    return requester;
-//  }
-//
-//  public void setRequester(List<String> requester) {
-//    this.requester = requester;
-//  }
-//
-//  public List<String> getReceiver() {
-//    return receiver;
-//  }
-//
-//  public void setReceiver(List<String> receiver) {
-//    this.receiver = receiver;
-//  }
+  private String[] receiver;
 
 
-  public String getRequester() {
+  public String getAcronym() { return acronym; }
+
+  public Long getRn() { return rn; }
+
+  public String getStatus() { return status; }
+
+  public String[] getRequesters() {
     return requester;
   }
 
-  public void setRequester(String requester) {
-    this.requester = requester;
-  }
-
-  public String getReceiver() {
+  public String[] getReceivers() {
     return receiver;
   }
 
-  public void setReceiver(String receiver) {
-    this.receiver = receiver;
-  }
 
   public static String serialize(UserActivityPendencyRequestFilterDto userActivityPendencyRequestFilterDto) {
     return getGsonBuilder().create().toJson(userActivityPendencyRequestFilterDto);
