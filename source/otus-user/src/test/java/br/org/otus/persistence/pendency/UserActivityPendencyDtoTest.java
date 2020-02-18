0b30aa49a3b62de8e5c252d46eeed5ed9640f0a0
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-18 16:57:04 -0300

    OA-166 testes unitários para classe UserActivityPendencDto

--- source/otus-user/src/test/java/br/org/otus/persistence/pendency/UserActivityPendencyDtoTest.java
+++ source/otus-user/src/test/java/br/org/otus/persistence/pendency/UserActivityPendencyDtoTest.java
@@ -1,14 +1,14 @@
 package br.org.otus.persistence.pendency;
 
 import br.org.otus.persistence.pendency.dto.UserActivityPendencyDto;
+import br.org.otus.persistence.pendency.dto.UserActivityPendencyFilterDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 @RunWith(PowerMockRunner.class)
 public class UserActivityPendencyDtoTest {
@@ -18,29 +18,38 @@ public class UserActivityPendencyDtoTest {
   private static final String[] FIELDS_TO_ORDER = new String[] { "requester", "acronym" };
   private static final int ORDER_MODE = 1;
 
-  private UserActivityPendencyDto userActivityPendencyDto;
+  private UserActivityPendencyDto userActivityPendencyDto = new UserActivityPendencyDto();
   private String userActivityPendencyDtoJson;
+  private UserActivityPendencyFilterDto userActivityPendencyFilterDto = new UserActivityPendencyFilterDto();
 
   @Before
-  public void SetUp(){
+  public void setUp(){
     Whitebox.setInternalState(userActivityPendencyDto, "currentQuantity", CURRENT_QUANTITY);
     Whitebox.setInternalState(userActivityPendencyDto, "quantityToGet", QUANTITY_TO_GET);
     Whitebox.setInternalState(userActivityPendencyDto, "fieldsToOrder", FIELDS_TO_ORDER);
     Whitebox.setInternalState(userActivityPendencyDto, "orderMode", ORDER_MODE);
+    Whitebox.setInternalState(userActivityPendencyDto, "filterDto", userActivityPendencyFilterDto);
 
     userActivityPendencyDtoJson = UserActivityPendencyDto.serialize(userActivityPendencyDto);
   }
 
   @Test
-  public void userActivityPendencyDto_not_should_be_null(){
-    assertNotNull(userActivityPendencyDto);
-    assertNotNull(userActivityPendencyDto.getFilterDto());
+  public void unit_test_for_invoke_getters(){
+    assertEquals(CURRENT_QUANTITY, userActivityPendencyDto.getCurrentQuantity());
+    assertEquals(QUANTITY_TO_GET, userActivityPendencyDto.getQuantityToGet());
+    assertArrayEquals(FIELDS_TO_ORDER, userActivityPendencyDto.getFieldsToOrder());
+    assertEquals(ORDER_MODE, userActivityPendencyDto.getOrderMode());
+    assertEquals(userActivityPendencyFilterDto, userActivityPendencyDto.getFilterDto());
   }
 
   @Test
-  public void deserialize_results(){
-    assertEquals(CURRENT_QUANTITY, userActivityPendencyDto.getCurrentQuantity());
-    assertEquals(QUANTITY_TO_GET, userActivityPendencyDto.getQuantityToGet());
+  public void serializeStaticMethdos_should_convert_objectModel_to_JsonString(){
+    assertTrue(UserActivityPendencyDto.serialize(userActivityPendencyDto) instanceof String);
+  }
+
+  @Test
+  public void deserializeStaticMethdos_should_convert_JsonString_to_objectModel(){
+    assertTrue(UserActivityPendencyDto.deserialize(userActivityPendencyDtoJson) instanceof UserActivityPendencyDto);
   }
 
 }
