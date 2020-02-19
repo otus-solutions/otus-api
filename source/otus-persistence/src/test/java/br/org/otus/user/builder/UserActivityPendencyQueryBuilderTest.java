92a8d5857f5c9185f6c5c9bda37e35dfae254b12
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-19 20:33:12 -0300

    OA-166 teste unitário para metodo getAllPendencies de UserActivityPendencyQueryBuilder

--- source/otus-persistence/src/test/java/br/org/otus/user/builder/UserActivityPendencyQueryBuilderTest.java
+++ source/otus-persistence/src/test/java/br/org/otus/user/builder/UserActivityPendencyQueryBuilderTest.java
@@ -1,17 +1,29 @@
 package br.org.otus.user.builder;
 
+import br.org.otus.persistence.pendency.dto.UserActivityPendencyDto;
+import br.org.otus.persistence.pendency.dto.UserActivityPendencyFilterDto;
 import br.org.otus.user.pendency.builder.UserActivityPendencyQueryBuilder;
 import com.google.gson.GsonBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import java.util.zip.DataFormatException;
 
 import static org.junit.Assert.assertEquals;
 
 public class UserActivityPendencyQueryBuilderTest {
 
-  private static UserActivityPendencyQueryBuilder builder = null;
   private static final String USER_ROLE = "requester";
   private static final String USER_EMAIL = "requester@otus.com";
+
+  private static final String ACTIVITY_ID_FIELD = UserActivityPendencyQueryBuilder.ACTIVITY_ID_FIELD;
+  private static final String ACTIVITY_INFO = UserActivityPendencyQueryBuilder.ACTIVITY_INFO;
+  private static final String ACTIVITY_NAME_FIELD = UserActivityPendencyQueryBuilder.ACTIVITY_NAME_FIELD;
+  private static final String ACTIVITY_ACRONYM_FIELD = UserActivityPendencyQueryBuilder.ACTIVITY_ACRONYM_FIELD;
+  private static final String ACTIVITY_RN_FIELD = UserActivityPendencyQueryBuilder.ACTIVITY_RN_FIELD;
+
+  private UserActivityPendencyQueryBuilder builder = null;
 
   @Before
   public void setUp() {
@@ -19,7 +31,57 @@ public class UserActivityPendencyQueryBuilderTest {
   }
 
   @Test
-  public void getListAllPendenciesByUserQuery() {
+  public void getAllPendencies() throws DataFormatException {
+    final int CURRENT_QUANTITY = 100;
+    final int QUANTITY_TO_GET = 50;
+    final String[] FIELDS_TO_ORDER = new String[] { "rn", "dueDate" };
+    final Integer ASCENDING_ORDER_MODE = 1;
+    final String ACRONYM = "XXX";
+    UserActivityPendencyFilterDto userActivityPendencyFilterDto = new UserActivityPendencyFilterDto();
+    Whitebox.setInternalState(userActivityPendencyFilterDto, "acronym", ACRONYM);
+    Whitebox.setInternalState(userActivityPendencyFilterDto, "requester", new String[]{ USER_EMAIL });
+
+    UserActivityPendencyDto userActivityPendencyDto = new UserActivityPendencyDto();
+    Whitebox.setInternalState(userActivityPendencyDto, "currentQuantity", CURRENT_QUANTITY);
+    Whitebox.setInternalState(userActivityPendencyDto, "quantityToGet", QUANTITY_TO_GET);
+    Whitebox.setInternalState(userActivityPendencyDto, "fieldsToOrder", FIELDS_TO_ORDER);
+    Whitebox.setInternalState(userActivityPendencyDto, "orderMode", ASCENDING_ORDER_MODE);
+    Whitebox.setInternalState(userActivityPendencyDto, "filterDto", userActivityPendencyFilterDto);
+
+    final String EXPECTED_QUERY = "[{" +
+      "\"$lookup\":{" +
+      "\"from\":\"activity\"," +
+      "\"let\":{\"" + ACTIVITY_ID_FIELD+"\":\"$"+ACTIVITY_ID_FIELD+"\"}," +
+      "\"pipeline\":[{" +
+      "\"$match\":{" +
+      "\"$expr\":{" +
+      "\"$and\":[" +
+      "{\"$eq\":[\"$"+ACTIVITY_ACRONYM_FIELD+"\",\""+ ACRONYM +"\"]}," +
+      "{\"$eq\":[\"$$"+ACTIVITY_ID_FIELD+"\",\"$_id\"]}," +
+      "{\"$eq\":[false,\"$isDiscarded\"]}]}}}," +
+      "{\"$project\":{" +
+      "\"recruitmentNumber\":\"$" + ACTIVITY_RN_FIELD + "\"," +
+      "\"name\":\"$"+ACTIVITY_NAME_FIELD+"\"," +
+      "\"acronym\":\"$"+ACTIVITY_ACRONYM_FIELD+"\"," +
+      "\"lastStatusName\":{\"$arrayElemAt\":[\"$statusHistory.name\",-1.0]}," +
+      "\"externalID\":1.0}}],"+
+      "\"as\":\""+ACTIVITY_INFO+"\"}},"+
+      "{\"$match\":{"+
+      "\"$expr\":{"+
+      "\"$and\":["+
+      "{\"$in\":[\"$" + USER_ROLE + "\",[\"" + USER_EMAIL + "\"]]}," +
+      "{\"$gt\":[{\"$size\":\"$"+ACTIVITY_INFO+"\"},0.0]}"+
+      "]}}},{"+
+      "\"$addFields\":{\""+ACTIVITY_INFO+"\":{\"$arrayElemAt\":[\"$"+ACTIVITY_INFO+"\",0.0]}}},"+
+      "{\"$skip\":"+CURRENT_QUANTITY+".0},"+
+      "{\"$limit\":"+QUANTITY_TO_GET+".0},"+
+      "{\"$sort\":{\""+ACTIVITY_INFO+".recruitmentNumber\":1.0,\"dueDate\":1.0}}]";
+    assertEquals(EXPECTED_QUERY,
+      new GsonBuilder().create().toJson(builder.getAllPendenciesWithFiltersQuery(userActivityPendencyDto)));
+  }
+
+  @Test
+  public void getAllPendenciesByUserQuery() {
     final String EXPECTED_QUERY = "[{" +
       "\"$lookup\":{" +
       "\"from\":\"activity\"," +
@@ -51,7 +113,7 @@ public class UserActivityPendencyQueryBuilderTest {
   }
 
   @Test
-  public void getListOpenedPendenciesByUserQuery() {
+  public void getOpenedPendenciesByUserQuery() {
     final String EXPECTED_QUERY = "[{" +
       "\"$lookup\":{" +
       "\"from\":\"activity\"," +
@@ -61,8 +123,8 @@ public class UserActivityPendencyQueryBuilderTest {
       "\"$expr\":{" +
       "\"$and\":[" +
       "{\"$eq\":[\"$$activityId\",\"$_id\"]}," +
-      "{\"$eq\":[false,\"$isDiscarded\"]}" +
-      ",{\"$ne\":[\"" + UserActivityPendencyQueryBuilder.FINALIZED_STATUS + "\",{\"$arrayElemAt\":[\"$statusHistory.name\",-1.0]}]}" +
+      "{\"$eq\":[false,\"$isDiscarded\"]}," +
+      "{\"$ne\":[\"" + UserActivityPendencyQueryBuilder.FINALIZED_STATUS + "\",{\"$arrayElemAt\":[\"$statusHistory.name\",-1.0]}]}" +
       "]}}}," +
       "{\"$project\":{" +
       "\"recruitmentNumber\":\"$participantData.recruitmentNumber\"," +
@@ -84,7 +146,7 @@ public class UserActivityPendencyQueryBuilderTest {
   }
 
   @Test
-  public void getListDonePendenciesByUserQuery() {
+  public void getDonePendenciesByUserQuery() {
     final String EXPECTED_QUERY = "[{" +
       "\"$lookup\":{" +
       "\"from\":\"activity\"," +
