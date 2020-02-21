8ebcc14644fa0690afe6d68b43d400b63ca2a913
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-21 12:12:29 -0300

    OA-166 testes unitários para métodos da enum UserActivityPendencyFieldOrderingOptionsTest

---
+++ source/otus-user/src/test/java/br/org/otus/persistence/pendency/dto/UserActivityPendencyFieldOrderingOptionsTest.java
@@ -0,0 +1,31 @@
+package br.org.otus.persistence.pendency.dto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class UserActivityPendencyFieldOrderingOptionsTest {
+
+  @Test
+  public void contains_static_method_should_return_TRUE_for_each_enum_value(){
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.STATUS.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.ACRONYM.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.RECRUITMENT_NUMBER.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.EXTERNAL_ID.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.DUE_DATE.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.REQUESTER.getName()));
+    assertTrue(UserActivityPendencyFieldOrderingOptions.contains(UserActivityPendencyFieldOrderingOptions.RECEIVER.getName()));
+  }
+
+  @Test
+  public void contains_static_method_should_return_FALE_for_invalid_value(){
+    assertFalse(UserActivityPendencyFieldOrderingOptions.contains(""));
+  }
+
+  @Test
+  public void contains_static_method_should_return_FALE_for_null(){
+    assertFalse(UserActivityPendencyFieldOrderingOptions.contains(null));
+  }
+
+}
