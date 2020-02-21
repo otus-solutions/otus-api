5dfe7e7a42d6b25771715d8195e66f251c2f0911
FlaviaAP <flavia.avila@ufrgs.br>
2020-02-21 11:51:10 -0300

    OA-166 UserActivityPendencyDtoTest com testes ajustados após add atributo orderDto

--- source/otus-user/src/test/java/br/org/otus/persistence/pendency/UserActivityPendencyDtoTest.java
+++ source/otus-user/src/test/java/br/org/otus/persistence/pendency/UserActivityPendencyDtoTest.java
@@ -6,7 +6,8 @@ import br.org.otus.persistence.pendency.dto.UserActivityPendencyOrderDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -18,43 +19,29 @@ public class UserActivityPendencyDtoTest {
 
   private final static int CURRENT_QUANTITY = 100;
   private final static int QUANTITY_TO_GET = 50;
-
   private final static String FILTER_DTO_FIELD_NAME = "filterDto";
   private final static String ORDER_DTO_FIELD_NAME = "orderDto";
 
-  private UserActivityPendencyDto userActivityPendencyDto = new UserActivityPendencyDto();
-  private String userActivityPendencyDtoJson;
+  @InjectMocks
+  private UserActivityPendencyDto userActivityPendencyDto = PowerMockito.spy(new UserActivityPendencyDto());
 
-  @Mock
-  private UserActivityPendencyFilterDto userActivityPendencyFilterDto = new UserActivityPendencyFilterDto();
-  @Mock
-  private UserActivityPendencyOrderDto userActivityPendencyOrderDto = new UserActivityPendencyOrderDto();
+  private UserActivityPendencyFilterDto filterDto = PowerMockito.spy(new UserActivityPendencyFilterDto());
+  private UserActivityPendencyOrderDto orderDto = PowerMockito.spy(new UserActivityPendencyOrderDto());
 
   @Before
   public void setUp(){
     Whitebox.setInternalState(userActivityPendencyDto, "currentQuantity", CURRENT_QUANTITY);
     Whitebox.setInternalState(userActivityPendencyDto, "quantityToGet", QUANTITY_TO_GET);
-    Whitebox.setInternalState(userActivityPendencyDto, FILTER_DTO_FIELD_NAME, userActivityPendencyFilterDto);
-    Whitebox.setInternalState(userActivityPendencyDto, ORDER_DTO_FIELD_NAME, userActivityPendencyOrderDto);
-    userActivityPendencyDtoJson = UserActivityPendencyDto.serialize(userActivityPendencyDto);
+    Whitebox.setInternalState(userActivityPendencyDto, FILTER_DTO_FIELD_NAME, filterDto);
+    Whitebox.setInternalState(userActivityPendencyDto, ORDER_DTO_FIELD_NAME, orderDto);
   }
 
   @Test
   public void unit_test_for_invoke_getters(){
     assertEquals(CURRENT_QUANTITY, userActivityPendencyDto.getCurrentQuantity());
     assertEquals(QUANTITY_TO_GET, userActivityPendencyDto.getQuantityToGet());
-    assertEquals(userActivityPendencyFilterDto, userActivityPendencyDto.getFilterDto());
-    assertEquals(userActivityPendencyOrderDto, userActivityPendencyDto.getOrderDto());
-  }
-
-  @Test
-  public void serializeStaticMethdos_should_convert_objectModel_to_JsonString(){
-    assertTrue(UserActivityPendencyDto.serialize(userActivityPendencyDto) instanceof String);
-  }
-
-  @Test
-  public void deserializeStaticMethdos_should_convert_JsonString_to_objectModel(){
-    assertTrue(UserActivityPendencyDto.deserialize(userActivityPendencyDtoJson) instanceof UserActivityPendencyDto);
+    assertEquals(filterDto, userActivityPendencyDto.getFilterDto());
+    assertEquals(orderDto, userActivityPendencyDto.getOrderDto());
   }
 
   @Test
@@ -75,10 +62,25 @@ public class UserActivityPendencyDtoTest {
   }
 
   @Test
-  public void isValid_method_should_be_return_FALSE_if_filterDto_is_invalid(){
-    //TODO
-//    when(userActivityPendencyFilterDto.isValid()).thenReturn(false);
-//    assertFalse(userActivityPendencyDto.isValid());
+  public void isValid_method_should_be_return_FALSE_if_filterDto_is_invalid() throws Exception {
+    when(filterDto, "isValid").thenReturn(false);
+    assertFalse(userActivityPendencyDto.isValid());
+  }
+
+  @Test
+  public void isValid_method_should_be_return_FALSE_if_orderDto_is_invalid() throws Exception {
+    when(orderDto, "isValid").thenReturn(false);
+    assertFalse(userActivityPendencyDto.isValid());
+  }
+
+  @Test
+  public void serializeStaticMethdos_should_convert_objectModel_to_JsonString(){
+    assertTrue(UserActivityPendencyDto.serialize(new UserActivityPendencyDto()) instanceof String);
+  }
+
+  @Test
+  public void deserializeStaticMethdos_should_convert_JsonString_to_objectModel(){
+    assertTrue(UserActivityPendencyDto.deserialize("{}") instanceof UserActivityPendencyDto);
   }
 
 }
