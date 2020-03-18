e94d58a95670486baeac5ab5ede5d142e992a09e
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-18 14:51:13 -0300

    OA-163 classe ParticipantContactItemValue com novo atributo objectType e método toJson

--- source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/ParticipantContactItemValue.java
+++ source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/ParticipantContactItemValue.java
@@ -2,6 +2,14 @@ package org.ccem.otus.participant.model.participant_contact;
 
 public abstract class ParticipantContactItemValue {
 
+  protected String objectType;
+
+  public String getObjectType() {
+    return objectType;
+  }
+
   public abstract boolean isValid();
+
+  public abstract String toJson();
 
 }
