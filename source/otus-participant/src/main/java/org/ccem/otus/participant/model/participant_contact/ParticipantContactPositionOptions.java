332baa5ed0c5529ecb116a5b8738a4cfb4b91798
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-20 00:50:40 -0300

    OA-163 add atributo int ranking na enum ParticipantContactPositionOptions

--- source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/ParticipantContactPositionOptions.java
+++ source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/ParticipantContactPositionOptions.java
@@ -4,25 +4,41 @@ import java.util.Arrays;
 
 public enum ParticipantContactPositionOptions {
 
-  MAIN("main"),
-  SECOND("second"),
-  THIRD("third"),
-  FOURTH("fourth"),
-  FIFTH("fifth");
+  MAIN("main", 1),
+  SECOND("second", 2),
+  THIRD("third", 3),
+  FOURTH("fourth", 4),
+  FIFTH("fifth", 5);
 
   private String name;
+  private int ranking;
 
-  ParticipantContactPositionOptions(String name){
+  ParticipantContactPositionOptions(String name, int ranking){
     this.name = name;
+    this.ranking = ranking;
   }
 
   public String getName() {
     return name;
   }
 
+  public int getRanking() {
+    return ranking;
+  }
+
   public static boolean contains(String otherValue){
     return Arrays.asList(ParticipantContactPositionOptions.values()).stream()
       .map(ParticipantContactPositionOptions::getName)
       .anyMatch(otherValue::equals);
+  }
+
+  public static ParticipantContactPositionOptions fromString(String name){
+    //TODO write loop like contains method
+    for (ParticipantContactPositionOptions option : ParticipantContactPositionOptions.values()) {
+      if(option.getName().equals(name)){
+        return option;
+      }
+    }
+    throw new IllegalArgumentException("String name " + name + " is not a ParticipantContactTypeOptions valid enum");
   }
 }
