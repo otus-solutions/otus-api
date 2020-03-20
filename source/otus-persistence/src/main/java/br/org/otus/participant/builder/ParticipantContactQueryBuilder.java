f6ac2181da93476d9fe49a3e5e652b2294b0d7e9
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-19 22:59:20 -0300

    OA-163 exclusão de métodos não mais usados de ParticipantContactQueryBuilder

--- source/otus-persistence/src/main/java/br/org/otus/participant/builder/ParticipantContactQueryBuilder.java
+++ source/otus-persistence/src/main/java/br/org/otus/participant/builder/ParticipantContactQueryBuilder.java
@@ -12,33 +12,5 @@ public class ParticipantContactQueryBuilder {
 
   private ArrayList<Bson> pipeline;
 
-  public ArrayList<Document> participantContactToDocuments(ParticipantContact participantContact) {
-    ArrayList<Document> documents = new ArrayList<>();
-    for (ParticipantContactTypeOptions contactType : ParticipantContactTypeOptions.values()) {
-      for (ParticipantContactPositionOptions position : ParticipantContactPositionOptions.values()) {
-        ParticipantContactItem participantContactItem = participantContact.getParticipantContactsItemByType(contactType)
-          .getItemByPosition(position);
-        if (participantContactItem == null)
-          break;
-        documents.add(participantContactItemToDocument(participantContactItem, participantContact.getRecruitmentNumber(), contactType, position));
-      }
-    }
-    return documents;
-  }
-
-  public Document participantContactItemToDocument(ParticipantContactItem participantContactItem, Long recruitmentNumber,
-                                                    ParticipantContactTypeOptions contactType, ParticipantContactPositionOptions position){
-      Document doc = new Document();
-      doc.put("recruitmentNumber", recruitmentNumber);
-      doc.put("position", position.toString());
-      doc.put(contactType.toString().toLowerCase(), Document.parse(participantContactItem.getValue().toJson()));
-      doc.put("observation", participantContactItem.getObservation());
-      return doc;
-  }
-
-  public void getByRecruitmentNumber(Long recruitmentNumber) {
-
-  }
-
 
 }
