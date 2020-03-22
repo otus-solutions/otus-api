d88163c3cd5843881b3a6c77c814e727a78aff57
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-21 23:28:58 -0300

    OA-163 testes unitários de ParticipantContactFacade (100%)

--- source/otus-business/src/test/java/br/org/otus/participant/api/ParticipantContactFacadeTest.java
+++ source/otus-business/src/test/java/br/org/otus/participant/api/ParticipantContactFacadeTest.java
@@ -15,6 +15,8 @@ import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.zip.DataFormatException;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -32,115 +34,218 @@ public class ParticipantContactFacadeTest {
   private ParticipantContact participantContact;
   private ObjectId participantContactOID;
   private String participantContactJson;
-  private DataNotFoundException dataNotFoundException;
+  private DataNotFoundException dataNotFoundException = PowerMockito.spy(new DataNotFoundException());
+  private DataFormatException dataFormatException = PowerMockito.spy(new DataFormatException());
 
   @Before
   public void setUp(){
     participantContact = new ParticipantContact();
     participantContactOID = new ObjectId(PARTICIPANT_CONTACT_ID);
     participantContactJson = ParticipantContact.serialize(participantContact);
-    dataNotFoundException = PowerMockito.spy(new DataNotFoundException());
   }
 
-//  @Test
-//  public void create_method_should_invoke_create_from_participantContactService() {
-//    when(participantContactService.create(Mockito.any())).thenReturn(participantContactOID);
-//    assertEquals(PARTICIPANT_CONTACT_ID, participantContactFacade.create(participantContactJson));
-//  }
-//
-//  @Test
-//  public void updateMainContact_method_should_invoke_updateMainContact_from_participantContactService() throws Exception {
-//    participantContactFacade.updateMainContact(participantContactJson);
-//    verify(participantContactService, times(1)).updateMainContact(Mockito.any());
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void updateMainContact_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "updateMainContact", Mockito.any());
-//    participantContactFacade.updateMainContact(participantContactJson);
-//  }
-//
-//  @Test
-//  public void addNonMainEmail_method_should_invoke_addNonMainEmail_from_participantContactService() throws Exception {
-//    participantContactFacade.addNonMainEmail(participantContactJson);
-//    verify(participantContactService, times(1)).addNonMainEmail(Mockito.any());
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void addNonMainEmail_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "addNonMainEmail", Mockito.any());
-//    participantContactFacade.addNonMainEmail(participantContactJson);
-//  }
-//
-//  @Test
-//  public void updateSecondaryContact_method_should_invoke_updateSecondaryContact_from_participantContactService() throws Exception {
-//    participantContactFacade.updateSecondaryContact(participantContactJson);
-//    verify(participantContactService, times(1)).updateSecondaryContact(Mockito.any());
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void updateSecondaryContact_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "updateSecondaryContact", Mockito.any());
-//    participantContactFacade.updateSecondaryContact(participantContactJson);
-//  }
-//
-//  @Test
-//  public void swapMainContactWithSecondary_method_should_invoke_swapMainContactWithSecondary_from_participantContactService() throws Exception {
-//    participantContactFacade.swapMainContactWithSecondary(participantContactJson);
-//    verify(participantContactService, times(1)).swapMainContactWithSecondary(Mockito.any());
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void swapMainContactWithSecondary_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "swapMainContactWithSecondary", Mockito.any());
-//    participantContactFacade.swapMainContactWithSecondary(participantContactJson);
-//  }
-//
-//  @Test
-//  public void delete_method_should_invoke_delete_from_participantContactService() throws Exception {
-//    participantContactFacade.delete(PARTICIPANT_CONTACT_ID);
-//    verify(participantContactService, times(1)).delete(participantContactOID);
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void delete_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "delete", participantContactOID);
-//    participantContactFacade.delete(PARTICIPANT_CONTACT_ID);
-//  }
-//
-//  @Test
-//  public void deleteNonMainContact_method_should_invoke_deleteNonMainContact_from_participantContactService() throws Exception {
-//    participantContactFacade.deleteNonMainContact(participantContactJson);
-//    verify(participantContactService, times(1)).deleteNonMainContact(Mockito.any());
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void deleteNonMainContact_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "deleteNonMainContact", Mockito.any());
-//    participantContactFacade.deleteNonMainContact(participantContactJson);
-//  }
-//
-//  @Test
-//  public void get_method_should_invoke_get_from_participantContactService() throws Exception {
-//    participantContactFacade.get(PARTICIPANT_CONTACT_ID);
-//    verify(participantContactService, times(1)).get(participantContactOID);
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void get_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "get", participantContactOID);
-//    participantContactFacade.get(PARTICIPANT_CONTACT_ID);
-//  }
-//
-//  @Test
-//  public void getByRecruitmentNumber_method_should_invoke_getByRecruitmentNumber_from_participantContactService() throws Exception {
-//    participantContactFacade.getByRecruitmentNumber(RN.toString());
-//    verify(participantContactService, times(1)).getByRecruitmentNumber(RN);
-//  }
-//
-//  @Test(expected = HttpResponseException.class)
-//  public void getByRecruitmentNumber_method_should_handle_DataNotFoundException() throws Exception {
-//    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "getByRecruitmentNumber", RN);
-//    participantContactFacade.getByRecruitmentNumber(RN.toString());
-//  }
+  @Test
+  public void create_method_should_invoke_create_from_participantContactService() throws DataFormatException {
+    when(participantContactService.create(Mockito.any())).thenReturn(participantContactOID);
+    assertEquals(PARTICIPANT_CONTACT_ID, participantContactFacade.create(participantContactJson));
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void create_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "create", Mockito.any());
+    participantContactFacade.create(participantContactJson);
+  }
+
+  @Test
+  public void addNonMainEmail_method_should_invoke_addNonMainEmail_from_participantContactService() throws Exception {
+    participantContactFacade.addNonMainEmail(participantContactJson);
+    verify(participantContactService, times(1)).addNonMainEmail(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainEmail_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "addNonMainEmail", Mockito.any());
+    participantContactFacade.addNonMainEmail(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainEmail_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "addNonMainEmail", Mockito.any());
+    participantContactFacade.addNonMainEmail(participantContactJson);
+  }
+
+
+  @Test
+  public void addNonMainAddress_method_should_invoke_addNonMainAddress_from_participantContactService() throws Exception {
+    participantContactFacade.addNonMainAddress(participantContactJson);
+    verify(participantContactService, times(1)).addNonMainAddress(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainAddress_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "addNonMainAddress", Mockito.any());
+    participantContactFacade.addNonMainAddress(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainAddress_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "addNonMainAddress", Mockito.any());
+    participantContactFacade.addNonMainAddress(participantContactJson);
+  }
+
+
+  @Test
+  public void addNonMainPhoneNumber_method_should_invoke_addNonMainPhoneNumber_from_participantContactService() throws Exception {
+    participantContactFacade.addNonMainPhoneNumber(participantContactJson);
+    verify(participantContactService, times(1)).addNonMainPhoneNumber(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainPhoneNumber_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "addNonMainPhoneNumber", Mockito.any());
+    participantContactFacade.addNonMainPhoneNumber(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void addNonMainPhoneNumber_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "addNonMainPhoneNumber", Mockito.any());
+    participantContactFacade.addNonMainPhoneNumber(participantContactJson);
+  }
+
+  @Test
+  public void updateEmail_method_should_invoke_updateEmail_from_participantContactService() throws Exception {
+    participantContactFacade.updateEmail(participantContactJson);
+    verify(participantContactService, times(1)).updateEmail(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updateEmail_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "updateEmail", Mockito.any());
+    participantContactFacade.updateEmail(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updateEmail_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "updateEmail", Mockito.any());
+    participantContactFacade.updateEmail(participantContactJson);
+  }
+
+
+  @Test
+  public void updateAddress_method_should_invoke_updateAddress_from_participantContactService() throws Exception {
+    participantContactFacade.updateAddress(participantContactJson);
+    verify(participantContactService, times(1)).updateAddress(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updateAddress_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "updateAddress", Mockito.any());
+    participantContactFacade.updateAddress(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updateAddress_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "updateAddress", Mockito.any());
+    participantContactFacade.updateAddress(participantContactJson);
+  }
+
+
+  @Test
+  public void updatePhoneNumber_method_should_invoke_updatePhoneNumber_from_participantContactService() throws Exception {
+    participantContactFacade.updatePhoneNumber(participantContactJson);
+    verify(participantContactService, times(1)).updatePhoneNumber(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updatePhoneNumber_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "updatePhoneNumber", Mockito.any());
+    participantContactFacade.updatePhoneNumber(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void updatePhoneNumber_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "updatePhoneNumber", Mockito.any());
+    participantContactFacade.updatePhoneNumber(participantContactJson);
+  }
+
+
+  @Test
+  public void swapMainContact_method_should_invoke_swapMainContact_from_participantContactService() throws Exception {
+    participantContactFacade.swapMainContact(participantContactJson);
+    verify(participantContactService, times(1)).swapMainContact(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void swapMainContact_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "swapMainContact", Mockito.any());
+    participantContactFacade.swapMainContact(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void swapMainContact_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "swapMainContact", Mockito.any());
+    participantContactFacade.swapMainContact(participantContactJson);
+  }
+
+
+  @Test
+  public void delete_method_should_invoke_delete_from_participantContactService() throws Exception {
+    participantContactFacade.delete(PARTICIPANT_CONTACT_ID);
+    verify(participantContactService, times(1)).delete(participantContactOID);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void delete_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "delete", participantContactOID);
+    participantContactFacade.delete(PARTICIPANT_CONTACT_ID);
+  }
+
+
+  @Test
+  public void deleteNonMainContact_method_should_invoke_deleteNonMainContact_from_participantContactService() throws Exception {
+    participantContactFacade.deleteNonMainContact(participantContactJson);
+    verify(participantContactService, times(1)).deleteNonMainContact(Mockito.any());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void deleteNonMainContact_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "deleteNonMainContact", Mockito.any());
+    participantContactFacade.deleteNonMainContact(participantContactJson);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void deleteNonMainContact_method_should_handle_DataFormatException() throws Exception {
+    PowerMockito.doThrow(dataFormatException).when(participantContactService, "deleteNonMainContact", Mockito.any());
+    participantContactFacade.deleteNonMainContact(participantContactJson);
+  }
+
+  @Test
+  public void get_method_should_invoke_get_from_participantContactService() throws Exception {
+    participantContactFacade.getParticipantContact(PARTICIPANT_CONTACT_ID);
+    verify(participantContactService, times(1)).getParticipantContact(participantContactOID);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void get_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "getParticipantContact", participantContactOID);
+    participantContactFacade.getParticipantContact(PARTICIPANT_CONTACT_ID);
+  }
+
+  @Test
+  public void getByRecruitmentNumber_method_should_invoke_getByRecruitmentNumber_from_participantContactService() throws Exception {
+    participantContactFacade.getParticipantContactByRecruitmentNumber(RN.toString());
+    verify(participantContactService, times(1)).getParticipantContactByRecruitmentNumber(RN);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void getByRecruitmentNumber_method_should_handle_DataNotFoundException() throws Exception {
+    PowerMockito.doThrow(dataNotFoundException).when(participantContactService, "getParticipantContactByRecruitmentNumber", RN);
+    participantContactFacade.getParticipantContactByRecruitmentNumber(RN.toString());
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void getByRecruitmentNumber_method_should_handle_NumberFormatException() throws Exception {
+    PowerMockito.doThrow(new NumberFormatException()).when(participantContactService, "getParticipantContactByRecruitmentNumber", RN);
+    participantContactFacade.getParticipantContactByRecruitmentNumber(RN.toString());
+  }
 }
