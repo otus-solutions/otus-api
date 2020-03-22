ad7bf39eedf39e3640f70467229da2101e213d97
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-21 21:53:48 -0300

    OA-163 renomação de parametros dos métodos (de)serialize de Email

--- source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/Email.java
+++ source/otus-participant/src/main/java/org/ccem/otus/participant/model/participant_contact/Email.java
@@ -2,12 +2,12 @@ package org.ccem.otus.participant.model.participant_contact;
 
 public class Email extends ParticipantContactItemValueString {
 
-  public static String serialize(Email participantContactItems){
-    return getGsonBuilder().create().toJson(participantContactItems);
+  public static String serialize(Email email){
+    return getGsonBuilder().create().toJson(email);
   }
 
-  public static Email deserialize(String participantContactItemsJson){
-    return getGsonBuilder().create().fromJson(participantContactItemsJson, Email.class);
+  public static Email deserialize(String emailJson){
+    return getGsonBuilder().create().fromJson(emailJson, Email.class);
   }
 
   @Override
