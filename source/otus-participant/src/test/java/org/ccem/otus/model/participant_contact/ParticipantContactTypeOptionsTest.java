809414998a1f220037c55f3deb61fd403aa014ab
FlaviaAP <flavia.avila@ufrgs.br>
2020-03-21 21:17:33 -0300

    OA-163 testes untários das enums ParticipantContactPositionOptionsTest e ParticipantContactTypeOptionsTest (100% cada)

--- source/otus-participant/src/test/java/org/ccem/otus/model/participant_contact/ParticipantContactTypeOptionsTest.java
+++ source/otus-participant/src/test/java/org/ccem/otus/model/participant_contact/ParticipantContactTypeOptionsTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(PowerMockRunner.class)
 public class ParticipantContactTypeOptionsTest {
+
+  private static final String INVALID_STRING_VALUE = "0";
 
   @Test
   public void contains_static_method_should_return_TRUE_for_each_enum_value(){
@@ -20,6 +22,18 @@ public class ParticipantContactTypeOptionsTest {
 
   @Test
   public void contains_static_method_should_return_FALSE_for_invalid_value(){
-    assertFalse(ParticipantContactTypeOptions.contains(""));
+    assertFalse(ParticipantContactTypeOptions.contains(INVALID_STRING_VALUE));
+  }
+
+  @Test
+  public void fromString_static_method_should_convert_valid_string_value_to_enum(){
+    assertEquals(ParticipantContactTypeOptions.EMAIL, ParticipantContactTypeOptions.fromString(ParticipantContactTypeOptions.EMAIL.getName()));
+    assertEquals(ParticipantContactTypeOptions.ADDRESS, ParticipantContactTypeOptions.fromString(ParticipantContactTypeOptions.ADDRESS.getName()));
+    assertEquals(ParticipantContactTypeOptions.PHONE, ParticipantContactTypeOptions.fromString(ParticipantContactTypeOptions.PHONE.getName()));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void fromString_static_method_throw_IllegalArgumentException_in_case_invalid_string_value(){
+    ParticipantContactTypeOptions.fromString(INVALID_STRING_VALUE);
   }
 }
