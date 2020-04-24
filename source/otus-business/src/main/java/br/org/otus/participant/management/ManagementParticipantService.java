f9c4fc80a9fbe3dc22a7896dafb73fbc8f782e77
Fabiano Ramires <fdrtec@gmail.com>
2020-04-24 14:56:21 -0300

    OA-182 estrutura base para alcançar serviço de envio de email

---
+++ source/otus-business/src/main/java/br/org/otus/participant/management/ManagementParticipantService.java
@@ -0,0 +1,4 @@
+package br.org.otus.participant.management;
+
+public interface ManagementParticipantService {
+}
---
+++ source/otus-business/src/main/java/br/org/otus/participant/management/ManagementParticipantServiceBean.java
@@ -0,0 +1,4 @@
+package br.org.otus.participant.management;
+
+public class ManagementParticipantServiceBean {
+}
