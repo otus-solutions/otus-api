24f5d2a9dd2f52820fd971a188edd9b6595d3c4b
Fabiano Ramires <fdrtec@gmail.com>
2020-05-04 14:29:51 -0300

    OA-182: 2 unitTest para requestParticipantPasswordResetby securityFacade

---
+++ source/otus-business/src/test/java/br/org/otus/participant/management/ManagementParticipantServiceBeanTest.java
@@ -0,0 +1,12 @@
+package br.org.otus.participant.management;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class ManagementParticipantServiceBeanTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
