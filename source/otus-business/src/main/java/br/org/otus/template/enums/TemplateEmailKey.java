6082ba40c466ceb3a9d1fe85b7e9a04f8524944b
Breno Scheffer <brenoscheffer@gmail.com>
2020-05-13 21:08:43 -0300

    OA-187 Mudança do id de template

--- source/otus-business/src/main/java/br/org/otus/template/enums/TemplateEmailKey.java
+++ source/otus-business/src/main/java/br/org/otus/template/enums/TemplateEmailKey.java
@@ -2,7 +2,7 @@ package br.org.otus.template.enums;
 
 public enum TemplateEmailKey {
   TEMPLATE_RESET_PASSWD_PARTICIPANT_ID("5ea88862ae51d800083aeba7"),
-  TEMPLATE_ACTIVITY_SENDING_ID("5ea88862ae51d800083aeba7");
+  TEMPLATE_ACTIVITY_SENDING_ID("5e1e2ef297369acec0f34429");
 
   private String value;
 
