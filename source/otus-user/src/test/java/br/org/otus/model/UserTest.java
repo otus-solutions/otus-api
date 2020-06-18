60e34d9fa319fe1d9fcc9c24b958fb06481da8d9
Fabiano Ramires <fdrtec@gmail.com>
2020-06-18 14:51:09 -0300

    OA-190 tratamento lowerCase no atributo de email do user e conserto em test do UserCodec

---
+++ source/otus-user/src/test/java/br/org/otus/model/UserTest.java
@@ -0,0 +1,11 @@
+package br.org.otus.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+public class UserTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
