6e3f1d98948d2bf49e0dfba011d524f6e133252b
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-24 11:42:17 -0300

    OA-185 correção dos formatos de data em DataAdapater

--- source/otus-commons/src/main/java/org/ccem/otus/utils/DateAdapter.java
+++ source/otus-commons/src/main/java/org/ccem/otus/utils/DateAdapter.java
@@ -1,15 +1,33 @@
 package org.ccem.otus.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class DateAdapter {
 
-  public static String getNowAsISODate(){
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-    sdf.setTimeZone(TimeZone.getTimeZone("CET"));
-    return sdf.format(new Date());
+  private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+  private static final String TIME_ZONE = "UTC";
+
+  public static String toISODate(Date date){
+    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    dateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
+    return dateFormat.format(date);
+  }
+
+  public static String nowToISODate(){
+    return toISODate(new Date());
+  }
+
+  public static String getDatePlusDays(String dateStr, int days) throws ParseException {
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    dateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
+    calendar.setTime(dateFormat.parse(dateStr));
+    calendar.add(Calendar.DATE, days);
+    return toISODate(calendar.getTime());
   }
 
 }
