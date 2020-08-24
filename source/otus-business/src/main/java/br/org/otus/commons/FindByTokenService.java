95fdcbceb2c9b5bc252f370a212b966f60120e79
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-24 13:19:49 -0300

    OA-185 refatoração de FindByTokenService
    e add método para encotnrar User ou Participant pelo token

--- source/otus-business/src/main/java/br/org/otus/commons/FindByTokenService.java
+++ source/otus-business/src/main/java/br/org/otus/commons/FindByTokenService.java
@@ -13,47 +13,50 @@ import java.text.ParseException;
 
 public class FindByTokenService {
 
+  private static final String USER_MODE = "user";
+  private static final String PARTICIPANT_MODE = "participant";
+  private static final String INVALID_MODE_MESSAGE = "Invalid Mode";
+
   @Inject
   private ParticipantService participantService;
 
   @Inject
   private ManagementUserService managementUserService;
 
-  public String findUserIdByToken(String token) throws DataNotFoundException, ValidationException, ParseException {
-    final String mode = getTokenMode(token);
-    final String email = getTokenEmail(token);
 
-    if (!mode.equals("user")) {
-      throw new ValidationException("Invalid Mode");
+  public User findUserByToken(String token) throws DataNotFoundException, ValidationException, ParseException {
+    if (!getTokenMode(token).equals(USER_MODE)) {
+      throw new ValidationException(INVALID_MODE_MESSAGE);
     }
-
-    User user = managementUserService.fetchByEmail(email);
-    return String.valueOf(user.get_id());
+    return managementUserService.fetchByEmail(getTokenEmail(token));
   }
 
-  public String findParticipantIdByToken(String token) throws DataNotFoundException, ValidationException, ParseException {
-    final String mode = getTokenMode(token);
-    final String email = getTokenEmail(token);
-
-    if (!mode.equals("participant")) {
-      throw new ValidationException("Invalid Mode");
+  public Participant findParticipantByToken(String token) throws DataNotFoundException, ValidationException, ParseException {
+    if (!getTokenMode(token).equals(PARTICIPANT_MODE)) {
+      throw new ValidationException(INVALID_MODE_MESSAGE);
     }
-
-    Participant participant = participantService.getByEmail(email);
-    return String.valueOf(participant.getId());
+    return participantService.getByEmail(getTokenEmail(token));
   }
 
+  public Object findPersonByToken(String token) throws ParseException, DataNotFoundException, ValidationException {
+    switch (getTokenMode(token)){
+      case USER_MODE:
+        return managementUserService.fetchByEmail(getTokenEmail(token));
+      case PARTICIPANT_MODE:
+        return participantService.getByEmail(getTokenEmail(token));
+      default:
+        throw new ValidationException(INVALID_MODE_MESSAGE);
+    }
+  }
 
   private String getTokenEmail(String token) throws ParseException {
     SignedJWT signedJWT = SignedJWT.parse(token);
-    String email = signedJWT.getJWTClaimsSet().getClaim("iss").toString();
-    return email;
+    return signedJWT.getJWTClaimsSet().getClaim("iss").toString();
   }
 
   private String getTokenMode(String token) throws ParseException {
     SignedJWT signedJWT = SignedJWT.parse(token);
-    String mode = signedJWT.getJWTClaimsSet().getClaim("mode").toString();
-    return mode;
+    return signedJWT.getJWTClaimsSet().getClaim("mode").toString();
   }
 
 }
