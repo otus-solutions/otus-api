20cc96f43451216fa18762c9548682c8e798ca8e
FlaviaAP <flavia.avila@ufrgs.br>
2020-08-27 10:43:23 -0300

    OA-185 atualização de testes de ActivitySharingServiceBean

--- source/otus-activity/src/test/java/org/ccem/otus/service/ActivitySharingServiceBeanTest.java
+++ source/otus-activity/src/test/java/org/ccem/otus/service/ActivitySharingServiceBeanTest.java
@@ -43,13 +43,17 @@ public class ActivitySharingServiceBeanTest {
     assertNotNull(activitySharingServiceBean.getSharedURL(activitySharing));
   }
 
-  @Test
+  @Test(expected = DataNotFoundException.class)
   public void getSharedURL_method_should_handle_DataNotFoundException_from_activitySharingDao_get_method() throws DataNotFoundException {
     when(activitySharingDao.getSharedURL(ACTIVITY_OID)).thenThrow(new DataNotFoundException());
+    activitySharingServiceBean.getSharedURL(activitySharing);
+  }
+
+  
+  @Test
+  public void createSharedURL_method_should_return_url() {
     when(activitySharingDao.createSharedURL(activitySharing)).thenReturn(activitySharing);
-    assertNotNull(activitySharingServiceBean.getSharedURL(activitySharing));
-    verify(activitySharingDao, Mockito.times(1)).getSharedURL(ACTIVITY_OID);
-    verify(activitySharingDao, Mockito.times(1)).createSharedURL(activitySharing);
+    assertNotNull(activitySharingServiceBean.createSharedURL(activitySharing));
   }
 
 
