45f7ae96940baddabb3e2aed21edfb2c9dd8e4e0
FlaviaAP <flavia.avila@ufrgs.br>
2020-09-02 17:16:18 -0300

    OA-185 novo método static DateUtil.before

--- source/otus-commons/src/main/java/org/ccem/otus/utils/DateUtil.java
+++ source/otus-commons/src/main/java/org/ccem/otus/utils/DateUtil.java
@@ -34,4 +34,20 @@ public class DateUtil {
     return dateStr1.substring(0, 11).equals(dateStr2.substring(0, 11));
   }
 
+  public static boolean before(String dateStr1, String dateStr2) throws ParseException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+    dateFormat.setTimeZone(TimeZone.getTimeZone(TIME_ZONE));
+
+    Calendar calendar1 = Calendar.getInstance();
+    calendar1.setTime(dateFormat.parse(dateStr1));
+
+    Calendar calendar2 = Calendar.getInstance();
+    calendar2.setTime(dateFormat.parse(dateStr2));
+
+    System.out.println(calendar1.getTime());
+    System.out.println(calendar2.getTime());
+
+    return calendar1.before(calendar2);
+  }
+
 }
