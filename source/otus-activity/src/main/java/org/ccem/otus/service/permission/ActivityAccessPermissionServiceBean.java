db0eabea868c456fb1186fb96aef8c0d6b28b7a7
FlaviaAP <flavia.avila@ufrgs.br>
2020-09-09 17:18:22 -0300

    OA-197 novo método ActivityAccessPermissionService.get

--- source/otus-activity/src/main/java/org/ccem/otus/service/permission/ActivityAccessPermissionServiceBean.java
+++ source/otus-activity/src/main/java/org/ccem/otus/service/permission/ActivityAccessPermissionServiceBean.java
@@ -5,19 +5,15 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
 import org.ccem.otus.model.survey.activity.permission.ActivityAccessPermission;
 import org.ccem.otus.persistence.ActivityAccessPermissionDao;
 
 @Stateless
-class ActivityAccessPermissionServiceBean implements ActivityAccessPermissionService {
+public class ActivityAccessPermissionServiceBean implements ActivityAccessPermissionService {
 
   @Inject
   private ActivityAccessPermissionDao activityAccessPermissionDao;
-
-  @Override
-  public List<ActivityAccessPermission> list() {
-    return activityAccessPermissionDao.find();
-  }
 
   @Override
   public void create(ActivityAccessPermission activityAccessPermission) {
@@ -28,4 +24,15 @@ class ActivityAccessPermissionServiceBean implements ActivityAccessPermissionSer
   public void update(ActivityAccessPermission activityAccessPermission) {
     activityAccessPermissionDao.update(activityAccessPermission);
   }
+
+  @Override
+  public List<ActivityAccessPermission> list() {
+    return activityAccessPermissionDao.find();
+  }
+
+  @Override
+  public ActivityAccessPermission get(String acronym, Integer version) throws DataNotFoundException {
+    return activityAccessPermissionDao.get(acronym, version);
+  }
+
 }
