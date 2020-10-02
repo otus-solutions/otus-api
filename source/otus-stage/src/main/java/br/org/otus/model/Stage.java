fbf651f43554bae3ffa6c0f7b29a1ea2111a1eba
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-02 16:46:01 -0300

    OA-205 add método estático getFrontGsonBuilder na classe Stage

--- source/otus-stage/src/main/java/br/org/otus/model/Stage.java
+++ source/otus-stage/src/main/java/br/org/otus/model/Stage.java
@@ -4,6 +4,7 @@ import com.google.gson.GsonBuilder;
 
 import com.google.gson.annotations.SerializedName;
 import org.bson.types.ObjectId;
+import org.ccem.otus.utils.ObjectIdToStringAdapter;
 
 public class Stage {
 
@@ -34,6 +35,12 @@ public class Stage {
 
   public static String serialize(Stage stage) {
     return new GsonBuilder().create().toJson(stage);
+  }
+
+  public static GsonBuilder getFrontGsonBuilder() {
+    GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
+    return builder;
   }
 
 }
