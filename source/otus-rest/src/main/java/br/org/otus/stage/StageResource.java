a0c201a48640886a47b5bdef0ce181792929c7b5
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-02 16:49:23 -0300

    OA-205 nova classe StageResource

---
+++ source/otus-rest/src/main/java/br/org/otus/stage/StageResource.java
@@ -0,0 +1,58 @@
+package br.org.otus.stage;
+
+import br.org.otus.model.Stage;
+import br.org.otus.rest.Response;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("stage")
+public class StageResource {
+
+  @Inject
+  private StageFacade stageFacade;
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String create(String stageJson){
+    String stageId = stageFacade.create(stageJson);
+    return (new Response()).buildSuccess(stageId).toJson();
+  }
+
+  @PUT
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String update(String stageJson){
+    stageFacade.update(stageJson);
+    return (new Response()).buildSuccess().toJson();
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String delete(@PathParam("id") String stageID){
+    stageFacade.delete(stageID);
+    return (new Response()).buildSuccess().toJson();
+  }
+
+  @GET
+  @Path("/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getByID(@PathParam("id") String stageID){
+    Stage stage = stageFacade.getByID(stageID);
+    return (new Response()).buildSuccess(stage).toJson(Stage.getFrontGsonBuilder());
+  }
+
+  @GET
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getAll(){
+    List<Stage> stages = stageFacade.getAll();
+    return (new Response()).buildSuccess(stages).toJson(Stage.getFrontGsonBuilder());
+  }
+}
