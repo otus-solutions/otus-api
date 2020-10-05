1090c14bd3494cf7e5c25e3081092029e4fb46c0
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-05 11:52:49 -0300

    OA-205 testes unitários de StageServiceBean

---
+++ source/otus-configuration/src/test/java/service/StageServiceBeanTest.java
@@ -0,0 +1,67 @@
+package service;
+
+import model.Stage;
+import org.bson.types.ObjectId;
+import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
+import org.ccem.otus.exceptions.webservice.common.MemoryExcededException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import persistence.StageDao;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(PowerMockRunner.class)
+public class StageServiceBeanTest {
+
+  private static final ObjectId STAGE_OID = new ObjectId("5f77920624439758ce4a43ab");
+
+  @InjectMocks
+  private StageServiceBean stageServiceBean;
+
+  @Mock
+  private StageDao stageDao;
+
+  private Stage stage;
+
+  @Before
+  public void setUp(){
+    stage = new Stage();
+    stage.setId(STAGE_OID);
+  }
+
+  @Test
+  public void create_method_shoul_call_dao_create_method(){
+    stageServiceBean.create(stage);
+    verify(stageDao, Mockito.times(1)).create(stage);
+  }
+
+  @Test
+  public void update_method_shoul_call_dao_update_method() throws DataNotFoundException {
+    stageServiceBean.update(stage);
+    verify(stageDao, Mockito.times(1)).update(stage);
+  }
+
+  @Test
+  public void delete_method_shoul_call_dao_delete_method() throws DataNotFoundException {
+    stageServiceBean.delete(STAGE_OID);
+    verify(stageDao, Mockito.times(1)).delete(STAGE_OID);
+  }
+
+  @Test
+  public void getByID_method_shoul_call_dao_getByID_method() throws DataNotFoundException {
+    stageServiceBean.getByID(STAGE_OID);
+    verify(stageDao, Mockito.times(1)).getByID(STAGE_OID);
+  }
+
+  @Test
+  public void getAll_method_shoul_call_dao_getAll_method() throws MemoryExcededException {
+    stageServiceBean.getAll();
+    verify(stageDao, Mockito.times(1)).getAll();
+  }
+
+}
