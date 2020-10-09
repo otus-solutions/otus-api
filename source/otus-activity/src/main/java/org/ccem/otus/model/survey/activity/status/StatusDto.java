ab33f75fffd4c5ab690efb973b2a67abab62cf14
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-09 18:09:34 -0300

    OA-204 refatoração de StatusDto, add método deserialize

--- source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/status/StatusDto.java
+++ source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/status/StatusDto.java
@@ -1,21 +1,16 @@
 package org.ccem.otus.model.survey.activity.status;
 
-import org.ccem.otus.model.SerializableModelWithID;
+import org.ccem.otus.model.SerializableModel;
 import org.ccem.otus.model.survey.activity.User;
 
-public class StatusDto extends SerializableModelWithID {
+public class StatusDto extends SerializableModel {
 
   private String name;
   private User user;
   private String date;
 
 
-  public StatusDto() {
-  }
-
-  public StatusDto(ActivityStatus activityStatus) {
-    name = activityStatus.getName();
-    user = activityStatus.getUser();
-    date = activityStatus.getDate().toString();
+  public static StatusDto deserialize(String json){
+    return (StatusDto)deserialize(json, StatusDto.class);
   }
 }
