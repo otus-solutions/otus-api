cad5eec36a1bac6ba43d2826a1ada9a20852eac3
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-13 11:21:15 -0300

    OA-204 add métodos hasAcronyms,removeAcronymsWithoutActivities em StageSurveyActivitiesDto
    para extrair dados de dentro dos objetos "_id" gerados pelo group do mongo

--- source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageSurveyActivitiesDto.java
+++ source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageSurveyActivitiesDto.java
@@ -4,6 +4,9 @@ import com.google.gson.annotations.SerializedName;
 import org.bson.types.ObjectId;
 import org.ccem.otus.model.SerializableModelWithID;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StageSurveyActivitiesDto extends SerializableModelWithID {
 
   @SerializedName("_id")
@@ -11,14 +14,14 @@ public class StageSurveyActivitiesDto extends SerializableModelWithID {
 
   private String stageName;
 
-  private StageAcronymSurveyActivitiesDto[] acronyms;
+  private List<StageAcronymSurveyActivitiesDto> acronyms;
 
 
   public ObjectId getStageID() {
     return stageID;
   }
 
-  public StageAcronymSurveyActivitiesDto[] getAcronyms() {
+  public List<StageAcronymSurveyActivitiesDto> getAcronyms() {
     return acronyms;
   }
 
@@ -28,6 +31,19 @@ public class StageSurveyActivitiesDto extends SerializableModelWithID {
 
   public static StageSurveyActivitiesDto deserialize(String json){
     return (StageSurveyActivitiesDto) SerializableModelWithID.deserialize(json, StageSurveyActivitiesDto.class);
+  }
+
+
+  public boolean hasAcronyms(){
+    return (acronyms != null && !acronyms.isEmpty());
+  }
+
+  public void removeAcronymsWithoutActivities(){
+    acronyms = acronyms.stream().filter(dto -> dto.hasActivities()).collect(Collectors.toList());
+
+    for(StageAcronymSurveyActivitiesDto dto : acronyms){
+      dto.removeAcronymGroup();
+    }
   }
 
 }
