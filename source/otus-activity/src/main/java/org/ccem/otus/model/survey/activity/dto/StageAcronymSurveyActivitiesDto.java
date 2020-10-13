951fd6dc967e9cc8ba315e737a1c3b92b0a86a4c
FlaviaAP <flavia.avila@ufrgs.br>
2020-10-13 11:19:03 -0300

    OA-204 add métodos hasActivities, removeAcronymGroup em StageAcronymSurveyActivitiesDto
    para extrair dados de dentro dos objetos "_id" gerados pelo group do mongo

--- source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageAcronymSurveyActivitiesDto.java
+++ source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageAcronymSurveyActivitiesDto.java
@@ -3,24 +3,33 @@ package org.ccem.otus.model.survey.activity.dto;
 import com.google.gson.annotations.SerializedName;
 import org.ccem.otus.model.SerializableModel;
 
+import java.util.List;
+
 public class StageAcronymSurveyActivitiesDto extends SerializableModel {
 
   @SerializedName("_id")
   private AcronymGroup acronymGroup;
-
-  private SurveyActivityItemListDto[] activities;
 
   private String acronym;
 
   @SerializedName("name")
   private String activityName;
 
+  private List<SurveyActivityItemListDto> activities;
+
 
   public static StageAcronymSurveyActivitiesDto deserialize(String json){
-    StageAcronymSurveyActivitiesDto dto = (StageAcronymSurveyActivitiesDto)deserialize(json, StageAcronymSurveyActivitiesDto.class);
-    dto.acronym = dto.acronymGroup.getAcronym();
-    dto.acronymGroup = null;
-    return dto;
+    return (StageAcronymSurveyActivitiesDto)deserialize(json, StageAcronymSurveyActivitiesDto.class);
+  }
+
+  public boolean hasActivities(){
+    return !activities.isEmpty();
+  }
+
+  public void removeAcronymGroup(){
+    acronym = acronymGroup.getAcronym();
+    acronymGroup = null;
+    activityName = activities.get(0).getName();
   }
 
 }
