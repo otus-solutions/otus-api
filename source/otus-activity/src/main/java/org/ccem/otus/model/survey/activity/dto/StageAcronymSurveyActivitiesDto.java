133fa8ae7afc67442c5da76b783045ff321191db
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-12 11:36:08 -0300

    OA-212 novo construtor parametrizado para StageAcronymSurveyActivitiesDto

--- source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageAcronymSurveyActivitiesDto.java
+++ source/otus-activity/src/main/java/org/ccem/otus/model/survey/activity/dto/StageAcronymSurveyActivitiesDto.java
@@ -3,19 +3,27 @@ package org.ccem.otus.model.survey.activity.dto;
 import com.google.gson.annotations.SerializedName;
 import org.ccem.otus.model.SerializableModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StageAcronymSurveyActivitiesDto extends SerializableModel {
 
   @SerializedName("_id")
   private AcronymGroup acronymGroup;
-
   private String acronym;
-
   @SerializedName("name")
   private String activityName;
-
   private List<SurveyActivityItemListDto> activities;
+
+
+  public StageAcronymSurveyActivitiesDto(){}
+
+  public StageAcronymSurveyActivitiesDto(String acronym, String activityName){
+    this.acronym = acronym;
+    this.activityName = activityName;
+    this.activities = new ArrayList<>();
+  }
+
 
   public AcronymGroup getAcronymGroup() {
     return acronymGroup;
@@ -58,5 +66,5 @@ public class StageAcronymSurveyActivitiesDto extends SerializableModel {
     acronymGroup = null;
     activityName = activities.get(0).getName();
   }
-
+  
 }
