06f4f946d534896fa796df97f2c02c23a9499423
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-16 14:03:13 -0300

    OA-212 testes unitários de SerializableModel

---
+++ source/otus-commons/src/test/java/org/ccem/otus/model/SerializableModelTest.java
@@ -0,0 +1,28 @@
+package org.ccem.otus.model;
+
+import com.google.gson.GsonBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(PowerMockRunner.class)
+public class SerializableModelTest {
+
+  @Test
+  public void serializeStaticMethod_should_convert_objectModel_to_JsonString() {
+    assertTrue(SerializableModelWithID.serialize(Mockito.anyObject()) instanceof String);
+  }
+
+  @Test
+  public void deserializeStaticMethod_should_convert_JsonString_to_objectModel() {
+    assertTrue(SerializableModelWithID.deserialize("{}", Object.class) instanceof Object);
+  }
+
+  @Test
+  public void getGsonBuilder_return_GsonBuilder_instance(){
+    assertTrue(SerializableModelWithID.getGsonBuilder() instanceof GsonBuilder);
+  }
+}
