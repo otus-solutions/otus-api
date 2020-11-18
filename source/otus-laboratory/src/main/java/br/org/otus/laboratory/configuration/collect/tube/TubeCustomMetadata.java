ad312407c12e228df587999132ae958498e0111b
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-18 18:37:25 -0300

    OAP-33 TubeCustomMetadata: getters, refatoração de getGsonBuilder

--- source/otus-laboratory/src/main/java/br/org/otus/laboratory/configuration/collect/tube/TubeCustomMetadata.java
+++ source/otus-laboratory/src/main/java/br/org/otus/laboratory/configuration/collect/tube/TubeCustomMetadata.java
@@ -7,25 +7,45 @@ import org.ccem.otus.utils.ObjectIdToStringAdapter;
 
 public class TubeCustomMetadata {
 
+  public static final String OBJECT_TYPE = "TubeCustomMetadata";
+
   private ObjectId _id;
   private String objectType;
   private String type;
   private String value;
   private String extractionValue;
 
+  public ObjectId get_id() {
+    return _id;
+  }
+
+  public String getObjectType() {
+    return objectType;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public String getExtractionValue() {
+    return extractionValue;
+  }
+
   public static String serialize(TubeCustomMetadata laboratory) {
-    Gson builder = TubeCustomMetadata.getGsonBuilder();
-    return builder.toJson(laboratory);
+    return getGsonBuilder().create().toJson(laboratory);
   }
 
   public static TubeCustomMetadata deserialize(String laboratoryJson) {
-    Gson builder = TubeCustomMetadata.getGsonBuilder();
-    return builder.fromJson(laboratoryJson, TubeCustomMetadata.class);
+    return getGsonBuilder().create().fromJson(laboratoryJson, TubeCustomMetadata.class);
   }
 
-  public static Gson getGsonBuilder() {
+  public static GsonBuilder getGsonBuilder() {
     GsonBuilder builder = new GsonBuilder();
     builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
-    return builder.create();
+    return builder;
   }
 }
