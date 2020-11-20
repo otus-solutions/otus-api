9a08dfbe1a5df1f3e68e89a0e258e8bdf56a1673
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-20 17:09:11 -0300

    OAP-33 testes unitários de LaboratoryConfigurationFacade

---
+++ source/otus-business/src/test/java/br/org/otus/laboratory/configuration/LaboratoryConfigurationFacadeTest.java
@@ -0,0 +1,40 @@
+package br.org.otus.laboratory.configuration;
+
+import br.org.otus.response.exception.HttpResponseException;
+import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+public class LaboratoryConfigurationFacadeTest {
+
+  private static final String TUBE_TYPE = "type";
+
+  @InjectMocks
+  private LaboratoryConfigurationFacade laboratoryConfigurationFacade;
+  @Mock
+  private LaboratoryConfigurationService laboratoryConfigurationService;
+
+  @Before
+  public void setUp(){
+
+  }
+
+  @Test
+  public void getTubeMedataDataByType_method_should_invoke_service_method() throws DataNotFoundException {
+    laboratoryConfigurationFacade.getTubeMedataDataByType(TUBE_TYPE);
+    verify(laboratoryConfigurationService, times(1)).getTubeCustomMedataDataByType(TUBE_TYPE);
+  }
+
+  @Test(expected = HttpResponseException.class)
+  public void getTubeMedataDataByType_method_should_handle_DataNotFoundException() throws DataNotFoundException {
+    when(laboratoryConfigurationService.getTubeCustomMedataDataByType(TUBE_TYPE)).thenThrow(new DataNotFoundException());
+    laboratoryConfigurationFacade.getTubeMedataDataByType(TUBE_TYPE);
+  }
+}
