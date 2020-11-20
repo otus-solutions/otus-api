da0093e0016c7088d89a54f6937e2c88890a4c23
FlaviaAP <flavia.avila@ufrgs.br>
2020-11-20 18:03:36 -0300

    OAP-33 teste unitário de LaboratoryConfigurationResource.getTubeMedataDataByType

---
+++ source/otus-rest/src/test/java/br/org/otus/laboratory/configuration/LaboratoryConfigurationResourceTest.java
@@ -0,0 +1,41 @@
+package br.org.otus.laboratory.configuration;
+
+import br.org.otus.ResourceTestsParent;
+import br.org.otus.laboratory.configuration.collect.tube.TubeCustomMetadata;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+public class LaboratoryConfigurationResourceTest extends ResourceTestsParent {
+
+  private static final String TUBE_JSON = "{}";
+
+  @InjectMocks
+  private LaboratoryConfigurationResource resource;
+  @Mock
+  private LaboratoryConfigurationFacade laboratoryConfigurationFacade;
+  @Mock
+  private LaboratoryConfigurationService laboratoryConfigurationService;
+
+
+  @Test
+  public void updateTubeCustomMetadata_method_should_return_empty_response(){
+    List<TubeCustomMetadata> tubeCustomMetadata = new ArrayList<>();
+    when(laboratoryConfigurationFacade.getTubeMedataDataByType(TUBE_JSON)).thenReturn(tubeCustomMetadata);
+
+    String response = resource.getTubeMedataDataByType(TUBE_JSON);
+    verify(laboratoryConfigurationFacade, Mockito.times(1)).getTubeMedataDataByType(TUBE_JSON);
+    assertEquals(encapsulateExpectedResponse(tubeCustomMetadata.toString()), response);
+  }
+}
