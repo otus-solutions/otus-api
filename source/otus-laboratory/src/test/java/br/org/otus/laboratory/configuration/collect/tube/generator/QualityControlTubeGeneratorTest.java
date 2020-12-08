b1bce35670876b90dad060924e564a3b46bdb6f3
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-08 11:04:10 -0300

    OA-215 refatoração* de QualityControlTubeGeneratorTest
    *com adição de teste faltante para 100% de cobertura

--- source/otus-laboratory/src/test/java/br/org/otus/laboratory/configuration/collect/tube/generator/QualityControlTubeGeneratorTest.java
+++ source/otus-laboratory/src/test/java/br/org/otus/laboratory/configuration/collect/tube/generator/QualityControlTubeGeneratorTest.java
@@ -1,12 +1,10 @@
 package br.org.otus.laboratory.configuration.collect.tube.generator;
 
-import br.org.otus.laboratory.configuration.LaboratoryConfiguration;
 import br.org.otus.laboratory.configuration.LaboratoryConfigurationService;
 import br.org.otus.laboratory.configuration.collect.group.CollectGroupDescriptor;
 import br.org.otus.laboratory.configuration.collect.tube.TubeDefinition;
 import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
 import org.ccem.otus.model.FieldCenter;
-import org.ccem.otus.participant.model.Participant;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +15,6 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
@@ -26,34 +23,31 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @RunWith(PowerMockRunner.class)
 public class QualityControlTubeGeneratorTest {
 
+  private static final Integer TUBE_1_COUNT = 4;
+  private static final Integer TUBE_2_COUNT = 2;
+  private static final String TUBE_1_TYPE = "GEL";
+  private static final String TUBE_2_TYPE = "FLOURIDE";
+  private static final String TUBE_1_MOMENT = "POST_OVERLOAD";
+  private static final String TUBE_2_MOMENT = "FASTING";
+  private static final String DEFAULT = "DEFAULT";
+
   @InjectMocks
   private QualityControlTubeGenerator qualityControlTubeGenerator;
   @Mock
   private LaboratoryConfigurationService laboratoryConfigurationService;
-  @InjectMocks
-  private FieldCenter fieldCenter;
-  @InjectMocks
-  private LaboratoryConfiguration laboratoryConfiguration;
+
   private CollectGroupDescriptor collectGroupDescriptor;
   private Set<TubeDefinition> tubeSets;
-  private Set<TubeDefinition> expectedTubeSets;
-  private List<TubeDefinition> tubeDefinitionsExpected;
   private TubeSeed tubeSeed;
-  private String typeExpected;
-  private String momentExpected;
-  private int tubeCountExpected;
 
   @Before
   public void setUp() throws DataNotFoundException {
-    tubeSets = new HashSet<TubeDefinition>();
-    tubeSets.add(new TubeDefinition(1, "FLOURIDE", "POST_OVERLOAD"));
-    tubeSets.add(new TubeDefinition(4, "GEL", "FASTING"));
-    tubeSets.add(new TubeDefinition(3, "EDTA", "FASTING"));
-    tubeSets.add(new TubeDefinition(1, "URINE", "NONE"));
-    tubeSets.add(new TubeDefinition(1, "FLOURIDE", "FASTING"));
-    tubeSets.add(new TubeDefinition(2, "GEL", "POST_OVERLOAD"));
+    tubeSets = new HashSet<>();
+    tubeSets.add(new TubeDefinition(TUBE_1_COUNT, TUBE_1_TYPE, TUBE_1_MOMENT));
+    tubeSets.add(new TubeDefinition(TUBE_2_COUNT, TUBE_2_TYPE, TUBE_2_MOMENT));
 
-    collectGroupDescriptor = new CollectGroupDescriptor("DEFAULT", "DEFAULT", tubeSets);
+    FieldCenter fieldCenter = new FieldCenter();
+    collectGroupDescriptor = new CollectGroupDescriptor(DEFAULT, DEFAULT, tubeSets);
     tubeSeed = TubeSeed.generate(fieldCenter, collectGroupDescriptor);
 
     when(laboratoryConfigurationService.getTubeSetByGroupName(tubeSeed.getCollectGroupDescriptor().getName()))
@@ -61,46 +55,37 @@ public class QualityControlTubeGeneratorTest {
   }
 
   @Test
-  public void method_should_call_getTubeDefinitionList() throws DataNotFoundException {
+  public void getTubeDefinitions_method_should_call_getTubeSetByGroupName() throws DataNotFoundException {
     qualityControlTubeGenerator.getTubeDefinitions(tubeSeed);
-    verify(laboratoryConfigurationService).getTubeSetByGroupName("DEFAULT");
+    verify(laboratoryConfigurationService).getTubeSetByGroupName(DEFAULT);
   }
 
   @Test
-  public void method_should_getTubeDefinitions() throws DataNotFoundException {
-    expectedTubeSets = new HashSet<TubeDefinition>();
-    expectedTubeSets.add(new TubeDefinition(4, "GEL", "POST_OVERLOAD"));
-    expectedTubeSets.add(new TubeDefinition(3, "EDTA", "FASTING"));
+  public void getTubeDefinitions_method_check() throws DataNotFoundException {
+    List<TubeDefinition> tubeDefinitions = qualityControlTubeGenerator.getTubeDefinitions(tubeSeed);
+    assertEquals(tubeSets.size(), tubeDefinitions.size());
 
-    tubeDefinitionsExpected = expectedTubeSets.stream().map(definition -> definition).collect(Collectors.toList());
+    assertEquals(TUBE_1_TYPE, tubeDefinitions.get(0).getType());
+    assertEquals(TUBE_1_MOMENT, tubeDefinitions.get(0).getMoment());
 
-    typeExpected = tubeDefinitionsExpected.stream().filter(t -> t.getType().equals("GEL")).findFirst().get()
-      .getType();
+    assertEquals(TUBE_2_TYPE, tubeDefinitions.get(1).getType());
+    assertEquals(TUBE_2_MOMENT, tubeDefinitions.get(1).getMoment());
+  }
 
-    momentExpected = tubeDefinitionsExpected.stream().filter(t -> t.getMoment().equals("POST_OVERLOAD")).findFirst()
-      .get().getMoment();
-
-    assertEquals(typeExpected, qualityControlTubeGenerator.getTubeDefinitions(tubeSeed).stream()
-      .filter(t -> t.getType().equals("GEL")).findFirst().get().getType());
-
-    assertEquals(momentExpected, qualityControlTubeGenerator.getTubeDefinitions(tubeSeed).stream()
-      .filter(t -> t.getMoment().equals("POST_OVERLOAD")).findFirst().get().getMoment());
+  @Test
+  public void getTubeDefinitions_method_should_return_empty_list() throws DataNotFoundException {
+    tubeSets.clear();
+    assertEquals(0, qualityControlTubeGenerator.getTubeDefinitions(tubeSeed).size());
   }
 
   @Test
   public void method_should_getQuantityTubeDefinition() throws DataNotFoundException {
-    int expectatedSizeTubeDefinition = tubeSets.size();
-    assertEquals(expectatedSizeTubeDefinition, qualityControlTubeGenerator.getTubeDefinitions(tubeSeed).size());
+    assertEquals(tubeSets.size(), qualityControlTubeGenerator.getTubeDefinitions(tubeSeed).size());
   }
 
   @Test
   public void method_should_sumGetTubes() throws DataNotFoundException {
-    tubeCountExpected = 0;
-    for (TubeDefinition tubeSet : tubeSets) {
-      tubeCountExpected += tubeSet.getCount();
-    }
-
     qualityControlTubeGenerator.getTubeDefinitions(tubeSeed);
-    assertEquals(tubeCountExpected, (int) tubeSeed.getTubeCount());
+    assertEquals(TUBE_1_COUNT + TUBE_2_COUNT, (int) tubeSeed.getTubeCount());
   }
 }
