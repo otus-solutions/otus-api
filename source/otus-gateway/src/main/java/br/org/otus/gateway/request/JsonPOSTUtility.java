55eb7136cef374888ffc0d95d7d07787c4c07b43
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-22 15:09:00 -0300

    OA-220 classe JsonPOSTUtility como filha de JsonWritingRequestUtility

--- source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonPOSTUtility.java
+++ source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonPOSTUtility.java
@@ -1,46 +1,14 @@
 package br.org.otus.gateway.request;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import br.org.otus.gateway.response.exception.RequestException;
-
-public class JsonPOSTUtility {
-  private HttpURLConnection httpConn;
-  private DataOutputStream request;
+public class JsonPOSTUtility extends JsonWritingRequestUtility {
 
   public JsonPOSTUtility(URL requestURL, String body) throws IOException {
-    httpConn = (HttpURLConnection) requestURL.openConnection();
-    httpConn.setUseCaches(false);
-    httpConn.setDoOutput(true);
-    httpConn.setDoInput(true);
-
-    httpConn.setRequestMethod("POST");
-    httpConn.setRequestProperty("Connection", "Keep-Alive");
-    httpConn.setRequestProperty("Cache-Control", "no-cache");
-    httpConn.setRequestProperty("Content-Type", "application/json");
-    request = new DataOutputStream(httpConn.getOutputStream());
+    super(RequestTypeOptions.POST, requestURL, "application/json");
     request.write(body.getBytes(StandardCharsets.UTF_8));
-  }
-
-  public String finish() throws IOException, RequestException {
-    String response;
-
-    request.flush();
-    request.close();
-    int status = httpConn.getResponseCode();
-    if (status == HttpURLConnection.HTTP_OK) {
-      response = RequestUtility.getString(httpConn);
-    } else {
-      String errorMessage = httpConn.getResponseMessage();
-      Object errorContent = RequestUtility.getErrorContent(httpConn);
-      throw new RequestException(status, errorMessage, errorContent);
-    }
-
-    return response;
   }
 
 }
