ab483e3eaca8707b8fbbdc0144001e32f93e09d8
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-23 17:11:10 -0300

    OA-220 acréscimos em ResourceTestsParent
    constante SUCCESS_STATUS e método encapsulateExpectedStringResponse

--- source/otus-rest/src/test/java/br/org/otus/ResourceTestsParent.java
+++ source/otus-rest/src/test/java/br/org/otus/ResourceTestsParent.java
@@ -3,8 +3,13 @@ package br.org.otus;
 public abstract class ResourceTestsParent {
 
   protected final static String EMPTY_RESPONSE = "{\"data\":true}";
+  protected final static int SUCCESS_STATUS = 200;
 
   protected String encapsulateExpectedResponse(String data) {
     return "{\"data\":" + data + "}";
+  }
+
+  protected String encapsulateExpectedStringResponse(String data) {
+    return "{\"data\":\"" + data + "\"}";
   }
 }
