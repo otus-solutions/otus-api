a82c946e8bd8eaaa4ac77fdb54ee0fb4ce39170d
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-24 17:03:38 -0300

    OA-221 métodos create/update/discard em ActivityTasksService(Bean)
    com chamada assíncrona do respectivo método do ExtractionFacade

--- source/otus-business/src/main/java/br/org/otus/survey/services/ActivityTasksServiceBean.java
+++ source/otus-business/src/main/java/br/org/otus/survey/services/ActivityTasksServiceBean.java
@@ -1,5 +1,6 @@
 package br.org.otus.survey.services;
 
+import br.org.otus.extraction.ExtractionFacade;
 import br.org.otus.outcomes.FollowUpFacade;
 import br.org.otus.response.exception.HttpResponseException;
 import br.org.otus.response.info.Validation;
@@ -13,6 +14,7 @@ import org.ccem.otus.model.survey.activity.User;
 import org.ccem.otus.model.survey.activity.mode.ActivityMode;
 import org.ccem.otus.model.survey.activity.status.ActivityStatus;
 import org.ccem.otus.model.survey.activity.status.UserNotFoundException;
+import org.ccem.otus.model.survey.offlineActivity.OfflineActivityCollection;
 import org.ccem.otus.participant.model.Participant;
 import org.ccem.otus.participant.service.ParticipantService;
 import org.ccem.otus.service.ActivityService;
@@ -25,9 +27,13 @@ import javax.inject.Inject;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
 
 @Stateless
 public class ActivityTasksServiceBean implements ActivityTasksService {
+
+  private final static Logger LOGGER = Logger.getLogger("br.org.otus.extraction.ExtractionFacade");
 
   @Inject
   private ActivityService activityService;
@@ -53,17 +59,92 @@ public class ActivityTasksServiceBean implements ActivityTasksService {
   @Inject
   private SurveyService surveyService;
 
+  @Inject
+  private ExtractionFacade extractionFacade;
+
 
   private SurveyActivity currActivity;
 
   @Override
+  public String create(SurveyActivity surveyActivity, boolean notify) {
+    String activityId = activityService.create(surveyActivity);
+    surveyActivity.setActivityID(new ObjectId(activityId));
+
+    if (isSurveyActivityAutofill(surveyActivity.getMode())) {
+      followUpFacade.createParticipantActivityAutoFillEvent(surveyActivity, notify);
+    }
+
+    CompletableFuture.runAsync(() -> {
+      try{
+        extractionFacade.createActivityExtraction(surveyActivity.getActivityID().toString());
+      }
+      catch (Exception e){
+        LOGGER.severe("status: fail, action: create activity extraction for activityId " + surveyActivity.getActivityID().toString());
+        new Exception("Error while syncing results", e).printStackTrace();
+      }
+    });
+
+    return activityId;
+  }
+
+  @Override
   public SurveyActivity updateActivity(SurveyActivity surveyActivity, String token) throws ParseException, DataNotFoundException {
     updateStatusHistoryUser(surveyActivity, generateStatusHistoryUserForUpdate(token));
+
     currActivity = activityService.update(surveyActivity);
-    if (currActivity.getMode().name().equals(ActivityMode.AUTOFILL.name())) {
+
+    if (isSurveyActivityAutofill(currActivity.getMode())) {
       updateAutofillActivity();
     }
+
+    CompletableFuture.runAsync(() -> {
+      try{
+        extractionFacade.updateActivityExtraction(surveyActivity.getActivityID().toString());
+      }
+      catch (Exception e){
+        LOGGER.severe("status: fail, action: update activity extraction for activityId " + surveyActivity.getActivityID().toString());
+        new Exception("Error while syncing results", e).printStackTrace();
+      }
+    });
+
     return currActivity;
+  }
+
+  @Override
+  public void save(String userEmail, OfflineActivityCollection offlineActivityCollection) throws DataNotFoundException {
+    activityService.save(userEmail, offlineActivityCollection);
+
+    CompletableFuture.runAsync(() -> {
+      offlineActivityCollection.getActivities().forEach(surveyActivity -> {
+        try{
+          extractionFacade.updateActivityExtraction(surveyActivity.getActivityID().toString());
+        }
+        catch (Exception e){
+          LOGGER.severe("status: fail, action: save activity extraction for activityId " + surveyActivity.getActivityID().toString() +
+            " from offlineActivityCollection " + offlineActivityCollection.get_id());
+          new Exception("Error while syncing results", e).printStackTrace();
+        }
+      });
+    });
+  }
+
+  @Override
+  public void discardById(String activityId) throws DataNotFoundException {
+    activityService.discardByID(new ObjectId(activityId));
+
+    CompletableFuture.runAsync(() -> {
+      try{
+        extractionFacade.deleteActivityExtraction(activityId);
+      }
+      catch (Exception e){
+        LOGGER.severe("status: fail, action: delete activity extraction for activityId " + activityId);
+        new Exception("Error while syncing results", e).printStackTrace();
+      }
+    });
+  }
+
+  private boolean isSurveyActivityAutofill(ActivityMode activityMode) {
+    return (activityMode != null && activityMode == ActivityMode.AUTOFILL);
   }
 
   private User generateStatusHistoryUserForUpdate(String token) throws ParseException, DataNotFoundException {
