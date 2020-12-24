c6ffec01b0f0eb66539926b76b83ea7268723071
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-24 15:39:02 -0300

    OA-221 add métodos getActivityExtraction*Address em ExtractionMicroServiceResources
    *Create, Update, Delete

--- source/otus-gateway/src/main/java/br/org/otus/gateway/resource/ExtractionMicroServiceResources.java
+++ source/otus-gateway/src/main/java/br/org/otus/gateway/resource/ExtractionMicroServiceResources.java
@@ -9,11 +9,28 @@ public class ExtractionMicroServiceResources extends MicroservicesResources {
 
   private static final String PIPELINE_EXTRACTION_RESOURCE = "/pipeline/";
 
+  private static final String EXTRACTION_SUFFIX = "/extraction/";
+  private static final String EXTRACTION_CREATE_RESOURCE = EXTRACTION_SUFFIX + "create/";
+  private static final String EXTRACTION_UPDATE_RESOURCE = EXTRACTION_SUFFIX + "update/";
+  private static final String EXTRACTION_DELETE_RESOURCE = EXTRACTION_SUFFIX + "delete/";
+
   public ExtractionMicroServiceResources() {
     super(MicroservicesEnvironments.EXTRACTION);
   }
 
   public URL getPipelineExtractionAddress(String pipelineName) throws MalformedURLException {
     return new URL(getMainAddress() + PIPELINE_EXTRACTION_RESOURCE + pipelineName);
+  }
+
+  public URL getActivityExtractionCreateAddress(String activityId) throws MalformedURLException {
+    return new URL(getMainAddress() + EXTRACTION_CREATE_RESOURCE + activityId);
+  }
+
+  public URL getActivityExtractionUpdateAddress(String activityId) throws MalformedURLException {
+    return new URL(getMainAddress() + EXTRACTION_UPDATE_RESOURCE + activityId);
+  }
+
+  public URL getActivityExtractionDeleteAddress(String activityId) throws MalformedURLException {
+    return new URL(getMainAddress() + EXTRACTION_DELETE_RESOURCE + activityId);
   }
 }
