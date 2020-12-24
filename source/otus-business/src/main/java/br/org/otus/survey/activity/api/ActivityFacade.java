60430d51744fe1c4992f812dcc1206efd5d948bc
FlaviaAP <flavia.avila@ufrgs.br>
2020-12-24 11:54:46 -0300

    OA-221 ActivityFacade.updateActivity chamando activityTasksService.updateActivity

--- source/otus-business/src/main/java/br/org/otus/survey/activity/api/ActivityFacade.java
+++ source/otus-business/src/main/java/br/org/otus/survey/activity/api/ActivityFacade.java
@@ -4,13 +4,11 @@ import br.org.otus.outcomes.FollowUpFacade;
 import br.org.otus.response.builders.ResponseBuild;
 import br.org.otus.response.exception.HttpResponseException;
 import br.org.otus.response.info.Validation;
+import br.org.otus.survey.services.ActivityTasksService;
 import br.org.otus.survey.services.SurveyService;
-import br.org.otus.user.management.ManagementUserService;
 import com.google.gson.JsonSyntaxException;
-import com.nimbusds.jwt.SignedJWT;
 import model.Stage;
 import org.bson.types.ObjectId;
-import org.ccem.otus.enums.AuthenticationMode;
 import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
 import org.ccem.otus.exceptions.webservice.common.MemoryExcededException;
 import org.ccem.otus.exceptions.webservice.validation.ValidationException;
@@ -19,8 +17,6 @@ import org.ccem.otus.model.survey.activity.User;
 import org.ccem.otus.model.survey.activity.configuration.ActivityCategory;
 import org.ccem.otus.model.survey.activity.dto.StageSurveyActivitiesDto;
 import org.ccem.otus.model.survey.activity.mode.ActivityMode;
-import org.ccem.otus.model.survey.activity.status.ActivityStatus;
-import org.ccem.otus.model.survey.activity.status.UserNotFoundException;
 import org.ccem.otus.model.survey.offlineActivity.OfflineActivityCollection;
 import org.ccem.otus.model.survey.offlineActivity.OfflineActivityCollectionGroupsDTO;
 import org.ccem.otus.participant.model.Participant;
@@ -28,7 +24,6 @@ import org.ccem.otus.participant.service.ParticipantService;
 import org.ccem.otus.service.ActivityService;
 import org.ccem.otus.service.configuration.ActivityCategoryService;
 import org.ccem.otus.service.extraction.model.ActivityProgressResultExtraction;
-import org.ccem.otus.service.sharing.ActivitySharingService;
 import service.StageService;
 
 import javax.inject.Inject;
@@ -44,9 +39,6 @@ public class ActivityFacade {
   private ParticipantService participantService;
 
   @Inject
-  private ManagementUserService managementUserService;
-
-  @Inject
   private ActivityCategoryService activityCategoryService;
 
   @Inject
@@ -56,13 +48,11 @@ public class ActivityFacade {
   private FollowUpFacade followUpFacade;
 
   @Inject
-  private ActivitySharingService activitySharingService;
-
-  @Inject
   private SurveyService surveyService;
 
+  @Inject
+  private ActivityTasksService activityTasksService;
 
-  private SurveyActivity activityUpdated;
 
   public List<SurveyActivity> list(long rn, String userEmail) {
     return activityService.list(rn, userEmail);
@@ -163,69 +153,10 @@ public class ActivityFacade {
 
   public SurveyActivity updateActivity(SurveyActivity surveyActivity, String token) {
     try {
-      User statusHistoryUser = null;
-      Participant participant = null;
-      token = token.substring("Bearer".length()).trim();
-      SignedJWT signedJWT = SignedJWT.parse(token);
-      String mode = signedJWT.getJWTClaimsSet().getClaim("mode").toString();
-      Object email = signedJWT.getJWTClaimsSet().getClaim("iss");
-
-      switch (AuthenticationMode.valueFromName(mode)) {
-        case USER:
-          br.org.otus.model.User user = managementUserService.fetchByEmail(email.toString());
-          statusHistoryUser = new User(user.getName(), user.getEmail(), user.getSurname(), user.getPhone());
-          break;
-
-        case PARTICIPANT:
-          participant = participantService.getByEmail(email.toString());
-          statusHistoryUser = new User(participant.getName(), participant.getEmail(), "", null);
-          break;
-
-        case ACTIVITY_SHARING:
-          Long rn = Long.parseLong(signedJWT.getJWTClaimsSet().getClaim("recruitmentNumber").toString());
-          participant = participantService.getByRecruitmentNumber(rn);
-          statusHistoryUser = new User(participant.getName(), participant.getEmail(), "", null);
-          break;
-
-        default:
-          throw new HttpResponseException(Validation.build("Invalid token mode", null));
-      }
-
-      List<ActivityStatus> statusHistory = surveyActivity.getStatusHistory();
-      int size = statusHistory.size();
-      for (int i = size - 1; i != 0; i--) {
-        ActivityStatus activityStatus = statusHistory.get(i);
-        try {
-          activityStatus.getUser();
-          break;
-        } catch (UserNotFoundException e) {
-          activityStatus.setUser(statusHistoryUser);
-        }
-      }
-
-      activityUpdated = activityService.update(surveyActivity);
-
-      if (activityUpdated.getMode().name().equals(ActivityMode.AUTOFILL.name())) {
-        if(activityUpdated.isDiscarded()){
-          followUpFacade.cancelParticipantEventByActivityId(surveyActivity.getActivityID().toString());
-          removeShareUrl(activityUpdated.getActivityID());
-        }
-        else{
-          String nameLastStatusHistory = activityUpdated.getLastStatus().get().getName();
-          String activityId = String.valueOf(activityUpdated.getActivityID());
-          followUpFacade.statusUpdateEvent(nameLastStatusHistory, activityId);
-        }
-      }
-      return activityUpdated;
-
+      return activityTasksService.updateActivity(surveyActivity, token);
     } catch (DataNotFoundException | ParseException e) {
       throw new HttpResponseException(Validation.build(e.getMessage(), e.getCause()));
     }
-  }
-
-  private void removeShareUrl(ObjectId activityID) throws DataNotFoundException {
-    ObjectId activitySharingId = activitySharingService.getActivitySharingIdByActivityId(activityID);
-    if(Objects.nonNull(activitySharingId)) activitySharingService.deleteSharedURL(String.valueOf(activitySharingId));
   }
 
   public boolean updateCheckerActivity(String checkerUpdated) {
