3edff50c8b892b10672284c51ce099d1338b575f
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-15 18:04:10 -0300

    OA-220 novo construtor cm body para JsonPUTRequestUtility

--- source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonPUTRequestUtility.java
+++ source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonPUTRequestUtility.java
@@ -10,6 +10,11 @@ public class JsonPUTRequestUtility extends JsonWritingRequestUtility{
     super(RequestTypeOptions.PUT, requestURL, "application/json");
   }
 
+  public JsonPUTRequestUtility(URL requestURL, String body) throws IOException {
+    super(RequestTypeOptions.PUT, requestURL, "application/json");
+    writeBody(body);
+  }
+
   public void writeBody(String body) throws IOException {
     this.request.write(body.getBytes(StandardCharsets.UTF_8));
   }
