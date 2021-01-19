c8ec7b85e42865100e6d2dc9ef23cd03b08422b5
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-19 12:56:34 -0300

    OA-220 ajuste das urls CRUD de ExtractionMicroServiceResources

--- source/otus-gateway/src/main/java/br/org/otus/gateway/resource/ExtractionMicroServiceResources.java
+++ source/otus-gateway/src/main/java/br/org/otus/gateway/resource/ExtractionMicroServiceResources.java
@@ -7,32 +7,30 @@ import java.net.URL;
 
 public class ExtractionMicroServiceResources extends MicroservicesResources {
 
-  private static final String PIPELINE_EXTRACTION_SUFFIX = "/pipeline/";
-  private static final String PIPELINE_JSON_EXTRACTION_RESOURCE = PIPELINE_EXTRACTION_SUFFIX + "json/";
-  private static final String PIPELINE_CSV_EXTRACTION_RESOURCE = PIPELINE_EXTRACTION_SUFFIX + "csv/";
+  private static final String PIPELINE_EXTRACTION_SUFFIX = "/pipeline";
+  private static final String PIPELINE_JSON_EXTRACTION_RESOURCE = PIPELINE_EXTRACTION_SUFFIX + "/json";
+  private static final String PIPELINE_CSV_EXTRACTION_RESOURCE = PIPELINE_EXTRACTION_SUFFIX + "/csv";
 
   private static final String EXTRACTION_SUFFIX = "/extractions";
-  private static final String ACTIVITY_EXTRACTION_SUFFIX = EXTRACTION_SUFFIX + "/activity";
-  private static final String EXTRACTION_CREATE_RESOURCE = ACTIVITY_EXTRACTION_SUFFIX + "/create";
-  private static final String EXTRACTION_DELETE_RESOURCE = ACTIVITY_EXTRACTION_SUFFIX + "/delete";
+  private static final String ACTIVITY_EXTRACTION_RESOURCE = EXTRACTION_SUFFIX + "/activity";
 
   public ExtractionMicroServiceResources() {
     super(MicroservicesEnvironments.EXTRACTION);
   }
 
-  public URL getPipelineJsonExtractionAddress(String pipelineName) throws MalformedURLException {
-    return new URL(getMainAddress() + PIPELINE_JSON_EXTRACTION_RESOURCE + pipelineName);
+  public URL getPipelineJsonExtractionAddress() throws MalformedURLException {
+    return new URL(getMainAddress() + PIPELINE_JSON_EXTRACTION_RESOURCE);
   }
 
-  public URL getPipelineCsvExtractionAddress(String pipelineName) throws MalformedURLException {
-    return new URL(getMainAddress() + PIPELINE_CSV_EXTRACTION_RESOURCE + pipelineName);
+  public URL getPipelineCsvExtractionAddress() throws MalformedURLException {
+    return new URL(getMainAddress() + PIPELINE_CSV_EXTRACTION_RESOURCE);
   }
 
   public URL getActivityExtractionCreateAddress() throws MalformedURLException {
-    return new URL(getMainAddress() + EXTRACTION_CREATE_RESOURCE);
+    return new URL(getMainAddress() + ACTIVITY_EXTRACTION_RESOURCE);
   }
 
   public URL getActivityExtractionDeleteAddress(String surveyId, String activityId) throws MalformedURLException {
-    return new URL(getMainAddress() + EXTRACTION_DELETE_RESOURCE + "/" + surveyId + "/" + activityId);
+    return new URL(getMainAddress() + ACTIVITY_EXTRACTION_RESOURCE + "/" + surveyId + "/" + activityId);
   }
 }
