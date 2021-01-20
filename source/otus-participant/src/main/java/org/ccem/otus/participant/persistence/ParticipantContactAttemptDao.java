edd5135482dc210b7989186c0a21a4829cc53369
erickpoleto <erick-poleto@hotmail.com>
2021-01-20 12:43:51 -0300

    OAP-50 updateAttemptaddress e change, campo isValid adicionado ao participantContactAttempt, DAOs

--- source/otus-participant/src/main/java/org/ccem/otus/participant/persistence/ParticipantContactAttemptDao.java
+++ source/otus-participant/src/main/java/org/ccem/otus/participant/persistence/ParticipantContactAttemptDao.java
@@ -4,6 +4,7 @@ import org.bson.types.ObjectId;
 import org.ccem.otus.exceptions.webservice.common.DataNotFoundException;
 import org.ccem.otus.participant.model.participantContactAttempt.ParticipantContactAddressAttempt;
 import org.ccem.otus.participant.model.participantContactAttempt.ParticipantContactAttempt;
+import org.ccem.otus.participant.model.participant_contact.Address;
 
 import java.util.ArrayList;
 import java.util.zip.DataFormatException;
@@ -13,6 +14,10 @@ public interface ParticipantContactAttemptDao {
   ObjectId create(ParticipantContactAttempt participantContactAttempt) throws DataFormatException;
 
   void delete(ObjectId participantContactAttemptOID) throws DataNotFoundException;
+
+  void updateAttemptAddress(Long recruitmentNumber, String objectType, String position, Address address) throws DataNotFoundException;
+
+  void changeAddress(Long recruitmentNumber, String objectType, String position) throws DataNotFoundException;
 
   ArrayList<ParticipantContactAddressAttempt> findAddressAttempts(Long recruitmentNumber, String objectType, String position) throws DataNotFoundException;
 
