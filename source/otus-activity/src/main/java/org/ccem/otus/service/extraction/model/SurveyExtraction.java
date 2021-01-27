c16b2e2b2668b80bde832a67ba9df91bdcb4ca1b
FlaviaAP <flavia.avila@ufrgs.br>
2021-01-27 20:19:23 -0300

    OA-220 nova classe SurveyExtraction

---
+++ source/otus-activity/src/main/java/org/ccem/otus/service/extraction/model/SurveyExtraction.java
@@ -0,0 +1,40 @@
+package org.ccem.otus.service.extraction.model;
+
+import org.ccem.otus.model.SerializableModelWithID;
+
+public class SurveyExtraction extends SerializableModelWithID {
+
+  private String surveyId;
+  private String surveyAcronym;
+  private Integer surveyVersion;
+  private String RscriptName;
+
+  public SurveyExtraction(String surveyAcronym, Integer surveyVesion, String rscriptName) {
+    this.surveyAcronym = surveyAcronym;
+    this.surveyVersion = surveyVesion;
+    RscriptName = rscriptName;
+  }
+
+  public String getSurveyAcronym() {
+    return surveyAcronym;
+  }
+
+  public Integer getSurveyVersion() {
+    return surveyVersion;
+  }
+
+  public String getRscriptName() {
+    return RscriptName;
+  }
+
+  public void setSurveyId(String surveyId) {
+    this.surveyId = surveyId;
+    this.surveyAcronym = null;
+    this.surveyVersion = null;
+  }
+
+  public static SurveyExtraction fromJson(String json){
+    return (SurveyExtraction)deserialize(json, SurveyExtraction.class);
+  }
+
+}
