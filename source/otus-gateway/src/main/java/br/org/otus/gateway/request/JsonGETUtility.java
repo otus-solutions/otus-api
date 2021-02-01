8a4fab0ca85aa0d9789ad96f39fb0de69ce5132b
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-01 11:50:09 -0300

    OA-220 limpeza de JsonGETUtility

--- source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonGETUtility.java
+++ source/otus-gateway/src/main/java/br/org/otus/gateway/request/JsonGETUtility.java
@@ -1,12 +1,9 @@
 package br.org.otus.gateway.request;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
 public class JsonGETUtility extends JsonRequestUtility {
-
-  protected DataOutputStream request;
 
   public JsonGETUtility(URL requestURL) throws IOException {
     super(RequestTypeOptions.GET, requestURL);
