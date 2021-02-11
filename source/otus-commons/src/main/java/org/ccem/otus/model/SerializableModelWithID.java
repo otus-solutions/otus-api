c55370a7c430e98dbb01378299bdd85756723c7e
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-11 19:18:53 -0300

    OA-225 (backup) deserialize não estático em SerializableModelWithID

--- source/otus-commons/src/main/java/org/ccem/otus/model/SerializableModelWithID.java
+++ source/otus-commons/src/main/java/org/ccem/otus/model/SerializableModelWithID.java
@@ -2,8 +2,12 @@ package org.ccem.otus.model;
 
 import com.google.gson.GsonBuilder;
 import org.bson.types.ObjectId;
+import org.ccem.otus.survey.template.utils.adapters.LocalDateTimeAdapter;
+import org.ccem.otus.utils.LongAdapter;
 import org.ccem.otus.utils.ObjectIdAdapter;
 import org.ccem.otus.utils.ObjectIdToStringAdapter;
+
+import java.time.LocalDateTime;
 
 public abstract class SerializableModelWithID {
 
@@ -18,7 +22,6 @@ public abstract class SerializableModelWithID {
   protected static Object deserialize(String json, Class clazz){
     return getGsonBuilder().create().fromJson(json, clazz);
   }
-
   protected static GsonBuilder getGsonBuilder() {
     GsonBuilder builder = new GsonBuilder();
     builder.registerTypeAdapter(ObjectId.class, new ObjectIdAdapter());
@@ -29,6 +32,38 @@ public abstract class SerializableModelWithID {
     GsonBuilder builder = new GsonBuilder();
     builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
     return builder;
+  }
+
+  // Non static
+
+  public <T> T deserializeNonStatic(String json){
+    return (T)gsonBuilder().create().fromJson(json, this.getClass());
+  }
+
+  protected GsonBuilder gsonBuilder() {
+    GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(ObjectId.class, new ObjectIdAdapter());
+    registerSpecificTypeAdapter(builder);
+    return builder;
+  }
+
+  public GsonBuilder frontGsonBuilder() {
+    GsonBuilder builder = new GsonBuilder();
+    builder.registerTypeAdapter(ObjectId.class, new ObjectIdToStringAdapter());
+    registerSpecificTypeAdapter(builder);
+    return builder;
+  }
+
+  protected void registerSpecificTypeAdapter(GsonBuilder builder){
+    // Override by child class
+  }
+
+  protected void registerGsonBuilderLongAdapter(GsonBuilder builder){
+    builder.registerTypeAdapter(Long.class, new LongAdapter());
+  }
+
+  protected void registerGsonBuilderLocalDateTimeAdapter(GsonBuilder builder){
+    builder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
   }
 
 }
