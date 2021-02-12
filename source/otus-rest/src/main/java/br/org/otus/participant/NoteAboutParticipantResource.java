2a5b48a210b70e5633216e84077b3f9a62880ed7
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-12 15:06:39 -0300

    OA-225 correções em NoteAboutParticipantResource
    add anotação Securedem cada metodo, troca do tipo de requisição do método delete

--- source/otus-rest/src/main/java/br/org/otus/participant/NoteAboutParticipantResource.java
+++ source/otus-rest/src/main/java/br/org/otus/participant/NoteAboutParticipantResource.java
@@ -3,6 +3,7 @@ package br.org.otus.participant;
 import br.org.otus.UserAuthenticationResource;
 import br.org.otus.participant.api.NoteAboutParticipantFacade;
 import br.org.otus.rest.Response;
+import br.org.otus.security.user.Secured;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +18,7 @@ public class NoteAboutParticipantResource extends UserAuthenticationResource {
   private NoteAboutParticipantFacade noteAboutParticipantFacade;
 
   @POST
+  @Secured
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   public String create(@Context HttpServletRequest request, String noteAboutParticipantJson){
@@ -25,15 +27,17 @@ public class NoteAboutParticipantResource extends UserAuthenticationResource {
   }
 
   @PUT
+  @Secured
   @Consumes(MediaType.APPLICATION_JSON)
   public String update(@Context HttpServletRequest request, String noteAboutParticipantJson){
     noteAboutParticipantFacade.update(getUser(request), noteAboutParticipantJson);
     return new Response().buildSuccess().toJson();
   }
 
-  @PUT
-  @Consumes(MediaType.APPLICATION_JSON)
-  public String delete(@Context HttpServletRequest request, String noteAboutParticipantId){
+  @DELETE
+  @Secured
+  @Path("/{id}")
+  public String delete(@Context HttpServletRequest request, @PathParam("id") String noteAboutParticipantId){
     noteAboutParticipantFacade.delete(getUser(request), noteAboutParticipantId);
     return new Response().buildSuccess().toJson();
   }
