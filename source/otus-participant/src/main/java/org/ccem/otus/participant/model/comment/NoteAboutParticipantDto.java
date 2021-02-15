61ad2f24909584779d3991b7882e155eec4bc5bc
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-15 14:51:38 -0300

    OA-225 refatoraçãodos campos de NoteAboutParticipantDto + deserialize

--- source/otus-participant/src/main/java/org/ccem/otus/participant/model/comment/NoteAboutParticipantDto.java
+++ source/otus-participant/src/main/java/org/ccem/otus/participant/model/comment/NoteAboutParticipantDto.java
@@ -8,57 +8,21 @@ public class NoteAboutParticipantDto extends SerializableModelWithID {
 
   private ObjectId _id;
   private Long recruitmentNumber;
-  private String date;
+  private String creationDate;
+  private String lastUpdate;
   private Boolean edited;
+  private Boolean starred;
   private String comment;
-  private Boolean isCreator;
   private String userName;
-  private String userEmail;
-
-
-  public ObjectId getId() {
-    return _id;
-  }
-
-  public Long getRecruitmentNumber() {
-    return recruitmentNumber;
-  }
-
-  public String getDate() {
-    return date;
-  }
-
-  public Boolean getEdited() {
-    return edited;
-  }
-
-  public String getComment() {
-    return comment;
-  }
-
-  public Boolean getCreator() {
-    return isCreator;
-  }
-
-  public String getUserName() {
-    return userName;
-  }
-
-  public String getUserEmail() {
-    return userEmail;
-  }
-
-  public void setCreator(Boolean creator) {
-    isCreator = creator;
-  }
-
-  public static NoteAboutParticipantDto deserialize(String json){
-    return (new NoteAboutParticipantDto()).deserializeNonStatic(json);//TODO
-  }
+  private Boolean isCreator;
 
   @Override
   protected void registerSpecificTypeAdapter(GsonBuilder builder){
     registerGsonBuilderLongAdapter(builder);
     registerGsonBuilderLocalDateTimeAdapter(builder);
+  }
+
+  public static NoteAboutParticipantDto deserialize(String json){
+    return (NoteAboutParticipantDto)deserialize(json, NoteAboutParticipantDto.class);
   }
 }
