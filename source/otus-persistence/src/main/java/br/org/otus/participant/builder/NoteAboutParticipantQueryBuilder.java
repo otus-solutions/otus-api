d4a8d6038f90d06a99c98e75115543b279ef5ce8
FlaviaAP <flavia.avila@ufrgs.br>
2021-02-15 14:48:34 -0300

    OA-225 criação de NoteAboutParticipantQueryBuilder, já com método getByRnQuery

---
+++ source/otus-persistence/src/main/java/br/org/otus/participant/builder/NoteAboutParticipantQueryBuilder.java
@@ -0,0 +1,71 @@
+package br.org.otus.participant.builder;
+
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.ccem.otus.service.ParseQuery;
+
+import java.util.ArrayList;
+
+public class NoteAboutParticipantQueryBuilder {
+
+  private ArrayList<Bson> pipeline;
+
+  public ArrayList<Bson> getByRnQuery(ObjectId userOid, Long recruitmentNumber, int skip, int limit) {
+    pipeline = new ArrayList<>();
+    pipeline.add(ParseQuery.toDocument("{\n" +
+      "        $match: {\n" +
+      "            \"recruitmentNumber\": " + recruitmentNumber +
+      "        }\n" +
+      "    }"));
+    pipeline.add(ParseQuery.toDocument("{\n" +
+      "        $lookup: {\n" +
+      "            from: \"user\",\n" +
+      "            let: {\n" +
+      "                \"userId\": \"$userId\"\n" +
+      "            },\n" +
+      "            pipeline: [\n" +
+      "                {\n" +
+      "                    $match: {\n" +
+      "                        $expr: {\n" +
+      "                            $eq: [\"$_id\", \"$$userId\"]\n" +
+      "                        }\n" +
+      "                    }\n" +
+      "                },\n" +
+      "                {\n" +
+      "                    $project: {\n" +
+      "                        \"name\": {\n" +
+      "                            $concat: [\"$name\", \" \", \"$surname\" ]\n" +
+      "                        }\n" +
+      "                    }\n" +
+      "                }\n" +
+      "            ],\n" +
+      "            as: \"user\"\n" +
+      "        }\n" +
+      "    }"));
+    pipeline.add(ParseQuery.toDocument("{\n" +
+      "        $addFields: {\n" +
+      "            \"userName\": {\n" +
+      "                $ifNull: [ { $arrayElemAt: [\"$user.name\",0] }, null ]\n" +
+      "            },\n" +
+      "            \"isCreator\": {\n" +
+      "                $eq: [ {$toString: \"$userId\"}, " + userOid.toHexString()+ "]\n" +
+      "            }\n" +
+      "        }\n" +
+      "    }"));
+    pipeline.add(ParseQuery.toDocument("{\n" +
+      "        $project: {\n" +
+      "            \"userId\": 0,\n" +
+      "            \"user\": 0\n" +
+      "        }\n" +
+      "    }"));
+    pipeline.add(ParseQuery.toDocument("{\n" +
+      "      $sort: {\n" +
+      "            \"creationDate\": -1\n" +
+      "        }\n" +
+      "    }"));
+    pipeline.add(ParseQuery.toDocument("{ $skip: " + skip + " }"));
+    pipeline.add(ParseQuery.toDocument("{ $limit: " + limit + " }"));
+    return pipeline;
+  }
+
+}
