5d05a9fc776761927a199c019f151d581e354638
Breno Scheffer <brenoscheffer@gmail.com>
2021-04-13 19:44:41 -0300

    OA-239 Teste de aplicação de filtros no query builder

--- source/otus-persistence/src/test/java/br/org/otus/participant/builder/NoteAboutParticipantQueryBuilderTest.java
+++ source/otus-persistence/src/test/java/br/org/otus/participant/builder/NoteAboutParticipantQueryBuilderTest.java
@@ -10,10 +10,13 @@ import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.doReturn;
 
-//@RunWith(PowerMockRunner.class)
+@RunWith(PowerMockRunner.class)
 public class NoteAboutParticipantQueryBuilderTest {
 
   private static final ObjectId USER_OID = new ObjectId("5a33cb4a28f10d1043710f00");
@@ -34,11 +37,11 @@ public class NoteAboutParticipantQueryBuilderTest {
     doReturn(LIMIT).when(searchSettingsDto).getQuantityToGet();
   }
 
-//  @Test
+  @Test
   public void getByRnQuery_method_test(){
     final String EXPECTED_QUERY = "[{" +
         "        \"$match\": {" +
-        "            \"recruitmentNumber\": " + RECRUITMENT_NUMBER.toString() + ".0" +
+        "            \"recruitmentNumber\": " + RECRUITMENT_NUMBER.toString() +
         "        }" +
         "    }," +
         "{" +
@@ -66,35 +69,105 @@ public class NoteAboutParticipantQueryBuilderTest {
         "            \"as\": \"user\"" +
         "        }" +
         "    }," +
-      "{" +
-      "        \"$addFields\": {" +
-      "            \"userName\": {" +
-      "                \"$ifNull\": [ { \"$arrayElemAt\": [\"$user.name\",0.0] }, null ]" +
-      "            }," +
-      "            \"isCreator\": {" +
-      "                \"$eq\": [ {\"$toString\": \"$userId\"}, \"" + USER_OID.toHexString()+ "\"]" +
-      "            }" +
-      "        }" +
-      "    }," +
-      "{" +
-      "        \"$project\": {" +
-      "            \"userId\": 0.0," +
-      "            \"user\": 0.0" +
-      "        }" +
-      "    }," +
-      "{" +
-      "      \"$sort\": {" +
-      "            \"creationDate\": -1.0" +
-      "        }" +
-      "    }," +
-      "{ \"$skip\": " + SKIP + ".0}," +
-      "{ \"$limit\": " + LIMIT + ".0}"  + "]";
-      ;
+        "{" +
+        "        \"$addFields\": {" +
+        "            \"userName\": {" +
+        "                \"$ifNull\": [ { \"$arrayElemAt\": [\"$user.name\",0.0] }, null ]" +
+        "            }," +
+        "            \"isCreator\": {" +
+        "                \"$eq\": [ {\"$toString\": \"$userId\"}, \"" + USER_OID.toHexString()+ "\"]" +
+        "            }" +
+        "        }" +
+        "    }," +
+        "{" +
+        "        \"$project\": {" +
+        "            \"userId\": 0.0," +
+        "            \"user\": 0.0" +
+        "        }" +
+        "    }," +
+        "{" +
+        "      \"$sort\": {" +
+        "            \"creationDate\": -1.0" +
+        "        }" +
+        "    }," +
+        "{ \"$skip\": " + SKIP + ".0}," +
+        "{ \"$limit\": " + LIMIT + ".0}"  + "]";
+    ;
     assertEquals(
-      EXPECTED_QUERY.replaceAll(" ", "").replaceAll("\"\"", "\" \""),
-      new GsonBuilder().create().toJson(
-        queryBuilder.getByRnQuery(USER_OID, RECRUITMENT_NUMBER, searchSettingsDto)
-      ));
+        EXPECTED_QUERY.replaceAll(" ", "").replaceAll("\"\"", "\" \""),
+        new GsonBuilder().create().toJson(
+            queryBuilder.getByRnQuery(USER_OID, RECRUITMENT_NUMBER, searchSettingsDto)
+        ));
+  }
+
+  @Test
+  public void getByRnQuery_method_test_should_consider_filters(){
+    Map<String, Object> map = new HashMap();
+    map.put("starred", true);
+
+    doReturn(map).when(searchSettingsDto).getFilters();
+
+
+    final String EXPECTED_QUERY = "[{" +
+        "        \"$match\": {" +
+        "            \"starred\": " + true + "," +
+        "            \"recruitmentNumber\": " + RECRUITMENT_NUMBER.toString() +
+        "        }" +
+        "    }," +
+        "{" +
+        "        \"$lookup\": {" +
+        "            \"from\": \"user\"," +
+        "            \"let\": {" +
+        "                \"userId\": \"$userId\"" +
+        "            }," +
+        "            \"pipeline\": [" +
+        "                {" +
+        "                    \"$match\": {" +
+        "                        \"$expr\": {" +
+        "                            \"$eq\": [\"$_id\", \"$$userId\"]" +
+        "                        }" +
+        "                    }" +
+        "                }," +
+        "                {" +
+        "                    \"$project\": {" +
+        "                        \"name\": {" +
+        "                            \"$concat\": [\"$name\", \" \", \"$surname\" ]" +
+        "                        }" +
+        "                    }" +
+        "                }" +
+        "            ]," +
+        "            \"as\": \"user\"" +
+        "        }" +
+        "    }," +
+        "{" +
+        "        \"$addFields\": {" +
+        "            \"userName\": {" +
+        "                \"$ifNull\": [ { \"$arrayElemAt\": [\"$user.name\",0.0] }, null ]" +
+        "            }," +
+        "            \"isCreator\": {" +
+        "                \"$eq\": [ {\"$toString\": \"$userId\"}, \"" + USER_OID.toHexString()+ "\"]" +
+        "            }" +
+        "        }" +
+        "    }," +
+        "{" +
+        "        \"$project\": {" +
+        "            \"userId\": 0.0," +
+        "            \"user\": 0.0" +
+        "        }" +
+        "    }," +
+        "{" +
+        "      \"$sort\": {" +
+        "            \"creationDate\": -1.0" +
+        "        }" +
+        "    }," +
+        "{ \"$skip\": " + SKIP + ".0}," +
+        "{ \"$limit\": " + LIMIT + ".0}"  + "]";
+    ;
+    assertEquals(
+        EXPECTED_QUERY.replaceAll(" ", "").replaceAll("\"\"", "\" \""),
+        new GsonBuilder().create().toJson(
+            queryBuilder.getByRnQuery(USER_OID, RECRUITMENT_NUMBER, searchSettingsDto)
+        ));
   }
 
 }
